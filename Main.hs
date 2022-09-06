26813c8d4a1cfe088e9804286c14a46d2955ce00
EgorShibaev <mister.egor03@gmail.com>
2022-09-06 14:07:34 +0300

    HW done

--- Main.hs
+++ Main.hs
@@ -27,29 +27,41 @@ square = Rectangle (PointD 0 0) (PointD 1 1)
 -- чтобы его левый нижний угол был первым аргументом конструктора,
 -- а правый верхний -- вторым.
 normalizeRectangle :: Shape -> Shape
-normalizeRectangle _ = undefined
+normalizeRectangle (Rectangle (PointD x0 y0) (PointD x1 y1)) =
+    Rectangle (PointD (min x0 x1) (min y0 y1)) (PointD (max x0 x1) (max y0 y1))
+normalizeRectangle circle = circle
 
 -- Проверяет, является ли фигура корректной
 -- У круга должен быть положительный радиус
 -- Стороны прямоугольника должны иметь положительную длину
 validateShape :: Shape -> Bool
-validateShape _ = undefined
+validateShape (Rectangle (PointD x0 y0) (PointD x1 y1)) = not ((x0 == x1) || (y0 == y1))
+validateShape (Circle center radius) = radius > 0
 
 -- Считает периметр фигуры
 perimeter :: Shape -> Double
-perimeter _ = undefined
+perimeter (Circle center radius) = 2 * radius * pi
+perimeter (Rectangle (PointD x0 y0) (PointD x1 y1)) = 2 * abs (x0 - x1) + 2 * abs (y0 - y1)
 
 -- Проверяет, является ли фигура квадратом
 isSquare :: Shape -> Bool
-isSquare _ = undefined
+isSquare (Rectangle (PointD x0 y0) (PointD x1 y1)) = (abs (x0 - x1)) == (abs (y0 - y1))
+isSquare _ = False
+
+-- Передвигает точку на x по горизонтали и на y по вертикали
+slidePoint :: PointT -> PointT -> PointT
+slidePoint (PointD x0 y0) (PointD x y) = PointD (x0 + x) (y0 + y)
 
 -- Передвигает фигуру на x по горизонтали и на y по вертикали
 slideShape :: Shape -> PointT -> Shape
-slideShape _ _ = undefined
+slideShape (Rectangle p1 p2) p = Rectangle (slidePoint p1 p) (slidePoint p2 p)
+slideShape (Circle center radius) p = Circle (slidePoint center p) radius
 
 -- Проверяет, находится ли точка внутри данной фигуры
 isPointInShape :: Shape -> PointT -> Bool
-isPointInShape _ _ = undefined
+isPointInShape (Rectangle (PointD x0 y0) (PointD x1 y1)) (PointD x y) =
+    ((x0 < x && x < x1) || (x1 < x && x < x0)) && ((y0 < y && y < y1) || (y1 < y && y < y0))
+isPointInShape (Circle (PointD x0 y0) radius) (PointD x y) = (abs (x0 - x)) ^ 2 + (abs (y0 - y)) ^ 2 < radius ^ 2
 
 -- В результате выполнения программы в консоль должно напечататься True
 -- Если решите не реализовывать одну из функций, закомментируйте соответствующий ей тест
