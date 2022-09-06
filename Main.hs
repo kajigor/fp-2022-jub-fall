866afa588cb95b6abb32823bed52893da5752282
Alex Kovrigin <a.kovrigin0@gmail.com>
2022-09-06 08:01:43 +0000

    complete hw1

--- Main.hs
+++ Main.hs
@@ -27,29 +27,43 @@ square = Rectangle (PointD 0 0) (PointD 1 1)
 -- чтобы его левый нижний угол был первым аргументом конструктора,
 -- а правый верхний -- вторым.
 normalizeRectangle :: Shape -> Shape
-normalizeRectangle _ = undefined
+normalizeRectangle (Rectangle (PointD x0 y0) (PointD x1 y1)) = Rectangle (PointD (min x0 x1) (min y0 y1)) (PointD (max x0 x1) (max y0 y1))
+normalizeRectangle (Circle center radius) = Circle center radius
 
 -- Проверяет, является ли фигура корректной
 -- У круга должен быть положительный радиус
 -- Стороны прямоугольника должны иметь положительную длину
 validateShape :: Shape -> Bool
-validateShape _ = undefined
+validateShape (Circle center radius) = radius > 0
+validateShape (Rectangle (PointD x0 y0) (PointD x1 y1)) = (x1 - x0) /= 0 && (y1 - y0) /= 0
 
 -- Считает периметр фигуры
 perimeter :: Shape -> Double
-perimeter _ = undefined
+perimeter (Circle center radius) = 2 * pi * radius
+perimeter (Rectangle (PointD x0 y0) (PointD x1 y1)) = 2 * (abs(x0 - x1) + abs(y0 - y1))
 
 -- Проверяет, является ли фигура квадратом
 isSquare :: Shape -> Bool
-isSquare _ = undefined
+isSquare (Rectangle (PointD x0 y0) (PointD x1 y1)) = abs(x0 - x1) == abs(y0 - y1)
+isSquare _ = False
 
 -- Передвигает фигуру на x по горизонтали и на y по вертикали
 slideShape :: Shape -> PointT -> Shape
-slideShape _ _ = undefined
+slideShape (Circle (PointD x0 y0) radius) (PointD dx dy) = Circle (PointD (x0 + dx) (y0 + dy)) radius
+slideShape (Rectangle (PointD x0 y0) (PointD x1 y1)) (PointD dx dy) = Rectangle (PointD (x0 + dx) (y0 + dy)) (PointD (x1 + dx) (y1 + dy))
+
+-- Считает квадрат расстояния между точками
+sqdist :: PointT -> PointT -> Double
+sqdist (PointD x0 y0) (PointD x1 y1) = ((x0 - x1)^2 + (y0 - y1)^2)
+
+-- Проверяет, находится ли число в числовом интервале
+isBetween :: Double -> Double -> Double -> Bool
+isBetween a b x = (min a b) < x && x < (max a b)
 
 -- Проверяет, находится ли точка внутри данной фигуры
 isPointInShape :: Shape -> PointT -> Bool
-isPointInShape _ _ = undefined
+isPointInShape (Circle center radius) pt = (sqdist center pt) < radius^2
+isPointInShape (Rectangle (PointD x0 y0) (PointD x1 y1)) (PointD rx ry) = (isBetween x0 x1 rx) && (isBetween y0 y1 ry)
 
 -- В результате выполнения программы в консоль должно напечататься True
 -- Если решите не реализовывать одну из функций, закомментируйте соответствующий ей тест
