96c929561ee31499faf5ddb16ba4b16c83f5b8ec
EgorShibaev <mister.egor03@gmail.com>
2022-09-12 21:43:41 +0200

    HW02

--- src/BinTree.hs
+++ src/BinTree.hs
@@ -6,22 +6,26 @@ data BinTree a = Leaf a -- Лист, содержащий значение
 
 -- Возвращает элемент в корне дерева
 root :: BinTree a -> a
-root _ = undefined
+root (Leaf a) = a
+root (Node a _ _) = a
 
 -- Возвращает листья дерева, перечисленные слева направо
 leaves :: BinTree a -> [a]
-leaves _ = undefined
+leaves (Leaf l) = [l]
+leaves (Node _ leftTree rightTree) = leaves leftTree ++ leaves rightTree
 
 -- Возвращает узлы дерева, перечисленные слева направо, сверху вниз
 nodes :: BinTree a -> [a]
-nodes _ = undefined
+nodes (Leaf l) = [l]
+nodes (Node a leftTree rightTree) = nodes leftTree ++ [a] ++ nodes rightTree
 
 -- Глубина дерева -- длина пути до самого глубокого листа
 -- Глубина дерева из одного листа -- 1
 depth :: BinTree a -> Int
-depth _ = undefined
+depth (Leaf _) = 1
+depth (Node _ leftTree rightTree) = 1 + max (depth leftTree) (depth rightTree)
 
 -- Применяет функцию ко всем элементам дерева
 mapTree :: (a -> b) -> BinTree a -> BinTree b
-mapTree _ _ = undefined
-
+mapTree f (Leaf l) = Leaf (f l)
+mapTree f (Node a leftTree rightTree) = Node (f a) (mapTree f leftTree) (mapTree f rightTree)
--- src/List.hs
+++ src/List.hs
@@ -10,16 +10,18 @@ data List a = Empty -- пустой список без элементов, a.k.
 -- Считает сумму и произведение элементов списка целых чисел за один проход
 -- Постарайтесь обобщить и использовать свертку, но это не обязательно
 sumAndMult :: List Int -> (Int, Int)
-sumAndMult _ = undefined
+sumAndMult = fold (\h ((,) s mult) -> (h + s, h * mult)) (0, 1)
 
 -- Найти максимальное значение в списке
 -- Рекомендую использовать вспомогательную функцию, принимающую значение текущего максимума
 maxNum :: List Int -> Int
-maxNum _ = undefined
+maxNum Empty = 0
+maxNum (AtLeastOne h t) = fold max h t
 
 -- Конкатенация двух списков, работает за длину первого списка
 append :: List a -> List a -> List a
-append _ _ = undefined
+append Empty list = list
+append (AtLeastOne h t) list = AtLeastOne h (append t list)
 
 -- Всюду определенная функция взятия первого элемента
 safeHead :: List a -> Maybe a
