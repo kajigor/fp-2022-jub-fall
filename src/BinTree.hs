49c6f373876c078c3116f796f0706e95be074c0f
Iandmee <48530455+Iandmee@users.noreply.github.com>
2022-09-14 16:47:34 +0300

    Tree

--- src/BinTree.hs
+++ src/BinTree.hs
@@ -6,22 +6,34 @@ data BinTree a = Leaf a -- Лист, содержащий значение
 
 -- Возвращает элемент в корне дерева
 root :: BinTree a -> a
-root _ = undefined
+root (Node a l r) = a
+root (Leaf a) = a
 
 -- Возвращает листья дерева, перечисленные слева направо
+merge :: [a] -> [a] -> [a]
+merge [] b = b
+merge (x : xs) b =  x : merge xs b
 leaves :: BinTree a -> [a]
-leaves _ = undefined
+leaves (Node n l r) = merge (leaves l) (leaves r)
+leaves (Leaf a) = [a]
+leaves _ = []
 
 -- Возвращает узлы дерева, перечисленные слева направо, сверху вниз
 nodes :: BinTree a -> [a]
-nodes _ = undefined
+nodes (Node n l r) = merge (merge (nodes l) [n]) (nodes r)
+nodes (Leaf a) = [a]
+nodes _ = []
 
 -- Глубина дерева -- длина пути до самого глубокого листа
 -- Глубина дерева из одного листа -- 1
 depth :: BinTree a -> Int
-depth _ = undefined
+depth (Node a l r) = max (1+ depth l) (1 + depth r)
+depth (Leaf a) = 1
+depth _ = 0
 
 -- Применяет функцию ко всем элементам дерева
 mapTree :: (a -> b) -> BinTree a -> BinTree b
+mapTree f (Node a l r) = (Node (f a) (mapTree f l) (mapTree f r))
+mapTree f (Leaf a) = Leaf (f a)
 mapTree _ _ = undefined
 
