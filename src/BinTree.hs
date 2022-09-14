fbc6c2f4db118dd72463861ea08b4a0b589b60bd
corwinofamber <andkov18@gmail.com>
2022-09-14 13:20:01 +0200

    HW-2 by Andrey Kovalevskiy

--- src/BinTree.hs
+++ src/BinTree.hs
@@ -6,22 +6,31 @@ data BinTree a = Leaf a -- Лист, содержащий значение
 
 -- Возвращает элемент в корне дерева
 root :: BinTree a -> a
-root _ = undefined
+root tree = case tree of
+  Leaf x -> x
+  Node x _ _ -> x
 
 -- Возвращает листья дерева, перечисленные слева направо
 leaves :: BinTree a -> [a]
-leaves _ = undefined
+leaves tree = case tree of
+  Leaf x -> [x]
+  Node x left right -> (leaves left) ++ (leaves right)
 
 -- Возвращает узлы дерева, перечисленные слева направо, сверху вниз
 nodes :: BinTree a -> [a]
-nodes _ = undefined
+nodes tree = case tree of
+  Leaf x -> [x]
+  Node x left right -> (nodes left) ++ [x] ++ (nodes right)
 
 -- Глубина дерева -- длина пути до самого глубокого листа
 -- Глубина дерева из одного листа -- 1
 depth :: BinTree a -> Int
-depth _ = undefined
+depth tree = case tree of
+  Leaf _ -> 1
+  Node x left right -> (max (depth left) (depth right)) + 1
 
 -- Применяет функцию ко всем элементам дерева
 mapTree :: (a -> b) -> BinTree a -> BinTree b
-mapTree _ _ = undefined
-
+mapTree f tree = case tree of
+  Leaf x -> Leaf (f x)
+  Node x left right -> Node (f x) (mapTree f left) (mapTree f right)
