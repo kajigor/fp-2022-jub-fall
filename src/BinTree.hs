6e2225e166110546e77f657f899914c94d174f10
Iandmee <milaevpro1@gmail.com>
2022-09-16 14:24:34 +0200

    fixes

--- src/BinTree.hs
+++ src/BinTree.hs
@@ -10,30 +10,23 @@ root (Node a l r) = a
 root (Leaf a) = a
 
 -- Возвращает листья дерева, перечисленные слева направо
-merge :: [a] -> [a] -> [a]
-merge [] b = b
-merge (x : xs) b =  x : merge xs b
 leaves :: BinTree a -> [a]
-leaves (Node n l r) = merge (leaves l) (leaves r)
+leaves (Node n l r) = (leaves l) ++ (leaves r)
 leaves (Leaf a) = [a]
-leaves _ = []
 
 -- Возвращает узлы дерева, перечисленные слева направо, сверху вниз
 nodes :: BinTree a -> [a]
-nodes (Node n l r) = merge (merge (nodes l) [n]) (nodes r)
+nodes (Node n l r) = (nodes l) ++ [n] ++ (nodes r)
 nodes (Leaf a) = [a]
-nodes _ = []
 
 -- Глубина дерева -- длина пути до самого глубокого листа
 -- Глубина дерева из одного листа -- 1
 depth :: BinTree a -> Int
-depth (Node a l r) = max (1+ depth l) (1 + depth r)
+depth (Node a l r) = max (1 + depth l) (1 + depth r)
 depth (Leaf a) = 1
-depth _ = 0
 
 -- Применяет функцию ко всем элементам дерева
 mapTree :: (a -> b) -> BinTree a -> BinTree b
 mapTree f (Node a l r) = (Node (f a) (mapTree f l) (mapTree f r))
 mapTree f (Leaf a) = Leaf (f a)
-mapTree _ _ = undefined
 
