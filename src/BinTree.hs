3123af98695a074a1ca93e904c70934794e10063
Timur05032003 <t.malanin@gmail.com>
2022-09-16 21:30:56 +0200

    all done

--- src/BinTree.hs
+++ src/BinTree.hs
@@ -1,4 +1,5 @@
 module BinTree where
+import Control.Arrow (ArrowChoice(left, right))
 
 data BinTree a = Leaf a -- Лист, содержащий значение
                | Node a (BinTree a) (BinTree a) -- Узел со значением и двумя потомками
@@ -6,22 +7,27 @@ data BinTree a = Leaf a -- Лист, содержащий значение
 
 -- Возвращает элемент в корне дерева
 root :: BinTree a -> a
-root _ = undefined
+root (Leaf a)= a
+root (Node a left right ) = a
 
 -- Возвращает листья дерева, перечисленные слева направо
 leaves :: BinTree a -> [a]
-leaves _ = undefined
+leaves (Leaf a) = [a]
+leaves (Node a left right) = leaves left ++ leaves right
 
 -- Возвращает узлы дерева, перечисленные слева направо, сверху вниз
 nodes :: BinTree a -> [a]
-nodes _ = undefined
+nodes (Leaf a) = [a]
+nodes (Node a left right) = nodes left ++ [a] ++ nodes right
 
 -- Глубина дерева -- длина пути до самого глубокого листа
 -- Глубина дерева из одного листа -- 1
 depth :: BinTree a -> Int
-depth _ = undefined
+depth (Leaf a) = 1
+depth (Node a left right) = 1 + max (depth left) (depth right)
 
 -- Применяет функцию ко всем элементам дерева
 mapTree :: (a -> b) -> BinTree a -> BinTree b
-mapTree _ _ = undefined
+mapTree f (Leaf a) = Leaf (f a)
+mapTree f (Node a left right) = Node (f a) (mapTree f left) (mapTree f right)
 
