e371508760c0bb95f22182681ca44e896bfae061
Gleb Ingman <igr2309@gmail.com>
2022-09-18 12:28:04 +0200

    did the task

--- src/BinTree.hs
+++ src/BinTree.hs
@@ -6,22 +6,27 @@ data BinTree a = Leaf a -- Лист, содержащий значение
 
 -- Возвращает элемент в корне дерева
 root :: BinTree a -> a
-root _ = undefined
+root (Node a _ _) = a
+root (Leaf a) = a
 
 -- Возвращает листья дерева, перечисленные слева направо
 leaves :: BinTree a -> [a]
-leaves _ = undefined
+leaves (Node _ l r) = leaves l ++ leaves r
+leaves (Leaf a) = [a]
 
 -- Возвращает узлы дерева, перечисленные слева направо, сверху вниз
 nodes :: BinTree a -> [a]
-nodes _ = undefined
+nodes (Node a l r) = nodes l ++ [a] ++ nodes r
+nodes (Leaf a) = [a]
 
 -- Глубина дерева -- длина пути до самого глубокого листа
 -- Глубина дерева из одного листа -- 1
 depth :: BinTree a -> Int
-depth _ = undefined
+depth (Node _ l r) = 1 + max (depth l) (depth r)
+depth (Leaf _) = 1
 
 -- Применяет функцию ко всем элементам дерева
 mapTree :: (a -> b) -> BinTree a -> BinTree b
-mapTree _ _ = undefined
+mapTree f (Node a l r) = Node (f a) (mapTree f l) (mapTree f r)
+mapTree f (Leaf a) = Leaf (f a)
 
--- src/List.hs
+++ src/List.hs
@@ -10,16 +10,17 @@ data List a = Empty -- пустой список без элементов, a.k.
 -- Считает сумму и произведение элементов списка целых чисел за один проход
 -- Постарайтесь обобщить и использовать свертку, но это не обязательно
 sumAndMult :: List Int -> (Int, Int)
-sumAndMult _ = undefined
+sumAndMult = fold (\x (summ, multt) -> (x + summ, x * multt)) (0, 1)
 
 -- Найти максимальное значение в списке
 -- Рекомендую использовать вспомогательную функцию, принимающую значение текущего максимума
 maxNum :: List Int -> Int
-maxNum _ = undefined
+maxNum = fold max minBound
 
 -- Конкатенация двух списков, работает за длину первого списка
 append :: List a -> List a -> List a
-append _ _ = undefined
+append (AtLeastOne x xs) list = AtLeastOne x (append xs list)
+append Empty list = list
 
 -- Всюду определенная функция взятия первого элемента
 safeHead :: List a -> Maybe a
