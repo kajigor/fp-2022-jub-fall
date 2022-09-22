792bbb3bf64bdb3366c44cf2929ad45059b1f37e
Alex Kovrigin <a.kovrigin0@gmail.com>
2022-09-22 07:53:55 +0000

    hw03 completed

--- src/List.hs
+++ src/List.hs
@@ -90,20 +90,25 @@ map g xs =
 -- Берет первые n элементов списка.
 -- Если в списке меньше n элементов -- возвращает все
 take' :: Int -> [a] -> [a]
-take' n xs = undefined
+take' 0 _ = []
+take' n xs =
+    case xs of
+        [] -> []
+        (h : t) -> h : take' (n - 1) t
 
 -- Реализуйте функцию filter с использованием foldr
 filter' :: (a -> Bool) -> [a] -> [a]
 filter' p xs =
     foldr f [] xs
   where
-    f = undefined
+    f x acc = if p x then x : acc else acc
 
 -- Функция-комбинация zip и map
 -- Применяет функцию f к соответствующим элементам списков xs и ys
 -- zipWith (+) [1,2,3] [10, 20, 30] = [11, 22, 33]
 zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
-zipWith' f xs ys = undefined
+zipWith' f (x:xs) (y:ys) = (f x y) : zipWith' f xs ys
+zipWith' f _ _ = []
 
 -- Бесконечный список от a: [a..]
 -- Диапазон от a до b: [a..b]
@@ -121,7 +126,7 @@ rightTriangles n =
 -- В результате должен получиться бесконечный список
 -- С помощью take из него можно взять конечный список
 squaresOfEvens :: [Int]
-squaresOfEvens = undefined
+squaresOfEvens = [ 4 * a * a | a <- [0..] ]
 
 -- Бесконечный список из единиц
 x :: [Int]
@@ -142,4 +147,6 @@ fibs = 1 : 1 : zipWith (+) fibs (tail fibs)
 -- Треугольные числа
 -- https://en.wikipedia.org/wiki/Triangular_number
 triangularNumbers :: [Int]
-triangularNumbers = undefined
+triangularNumbers = 0 : zipWith (+) nat triangularNumbers
+-- or alternatively
+-- triangularNumbers = [n * (n + 1) `div` 2 | n <- [0..]]
