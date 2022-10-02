01795a59f910436bd8231cd8397ae67c71834f4e
meramiha <62883073+meramiha@users.noreply.github.com>
2022-10-02 20:49:57 +0200

    task 1

--- src/Person.hs
+++ src/Person.hs
@@ -24,27 +24,28 @@ instance MyEq Person where
 instance ToString Person where
   toString :: Person -> String
   toString person =
-    undefined
+    firstName person ++ " " ++ lastName person ++ ", " ++ (toString $ age person)
 
 -- Увеличить возраст на 1
 ageUp :: Person -> Person
 ageUp person =
-  undefined
+  person {age = age person + 1}
 
 -- Сменить фамилию.
 -- Если новая фамилия совпадает с текущей, ничего не меняется
 -- Старая фамилия запоминается в formerLastNames
 updateLastName :: Person -> String -> Person
-updateLastName person newLastName =
-  undefined
+updateLastName person newLastName 
+  | newLastName == lastName person = person
+  | otherwise = person {formerLastNames = lastName person : formerLastNames person, lastName = newLastName}
 
 -- Проверки на корректность (указаны в комментариях к типу данных)
 validatePerson :: Person -> Bool
 validatePerson person =
-  undefined
+  firstName person /= "" && lastName person /= "" && age person >= 0 && (age person >= 14 || idNumber person == (0000, 000000))
 
 -- Проверить, что два человека -- тезки.
 -- Тезки -- разные люди с одинаковыми именами и фамилиями
 namesakes :: Person -> Person -> Bool
 namesakes x y =
-  undefined
+  firstName x == firstName y && lastName x == lastName y && idNumber x /= idNumber y
