f95d3117e2750d8793bc04fa0dac7998fbaa8f80
Mark Ipatov <markthehopeful@gmail.com>
2022-10-03 13:02:13 +0200

    FirstPart HW04

--- src/Person.hs
+++ src/Person.hs
@@ -23,28 +23,26 @@ instance MyEq Person where
 -- Между именем и фамилией пробел, дальше запятая, пробел, и возраст.
 instance ToString Person where
   toString :: Person -> String
-  toString person =
-    undefined
+  toString person = (firstName person) ++ " " ++ (lastName person) ++ ", " ++ (show (age person))
 
 -- Увеличить возраст на 1
 ageUp :: Person -> Person
-ageUp person =
-  undefined
+ageUp person = person {age = ((age person) + 1)}
 
 -- Сменить фамилию.
 -- Если новая фамилия совпадает с текущей, ничего не меняется
 -- Старая фамилия запоминается в formerLastNames
 updateLastName :: Person -> String -> Person
-updateLastName person newLastName =
-  undefined
+updateLastName person newLastName = if ((lastName person) == newLastName)
+                                    then person
+                                    else person {formerLastNames = (lastName person):(formerLastNames person), lastName = newLastName}
 
 -- Проверки на корректность (указаны в комментариях к типу данных)
 validatePerson :: Person -> Bool
-validatePerson person =
-  undefined
+validatePerson person = ((firstName person) /= "") && ((lastName person) /= "") && ((age person) >= 0) && (  ((age person) < 14 && (idNumber person) == (0000, 000000)) || ((age person) >= 14 )  )
 
 -- Проверить, что два человека -- тезки.
 -- Тезки -- разные люди с одинаковыми именами и фамилиями
 namesakes :: Person -> Person -> Bool
-namesakes x y =
-  undefined
+namesakes x y = not (x === y) && (firstName x) == (firstName y) && (lastName x) == (lastName y)
+
