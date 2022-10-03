b6ef5aa9b393339bd0b9deec663b341512f3b0c3
Sleepwalking <alex_shef_31@mail.ru>
2022-10-03 16:10:04 +0300

    done

--- src/NewPerson.hs
+++ src/NewPerson.hs
@@ -1,52 +1,68 @@
 {-# LANGUAGE InstanceSigs #-}
-module Person where
+module NewPerson where
 
 import MyEq (MyEq (..))
 import ToString
 
+data IdNumber = 
+  Passport {series_p :: Int, number_p :: Int} |
+  BirthSertificate {series_s :: String, number_s :: Int}
+  deriving (Show, Eq)
+
 -- Тип данных для человека
-data Person = Person
+data NewPerson = NewPerson
   { firstName :: String         -- Имя, должно быть непустым
   , lastName :: String          -- Фамилия, должна быть непустой
   , formerLastNames :: [String] -- Предыдущие фамилии, если фамилия менялась
-  , age :: Int                  -- Возраст, должен быть неотрицательным
-  , idNumber :: (Int, Int)      -- Номер паспорта: состоит из серии и номера.
-  }                             -- -- У детей (людей младше 14 лет) номера паспорта --- (0000, 000000)
+  , age :: Int                  -- Возраст, должен быть неотрицательным   
+  , idNumber :: IdNumber   
+  }                            
   deriving (Show, Eq)
 
+instance MyEq IdNumber where 
+  (===) :: IdNumber -> IdNumber -> Bool
+  (===) (Passport s1 n1) (Passport s2 n2) = s1 == s2 && n1 == n2
+  (===) (BirthSertificate s1 n1) (BirthSertificate s2 n2) = s1 == s2 && n1 == n2
+  (===) _ _ = False
+
 -- У разных людей разные номера паспортов
-instance MyEq Person where
-  (===) :: Person -> Person -> Bool
+instance MyEq NewPerson where
+  (===) :: NewPerson -> NewPerson -> Bool
   (===) x y = idNumber x === idNumber y
 
 -- Строка должна состоять из имени, фамилии и возраста.
 -- Между именем и фамилией пробел, дальше запятая, пробел, и возраст.
-instance ToString Person where
-  toString :: Person -> String
+instance ToString NewPerson where
+  toString :: NewPerson -> String
   toString person =
     firstName person ++ " " ++ lastName person ++ ", " ++ (show (age person))
 
 -- Увеличить возраст на 1
-ageUp :: Person -> Person
+ageUp :: NewPerson -> NewPerson
 ageUp person =
   person {age = age person + 1}
 
 -- Сменить фамилию.
 -- Если новая фамилия совпадает с текущей, ничего не меняется
 -- Старая фамилия запоминается в formerLastNames
-updateLastName :: Person -> String -> Person
+updateLastName :: NewPerson -> String -> NewPerson
 updateLastName person newLastName =
   person {formerLastNames = newLastNames, lastName = newLastName}
     where newLastNames | newLastName == lastName person = formerLastNames person
                        | otherwise = lastName person : formerLastNames person 
 
+isPassport :: IdNumber -> Bool
+isPassport (Passport s1 n1) = True
+isPassport _ = False
+
 -- Проверки на корректность (указаны в комментариях к типу данных)
-validatePerson :: Person -> Bool
+validatePerson :: NewPerson -> Bool
 validatePerson person =
-  not(null (firstName person)) && not (null (lastName person)) && age person >= 0 && not(age person < 14 && not ((idNumber person) == (0, 0)))
+  not(null (firstName person)) && not (null (lastName person)) && age person >= 0 && (age person >= 14 && isPassport (idNumber person) ||
+  age person < 14 && not (isPassport (idNumber person)))
 
 -- Проверить, что два человека -- тезки.
 -- Тезки -- разные люди с одинаковыми именами и фамилиями
-namesakes :: Person -> Person -> Bool
+namesakes :: NewPerson -> NewPerson -> Bool
 namesakes x y =
   (firstName x == firstName y) && (lastName x == lastName y) && not(x === y)
---
+++ test/Test/NewPerson.hs
@@ -0,0 +1,167 @@
+module Test.NewPerson where
+
+import Test.Tasty.HUnit (Assertion, (@?=), assertBool)
+import NewPerson
+import ToString
+
+person1 :: NewPerson
+person1 =
+  NewPerson { firstName = "Kate"
+         , lastName = "Verbitskaia"
+         , formerLastNames = []
+         , age = 29
+         , idNumber = (Passport 1234 567890) }
+
+person1Aged :: NewPerson
+person1Aged =
+  NewPerson { firstName = "Kate"
+         , lastName = "Verbitskaia"
+         , formerLastNames = []
+         , age = 30
+         , idNumber = (Passport 1234 567890) }
+
+person1AgedTwice :: NewPerson
+person1AgedTwice =
+  NewPerson { firstName = "Kate"
+         , lastName = "Verbitskaia"
+         , formerLastNames = []
+         , age = 31
+         , idNumber = (Passport 1234 567890) }
+
+person2 :: NewPerson
+person2 =
+  NewPerson { firstName = "Kate"
+         , lastName = "Verbitskaia"
+         , formerLastNames = []
+         , age = 42
+         , idNumber = (Passport 9876 543210) }
+
+person3 :: NewPerson
+person3 =
+  NewPerson { firstName = "Kate"
+         , lastName = "Smith"
+         , formerLastNames = []
+         , age = 21
+         , idNumber = (Passport 2121 212121) }
+
+person4 :: NewPerson
+person4 =
+  NewPerson { firstName = "Maria"
+         , lastName = "Verbitskaia"
+         , formerLastNames = []
+         , age = 23
+         , idNumber = (Passport 1111 111111) }
+
+person4NewLastName :: NewPerson
+person4NewLastName =
+  NewPerson { firstName = "Maria"
+         , lastName = "Ivanova"
+         , formerLastNames = ["Verbitskaia"]
+         , age = 23
+         , idNumber = (Passport 1111 111111) }
+
+person4NewLastNameNewLastName :: NewPerson
+person4NewLastNameNewLastName =
+  NewPerson { firstName = "Maria"
+         , lastName = "Sidorova"
+         , formerLastNames = [ "Ivanova", "Verbitskaia" ]
+         , age = 23
+         , idNumber = (Passport 1111 111111) }
+
+child1 :: NewPerson
+child1 =
+  NewPerson { firstName = "Ivan"
+         , lastName = "Ivanov"
+         , formerLastNames = []
+         , age = 7
+         , idNumber = (BirthSertificate "Vab" 328283) }
+
+child2 :: NewPerson
+child2 =
+  NewPerson { firstName = "Masha"
+         , lastName = "Ivanova"
+         , formerLastNames = []
+         , age = 3
+         , idNumber = (BirthSertificate "Xak" 283194) }
+
+notValid1 :: NewPerson
+notValid1 =
+  NewPerson { firstName = ""
+         , lastName = "Verbitskaia"
+         , formerLastNames = []
+         , age = 29
+         , idNumber = (Passport 1234 567890) }
+
+notValid2 :: NewPerson
+notValid2 =
+  NewPerson { firstName = "Kate"
+         , lastName = ""
+         , formerLastNames = []
+         , age = 29
+         , idNumber = (Passport 1234 567890) }
+
+notValid3 :: NewPerson
+notValid3 =
+  NewPerson { firstName = "Kate"
+         , lastName = "Verbitskaia"
+         , formerLastNames = []
+         , age = -13
+         , idNumber = (Passport 1234 567890) }
+
+notValid4 :: NewPerson
+notValid4 =
+  NewPerson { firstName = "Masha"
+         , lastName = "Ivanova"
+         , formerLastNames = []
+         , age = 3
+         , idNumber = (Passport 1234 567890) }
+
+
+unit_ageUp = do
+  ageUp person1 @?= person1Aged
+  ageUp (ageUp person1) @?= person1AgedTwice
+
+unit_updateLastName = do
+  let person4' = updateLastName person4 "Ivanova"
+  let person4'' = updateLastName person4NewLastName "Sidorova"
+  person4' @?= person4NewLastName
+  person4'' @?= person4NewLastNameNewLastName
+  person4 @?= updateLastName person4 (lastName person4)
+
+
+unit_namesakes = do
+  assertBool "namesakes" (namesakes person1 person2)
+  assertBool "notNamesakes: same person" (not $ namesakes person1 person1)
+  assertBool "notNamesakes: same person" (not $ namesakes person1 person1Aged)
+  assertBool "notNamesakes: different last name" (not $ namesakes person1 person3)
+  assertBool "notNamesakes: different first name" (not $ namesakes person1 person4)
+
+unit_toString = do
+  toString person1 @?= "Kate Verbitskaia, 29"
+  toString person1Aged @?= "Kate Verbitskaia, 30"
+  toString person1AgedTwice @?= "Kate Verbitskaia, 31"
+  toString person2 @?= "Kate Verbitskaia, 42"
+  toString person3 @?= "Kate Smith, 21"
+  toString person4 @?= "Maria Verbitskaia, 23"
+  toString person4NewLastName @?= "Maria Ivanova, 23"
+  toString person4NewLastNameNewLastName @?= "Maria Sidorova, 23"
+  toString child1 @?= "Ivan Ivanov, 7"
+  toString child2 @?= "Masha Ivanova, 3"
+
+unit_valid = do
+  assertBool "valid" (validatePerson person1)
+  assertBool "valid" (validatePerson person1Aged)
+  assertBool "valid" (validatePerson person1AgedTwice)
+  assertBool "valid" (validatePerson person2)
+  assertBool "valid" (validatePerson person3)
+  assertBool "valid" (validatePerson person4)
+  assertBool "valid" (validatePerson person4NewLastName)
+  assertBool "valid" (validatePerson person4NewLastNameNewLastName)
+  assertBool "valid" (validatePerson child1)
+  assertBool "valid" (validatePerson child2)
+
+  assertBool "not valid: no first name" (not $ validatePerson notValid1)
+  assertBool "not valid: no last name" (not $ validatePerson notValid2)
+  assertBool "not valid: negative age" (not $ validatePerson notValid3)
+  assertBool "not valid: child with id" (not $ validatePerson notValid4)
+
