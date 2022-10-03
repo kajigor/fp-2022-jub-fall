2cea2daa7ec0231d7265a78f5dcc0dedadab7b38
mikrise2 <mikrise2@mail.ru>
2022-10-03 13:55:45 +0200

    done

--- src/CustomPerson.hs
+++ src/CustomPerson.hs
@@ -1,48 +1,56 @@
 {-# LANGUAGE InstanceSigs #-}
-module Person where
+module CustomPerson where
 
 import MyEq (MyEq (..))
 import ToString
 
+data Document = Passport Int Int | BirthCertificate String Int deriving(Show, Eq)
+
+isChildDocument :: Document -> Bool
+isChildDocument document = case document of
+    Passport _ _ -> False
+    BirthCertificate _ _ -> True
+
+
 -- Тип данных для человека
-data Person = Person
+data CustomPerson = CustomPerson
   { firstName :: String         -- Имя, должно быть непустым
   , lastName :: String          -- Фамилия, должна быть непустой
   , formerLastNames :: [String] -- Предыдущие фамилии, если фамилия менялась
   , age :: Int                  -- Возраст, должен быть неотрицательным
-  , idNumber :: (Int, Int)      -- Номер паспорта: состоит из серии и номера.
-  }                             -- -- У детей (людей младше 14 лет) номера паспорта --- (0000, 000000)
+  , document :: Document     -- документ идентификации
+  }                             
   deriving (Show, Eq)
 
 -- У разных людей разные номера паспортов
-instance MyEq Person where
-  (===) :: Person -> Person -> Bool
-  (===) x y = idNumber x === idNumber y
+instance MyEq CustomPerson where
+  (===) :: CustomPerson -> CustomPerson -> Bool
+  (===) x y = document x == document y
 
 -- Строка должна состоять из имени, фамилии и возраста.
 -- Между именем и фамилией пробел, дальше запятая, пробел, и возраст.
-instance ToString Person where
-  toString :: Person -> String
+instance ToString CustomPerson where
+  toString :: CustomPerson -> String
   toString person = firstName person ++ " " ++ lastName person ++ ", " ++ show (age person)
 
 -- Увеличить возраст на 1
-ageUp :: Person -> Person
+ageUp :: CustomPerson -> CustomPerson
 ageUp person = person{age = (age person) + 1}
 
 -- Сменить фамилию.
 -- Если новая фамилия совпадает с текущей, ничего не меняется
 -- Старая фамилия запоминается в formerLastNames
-updateLastName :: Person -> String -> Person
+updateLastName :: CustomPerson -> String -> CustomPerson
 updateLastName person newLastName
   |lastName person == newLastName = person
   |otherwise = person{ lastName = newLastName, formerLastNames = lastName person : formerLastNames person}
 
 -- Проверки на корректность (указаны в комментариях к типу данных)
-validatePerson :: Person -> Bool
-validatePerson person = firstName person /= "" && lastName person /= "" && age person >= 0 && ((age person >=14 && idNumber person /= (0, 0)) ||
- (age person <14 && idNumber person == (0, 0)))
+validatePerson :: CustomPerson -> Bool
+validatePerson person = firstName person /= "" && lastName person /= "" && age person >= 0 && ((age person >=14 &&  not (isChildDocument (document person))) ||
+ (age person <14 && isChildDocument (document person)))
 
 -- Проверить, что два человека -- тезки.
 -- Тезки -- разные люди с одинаковыми именами и фамилиями
-namesakes :: Person -> Person -> Bool
+namesakes :: CustomPerson -> CustomPerson -> Bool
 namesakes x y = x =/= y && firstName x == firstName y && lastName x == lastName y
