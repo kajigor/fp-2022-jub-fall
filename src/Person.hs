3682aeadaa5d2d8877d3acb244a60a5229a7bcf4
Denis Shabanov <shadenis2015@gmail.com>
2022-10-04 17:24:07 +0200

    Complete Task 1

--- src/Person.hs
+++ src/Person.hs
@@ -14,6 +14,8 @@ data Person = Person
   }                             -- -- У детей (людей младше 14 лет) номера паспорта --- (0000, 000000)
   deriving (Show, Eq)
 
+emptyIdNumber = (0000, 000000)
+
 -- У разных людей разные номера паспортов
 instance MyEq Person where
   (===) :: Person -> Person -> Bool
@@ -23,28 +25,33 @@ instance MyEq Person where
 -- Между именем и фамилией пробел, дальше запятая, пробел, и возраст.
 instance ToString Person where
   toString :: Person -> String
-  toString person =
-    undefined
+  toString person = concat [firstName person, " ", lastName person, ", ", toString (age person)]
 
 -- Увеличить возраст на 1
 ageUp :: Person -> Person
-ageUp person =
-  undefined
+ageUp person = person { age = age person + 1 }
 
 -- Сменить фамилию.
 -- Если новая фамилия совпадает с текущей, ничего не меняется
 -- Старая фамилия запоминается в formerLastNames
 updateLastName :: Person -> String -> Person
-updateLastName person newLastName =
-  undefined
+updateLastName person newLastName
+  | oldLastName =/= newLastName = person { lastName = newLastName, formerLastNames = oldLastName : formerLastNames person }
+  | otherwise = person
+  where oldLastName = lastName person
 
 -- Проверки на корректность (указаны в комментариях к типу данных)
 validatePerson :: Person -> Bool
-validatePerson person =
-  undefined
+validatePerson person = and [
+  not (null (firstName person)),
+  not (null (lastName person)),
+  age person >= 0,
+  (age person < 14) <= (idNumber person === emptyIdNumber)
+  ]
+
 
 -- Проверить, что два человека -- тезки.
 -- Тезки -- разные люди с одинаковыми именами и фамилиями
 namesakes :: Person -> Person -> Bool
-namesakes x y =
-  undefined
+namesakes x y = (getPersonName x === getPersonName y) && (idNumber x =/= idNumber y)
+  where getPersonName person = (firstName person, lastName person)
