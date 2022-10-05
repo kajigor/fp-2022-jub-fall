2294828f224e80ec04ee7283e6100a88d27c17e4
de-shyt <de.shut@inbox.ru>
2022-10-05 18:57:29 +0200

    task 1: 4 of 5

--- src/Person.hs
+++ src/Person.hs
@@ -23,25 +23,29 @@ instance MyEq Person where
 -- Между именем и фамилией пробел, дальше запятая, пробел, и возраст.
 instance ToString Person where
   toString :: Person -> String
-  toString person =
-    undefined
+  toString person = firstName person ++ " " ++ lastName person ++ ", " ++ show (age person)
 
 -- Увеличить возраст на 1
 ageUp :: Person -> Person
 ageUp person =
-  undefined
+  person { age = age person + 1 }
 
 -- Сменить фамилию.
 -- Если новая фамилия совпадает с текущей, ничего не меняется
 -- Старая фамилия запоминается в formerLastNames
 updateLastName :: Person -> String -> Person
-updateLastName person newLastName =
-  undefined
+updateLastName person newLastName | lastName person == newLastName = person
+                                  | otherwise = person { formerLastNames = (lastName person) : (formerLastNames person)
+                                                       , lastName = newLastName }
 
 -- Проверки на корректность (указаны в комментариях к типу данных)
 validatePerson :: Person -> Bool
 validatePerson person =
-  undefined
+       firstName person /= ""
+    && lastName person /= ""
+    && age person >= 0
+    && (age person < 14 && idNumber person == (0, 0) || age person >= 14 && idNumber person /= (0, 0))
+
 
 -- Проверить, что два человека -- тезки.
 -- Тезки -- разные люди с одинаковыми именами и фамилиями
