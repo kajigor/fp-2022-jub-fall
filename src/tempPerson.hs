99ccf3c8e57a1299cbd0cede945875fdc7c386b6
Timur05032003 <t.malanin@gmail.com>
2022-10-05 14:24:57 +0200

    first

--- src/tempPerson.hs
+++ src/tempPerson.hs
@@ -1,11 +1,22 @@
 {-# LANGUAGE InstanceSigs #-}
-module Person where
+module TempPerson where
 
 import MyEq (MyEq (..))
 import ToString
 
+data IdNumber = Passport (Int, Int) | Bc (Int, String)
+    deriving (Show, Eq)
+
+instance MyEq IdNumber where
+    (===) :: IdNumber -> IdNumber -> Bool
+    (===) (Bc _) (Passport _) = False
+    (===) (Bc x) (Bc y) = x == y
+    (===) (Passport _) (Bc _) = False
+    (===) (Passport x) (Passport y) = x === y
+
+
 -- Тип данных для человека
-data Person = Person
+data TempPerson = TempPerson
   { firstName :: String         -- Имя, должно быть непустым
   , lastName :: String          -- Фамилия, должна быть непустой
   , formerLastNames :: [String] -- Предыдущие фамилии, если фамилия менялась
@@ -14,38 +25,39 @@ data Person = Person
   }                             -- -- У детей (людей младше 14 лет) номера паспорта --- (0000, 000000)
   deriving (Show, Eq)
 
+  
 -- У разных людей разные номера паспортов
-instance MyEq Person where
-  (===) :: Person -> Person -> Bool
+instance MyEq TempPerson where
+  (===) :: TempPerson -> TempPerson -> Bool
   (===) x y = idNumber x === idNumber y
 
 -- Строка должна состоять из имени, фамилии и возраста.
 -- Между именем и фамилией пробел, дальше запятая, пробел, и возраст.
-instance ToString Person where
-  toString :: Person -> String
+instance ToString TempPerson where
+  toString :: TempPerson -> String
   toString person =
       firstName person ++ " " ++ lastName person ++ ", " ++ toString (age person)
 
 -- Увеличить возраст на 1
-ageUp :: Person -> Person
+ageUp :: TempPerson -> TempPerson
 ageUp person =
   person {age = 1 + age person}
 
 -- Сменить фамилию.
 -- Если новая фамилия совпадает с текущей, ничего не меняется
 -- Старая фамилия запоминается в formerLastNames
-updateLastName :: Person -> String -> Person
+updateLastName :: TempPerson -> String -> TempPerson
 updateLastName person newLastName =
   if lastName person == newLastName then person
   else person {formerLastNames = lastName person : formerLastNames person, lastName = newLastName}
 
 -- Проверки на корректность (указаны в комментариях к типу данных)
-validatePerson :: Person -> Bool
+validatePerson :: TempPerson -> Bool
 validatePerson person =
-  firstName person /= "" && lastName person /= "" && age person > 0 && ((age person >= 14 && idNumber person /= (0,0)) || (age person <14 && idNumber person == (0,0)))
+  firstName person /= "" && lastName person /= "" && age person >= 0 && (age person >= 14 )
 
 -- Проверить, что два человека -- тезки.
 -- Тезки -- разные люди с одинаковыми именами и фамилиями
-namesakes :: Person -> Person -> Bool
+namesakes :: TempPerson -> TempPerson -> Bool
 namesakes x y =
   (firstName x == firstName y) && (lastName x == lastName y) && not (x === y)
