d536902c194134fd663a9c517c073a8334f20772
khbminus <khbminus@gmail.com>
2022-10-08 17:25:11 +0200

    Finished HW 05

--- src/Person.hs
+++ src/Person.hs
@@ -1,36 +1,101 @@
+{-# LANGUAGE InstanceSigs #-}
 module Person where
 
 import qualified Data.Set as Set
 
-data Tree = ???
+data Tree a = Leaf a | OneChild a (Tree a) | TwoChildren a (Tree a) (Tree a) deriving (Show, Eq)
 
-data Document = ???
+
+data Document = Passport (Int, Int) | BirthCertificate (String, Int) deriving (Show, Eq, Ord)
 
 -- Тип данных для человека
 data Person = Person
-  { firstName :: String         -- Имя, должно быть непустым
-  , lastName :: String          -- Фамилия, должна быть непустой
-  , formerLastNames :: [String] -- Предыдущие фамилии, если фамилия менялась
-  , age :: Int                  -- Возраст, должен быть неотрицательным
-  , idNumber :: Maybe Document  -- Какое-то удостоверение личности
-  , parents :: ??? Person       -- Родители данного человека. Выбрать подходящий контейнер.
+  { firstName :: String, -- Имя, должно быть непустым
+    lastName :: String, -- Фамилия, должна быть непустой
+    formerLastNames :: [String], -- Предыдущие фамилии, если фамилия менялась
+    age :: Int, -- Возраст, должен быть неотрицательным
+    idNumber :: Maybe Document, -- Какое-то удостоверение личности
+    parents :: (Maybe Person, Maybe Person) -- Родители данного человека. Выбрать подходящий контейнер.
   }
-  deriving (Show, Eq)
+  deriving (Show, Eq, Ord)
 
 -- Создание ребенка данных родителей
-createChild :: ??? Person -> Person
-createChild = undefined
+createChild :: Maybe Person -> Maybe Person -> String -> Person
+createChild (Just parent1) (Just parent2) name =
+  Person
+    { firstName = name,
+      lastName = lastName parent1 ++ "—" ++ lastName parent2, -- inspired by Kharitontcev-Beglov
+      formerLastNames = [],
+      age = 0,
+      idNumber = Nothing,
+      parents = (Just parent1, Just parent2)
+    }
+createChild (Just parent1) Nothing name =
+  Person
+    { firstName = name,
+      lastName = lastName parent1,
+      formerLastNames = [],
+      age = 0,
+      idNumber = Nothing,
+      parents = (Just parent1, Nothing)
+    }
+createChild Nothing (Just parent2) name =
+  Person
+    { firstName = name,
+      lastName = lastName parent2,
+      formerLastNames = [],
+      age = 0,
+      idNumber = Nothing,
+      parents = (Nothing, Just parent2)
+    }
+createChild Nothing Nothing name =
+  Person
+    { firstName = name,
+      lastName = "N/A",
+      formerLastNames = [],
+      age = 0,
+      idNumber = Nothing,
+      parents = (Nothing, Nothing)
+    }
 
 -- Самый далекий предок данного человека.
 -- Если на одном уровне иерархии больше одного предка -- вывести самого старшего из них.
 -- Если на одном уровне иерархии больше одного предка одного максимального возраста -- вывести любого из них
 greatestAncestor :: Person -> Person
-greatestAncestor = undefined
+greatestAncestor person = ancestor
+  where
+    helper :: Person -> (Person, Int) -- (самый далекий предок, его уровень иерархии)
+    helper x = case parents x of
+      (Nothing, Nothing) -> (x, 0) -- считаем, что сирота является предком самого себя
+      (Just a, Nothing) -> let (a', aDepth) = helper a in (a', aDepth + 1)
+      (Nothing, Just a) -> let (a', aDepth) = helper a in (a', aDepth + 1)
+      (Just a, Just b) ->
+        let (a', aDepth) = helper a
+         in let (b', bDepth) = helper b
+             in if (aDepth, age a') > (bDepth, age b')
+                  then (a', aDepth + 1)
+                  else (b', bDepth + 1)
+
+    (ancestor, _) = helper person
 
 -- Предки на одном уровне иерархии.
 ancestors :: Int -> Person -> Set.Set Person
-ancestors = undefined
+ancestors level person
+  | level < 0 = Set.empty
+  | level == 0 = Set.singleton person
+  | otherwise = foldTuple (parents person) (ancestors (level - 1))
+  where
+    foldTuple :: (Maybe Person, Maybe Person) -> (Person -> Set.Set Person) -> Set.Set Person
+    foldTuple t f = case t of
+      (Nothing, Nothing) -> Set.empty
+      (Just a, Nothing) -> f a
+      (Nothing, Just a) -> f a
+      (Just a, Just b) -> Set.union (f a) (f b)
 
 -- Возвращает семейное древо данного человека, описывающее его потомков.
-descendants :: Person -> Set.Set Person -> Tree Person
-descendants = undefined
+descendants :: Person -> Tree Person
+descendants person = case parents person of
+  (Nothing, Nothing) -> Leaf person
+  (Just a, Nothing) -> OneChild person (descendants a)
+  (Nothing, Just a) -> OneChild person (descendants a)
+  (Just a, Just b) -> TwoChildren person (descendants a) (descendants b)
--- test/Test/Person.hs
+++ test/Test/Person.hs
@@ -2,3 +2,113 @@ module Test.Person where
 
 import Test.Tasty.HUnit (Assertion, (@?=), assertBool)
 import Person
+    ( ancestors,
+      createChild,
+      descendants,
+      greatestAncestor,
+      Document(BirthCertificate, Passport),
+      Person(..),
+      Tree(TwoChildren, Leaf, OneChild) )
+import qualified Data.Set as Set
+
+person1 :: Person
+person1 = Person {
+    firstName = "Viktor",
+    lastName = "Kharitontcev",
+    formerLastNames = [],
+    age = 12,
+    idNumber = Just (Passport (12, 34)),
+    parents = (Nothing, Nothing)
+}
+person2 :: Person
+person2 = Person {
+    firstName = "Svetlana",
+    lastName = "Beglov",
+    formerLastNames = [],
+    age = 34,
+    idNumber = Just (BirthCertificate ("I-AK", 78)),
+    parents = (Nothing, Nothing)
+}
+
+person3 :: Person
+person3 = Person {
+    firstName = "3",
+    lastName = "3",
+    formerLastNames = [],
+    age = 1,
+    idNumber = Nothing,
+    parents = (Just person1, Just person2)
+}
+person4 :: Person
+person4 = Person {
+    firstName = "4",
+    lastName = "4",
+    formerLastNames = [],
+    age = 1,
+    idNumber = Nothing,
+    parents = (Just person1, Nothing)
+}
+
+person5 :: Person
+person5 = Person {
+    firstName = "5",
+    lastName = "5",
+    formerLastNames = [],
+    age = 1,
+    idNumber = Nothing,
+    parents = (Just person4, Just person2)
+}
+
+
+unit_create_child = do
+    let khb = createChild (Just person1) (Just person2) "Sergei"
+    
+    assertBool "wrong khb" (lastName khb == "Kharitontcev—Beglov")
+    assertBool "wrong khb" (firstName khb == "Sergei")
+    assertBool "wrong khb" (age khb == 0)
+    assertBool "wrong khb" (parents khb == (Just person1, Just person2)) -- preserve order
+
+    let child1 = createChild (Just khb) Nothing "Serg"
+    let child2 = createChild Nothing (Just khb) "Serg"
+    let child3 = createChild Nothing Nothing "lmao"
+
+    assertBool "wrong 1" (lastName child1 == "Kharitontcev—Beglov")
+    assertBool "wrong 1" (firstName child1 == "Serg")
+    assertBool "wrong 1" (age child1 == 0)
+    assertBool "wrong 1" (parents child1 == (Just khb, Nothing)) -- preserve order
+
+    assertBool "wrong 2" (lastName child2 == "Kharitontcev—Beglov")
+    assertBool "wrong 2" (firstName child2 == "Serg")
+    assertBool "wrong 2" (age child2 == 0)
+    assertBool "wrong 2" (parents child2 == (Nothing, Just khb)) -- preserve order
+
+    assertBool "wrong 3" (lastName child3 == "N/A")
+    assertBool "wrong 3" (firstName child3 == "lmao")
+    assertBool "wrong 3" (age child3 == 0)
+    assertBool "wrong 3" (parents child3 == (Nothing, Nothing)) -- preserve order
+
+unit_greatest_ancestor = do
+    greatestAncestor person1 @?= person1
+    greatestAncestor person2 @?= person2
+    greatestAncestor person3 @?= person2
+    greatestAncestor person4 @?= person1
+    greatestAncestor person5 @?= person1
+
+unit_ancestors = do
+    ancestors 0 person1 @?= Set.fromList [person1]
+    ancestors 1 person3 @?= Set.fromList [person1, person2]
+    ancestors 1 person4 @?= Set.fromList [person1]
+    ancestors 1 person3 @?= Set.fromList [person1, person2]
+    ancestors 1 person5 @?= Set.fromList [person4, person2]
+    ancestors 2 person5 @?= Set.fromList [person1]
+
+unit_descendants = do
+    let t1 = Leaf person1
+    let t2 = Leaf person2
+    let t3 = TwoChildren person3 t1 t2
+    let t4 = OneChild person4 t1
+    let t5 = TwoChildren person5 t4 t2
+    let res = [t1, t2, t3, t4, t5]
+    let persons = [descendants person1, descendants person2, descendants person3, descendants person4, descendants person5]
+
+    persons @?= res
