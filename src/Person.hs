d067cb939e1ab9c49b92926f33076c1c9a62ca79
Mikhail Budnikov <mh2481@mail.ru>
2022-10-10 10:58:22 +0200

    Done

--- src/Person.hs
+++ src/Person.hs
@@ -2,35 +2,71 @@ module Person where
 
 import qualified Data.Set as Set
 
-data Tree = Orphan | ChildOf (Tree, Tree)
+data Tree = Tree Person (Set.Set Tree) deriving (Show, Eq)
 
-data Document = Document Int
+instance Ord Tree where
+  (<=) (Tree a _) (Tree b _) = a <= b
+
+data Document = Document Int deriving (Show, Eq, Ord)
 
 -- Тип данных для человека
 data Person = Person
-  { firstName :: String           -- Имя, должно быть непустым
-  , lastName :: String            -- Фамилия, должна быть непустой
-  , formerLastNames :: [String]   -- Предыдущие фамилии, если фамилия менялась
-  , age :: Int                    -- Возраст, должен быть неотрицательным
-  , idNumber :: Maybe Document    -- Какое-то удостоверение личности
-  , parents :: (Person, Person) -- Родители данного человека. Выбрать подходящий контейнер.
+  { firstName :: String                       -- Имя, должно быть непустым
+  , lastName :: String                        -- Фамилия, должна быть непустой
+  , formerLastNames :: [String]               -- Предыдущие фамилии, если фамилия менялась
+  , age :: Int                                -- Возраст, должен быть неотрицательным
+  , idNumber :: Maybe Document                -- Какое-то удостоверение личности
+  , parents :: (Maybe Person, Maybe Person)   -- Родители данного человека. Выбрать подходящий контейнер.
   }
-  deriving (Show, Eq)
+  deriving (Eq, Ord)
 
--- Создание ребенка данных родителей
-createChild :: ??? Person -> Person
-createChild = undefined
+instance Show Person where
+  show Person{firstName = name, lastName = surname} = name ++ " " ++ surname
+
+-- Создание ребенка данных родителей (документы выдадут позже)
+createChild :: Person -> Person -> String -> Person
+createChild father mother name = Person {
+  firstName = name,
+  lastName = lastName father, 
+  formerLastNames = [],
+  age = 0,
+  idNumber = Nothing,
+  parents = (Just father, Just mother)
+}
+
+data PersonWithDepth = PersonWithDepth Person Int deriving (Show, Eq)
+
+instance Ord PersonWithDepth where 
+  (<=) (PersonWithDepth pa da) (PersonWithDepth pb db) = (da, age pa) <= (db, age pb)
+
+greatestAncestorHelper :: Person -> PersonWithDepth
+greatestAncestorHelper person = 
+  let fs = maybe (PersonWithDepth person 0) greatestAncestorHelper (fst $ parents person)
+      ms = maybe (PersonWithDepth person 0) greatestAncestorHelper (snd $ parents person) in
+  let PersonWithDepth answer depth = max fs ms in
+  PersonWithDepth answer (depth + 1)
 
 -- Самый далекий предок данного человека.
 -- Если на одном уровне иерархии больше одного предка -- вывести самого старшего из них.
 -- Если на одном уровне иерархии больше одного предка одного максимального возраста -- вывести любого из них
 greatestAncestor :: Person -> Person
-greatestAncestor = undefined
+greatestAncestor person = ancestor where PersonWithDepth ancestor _ = greatestAncestorHelper person
 
--- Предки на одном уровне иерархии.
+-- -- Предки на одном уровне иерархии.
 ancestors :: Int -> Person -> Set.Set Person
-ancestors = undefined
+ancestors 0 person = Set.fromList [person]
+ancestors k person = 
+  let fs = maybe Set.empty (ancestors (k - 1)) (fst $ parents person)
+      ms = maybe Set.empty (ancestors (k - 1)) (snd $ parents person)
+  in Set.union fs ms
 
--- Возвращает семейное древо данного человека, описывающее его потомков.
-descendants :: Person -> Set.Set Person -> Tree Person
-descendants = undefined
+isParent :: Person -> Person -> Bool
+isParent first second = Just first `elem` [fst p, snd p]
+  where p = parents second
+
+-- -- Возвращает семейное древо данного человека, описывающее его потомков.
+descendants :: Person -> Set.Set Person -> Tree
+descendants person people =
+  let children = Set.filter (isParent person) people in
+    Tree person (Set.map buildTree children)
+    where buildTree someone = descendants someone people
--- test/Test/Person.hs
+++ test/Test/Person.hs
@@ -2,3 +2,72 @@ module Test.Person where
 
 import Test.Tasty.HUnit (Assertion, (@?=), assertBool)
 import Person
+import Data.Set
+
+createPerson :: String -> String -> Int -> Int -> (Maybe Person, Maybe Person) -> Person
+createPerson name surname age id parents = Person {
+    firstName = name,
+    lastName = surname,
+    formerLastNames = [],
+    age = age,
+    idNumber = Just (Document id),
+    parents = parents
+}
+
+bb :: Person
+bb = createPerson "Berend" "Bohn" 50 143587 (Nothing, Nothing)
+vm :: Person
+vm = createPerson "Viktoria" "Mayer" 45 234983 (Nothing, Nothing)
+hl :: Person
+hl = createPerson "Hraban" "Larenz" 47 234983 (Nothing, Nothing)
+ho :: Person
+ho = createPerson "Henrike" "Otto" 48 485838 (Nothing, Nothing)
+md :: Person
+md = createPerson "Menne" "Dickson" 25 867342 (Just bb, Just vm)
+gb :: Person
+gb = createPerson "Grete" "Bauer" 22 192837 (Just hl, Just ho)
+al :: Person
+al = createPerson "Ada" "Luther" 100 989795 (Nothing, Nothing)
+lm :: Person
+lm = createPerson "Lothur" "Muller" 31 384950 (Nothing, Just vm)
+eg :: Person
+eg = createPerson "Elke" "Gerven" 5 12345 (Just lm, Just gb)
+js :: Person
+js = createPerson "Jurgen" "Shmidt" 3 54321 (Just md, Just al)
+kd :: Person
+kd = createChild md gb "Klaus"
+
+unit_greatestAncestor = do
+    greatestAncestor vm @?= vm
+    greatestAncestor md @?= bb
+    greatestAncestor gb @?= ho
+    greatestAncestor eg @?= ho
+    greatestAncestor js @?= bb
+    greatestAncestor kd @?= bb
+
+unit_ancestors = do
+    ancestors 0 kd @?= fromList [kd]
+    ancestors 1 kd @?= fromList [md, gb]
+    ancestors 2 kd @?= fromList [bb, vm, hl, ho]
+    ancestors 4 kd @?= fromList []
+    ancestors 1000 kd @?= fromList []
+    ancestors (-1) kd @?= fromList []
+    ancestors 2 js @?= fromList [bb, vm]
+    ancestors 2 eg @?= fromList [vm, hl, ho]
+
+unit_isParent = do
+    isParent bb md @?= True
+    isParent ho gb @?= True
+    isParent hl md @?= False
+    isParent md md @?= False
+    isParent js md @?= False
+    isParent eg md @?= False
+    isParent bb kd @?= False
+    isParent bb eg @?= False
+
+people = fromList [bb, vm, hl, ho, lm, md, gb, al, eg, kd, js]
+
+unit_descendants = do
+    descendants kd people @?= Tree kd empty
+    descendants md people @?= Tree md (fromList [Tree kd empty, Tree js empty])
+    descendants ho people @?= Tree ho (fromList [Tree gb (fromList [Tree eg empty, Tree kd empty])])
