94d1a239e45b77e5e812f526e8e769cd9163e70c
Sleepwalking <alex_shef_31@mail.ru>
2022-10-10 12:57:34 +0300

    first commit

--- src/Person.hs
+++ src/Person.hs
@@ -2,9 +2,16 @@ module Person where
 
 import qualified Data.Set as Set
 
-data Tree = ???
+data Tree a = Tree
+  {
+    curPerson :: a,
+    descendantTrees :: Set.Set (Tree a)
+  }
+  deriving (Show, Eq, Ord)
 
-data Document = ???
+data Document = Passport (Int, Int) | 
+                BirthCert Int
+                deriving (Show, Eq, Ord)
 
 -- Тип данных для человека
 data Person = Person
@@ -13,24 +20,36 @@ data Person = Person
   , formerLastNames :: [String] -- Предыдущие фамилии, если фамилия менялась
   , age :: Int                  -- Возраст, должен быть неотрицательным
   , idNumber :: Maybe Document  -- Какое-то удостоверение личности
-  , parents :: ??? Person       -- Родители данного человека. Выбрать подходящий контейнер.
+  , parents :: Set.Set Person       -- Родители данного человека. Выбрать подходящий контейнер.
   }
-  deriving (Show, Eq)
+  deriving (Show, Eq, Ord)
 
 -- Создание ребенка данных родителей
-createChild :: ??? Person -> Person
-createChild = undefined
+createChild :: Set.Set Person -> String -> String -> Person
+createChild parents firstName lastName = 
+  Person {firstName = firstName, lastName = lastName, formerLastNames = [], 
+  age = 0, idNumber = Nothing, parents = parents}
 
 -- Самый далекий предок данного человека.
 -- Если на одном уровне иерархии больше одного предка -- вывести самого старшего из них.
 -- Если на одном уровне иерархии больше одного предка одного максимального возраста -- вывести любого из них
 greatestAncestor :: Person -> Person
-greatestAncestor = undefined
+greatestAncestor person = 
+  let helpfunction person | parents person == Set.empty = (1, person)
+                          | otherwise = helpfunction1 (Set.drop 1 (parents person)) (helpfunction (Set.elemAt 0 (parents person)))
+                          where helpfunction1 parents (h, best_person) | parents == Set.empty = (h, best_person)
+                                                                       | ((fst cur) > h || (fst cur) == h && ((age (snd cur)) > (age best_person))) = helpfunction1 (Set.drop 1 parents) cur
+                                                                       | otherwise = helpfunction1 (Set.drop 1 parents) (h, best_person)
+                                                                       where cur = helpfunction (Set.elemAt 0 parents)
+  in snd(helpfunction person) 
 
 -- Предки на одном уровне иерархии.
 ancestors :: Int -> Person -> Set.Set Person
-ancestors = undefined
+ancestors h person | h == 0 = Set.singleton person
+                   | otherwise = helpfunction (parents person)
+                   where helpfunction parents | parents == Set.empty = Set.empty
+                                              | otherwise = (Set.union (ancestors (h - 1) (Set.elemAt 0 parents)) (helpfunction (Set.drop 1 parents)))
 
 -- Возвращает семейное древо данного человека, описывающее его потомков.
 descendants :: Person -> Set.Set Person -> Tree Person
-descendants = undefined
+descendants person setPersons = Tree person (Set.map (\curperson -> descendants curperson setPersons) (Set.filter (\curperson -> Set.member person (parents curperson)) setPersons))
--- test/Test/Person.hs
+++ test/Test/Person.hs
@@ -2,3 +2,65 @@ module Test.Person where
 
 import Test.Tasty.HUnit (Assertion, (@?=), assertBool)
 import Person
+import qualified Data.Set as Set
+
+person1 :: Person
+person1 =
+  Person { firstName = "Kate"
+         , lastName = "Verbitskaia"
+         , formerLastNames = []
+         , age = 56
+         , idNumber = Just (Passport (1234, 567890))
+         , parents = Set.empty }
+
+person2 :: Person
+person2 =
+  Person { firstName = "Yuri"
+         , lastName = "Verbitskiy"
+         , formerLastNames = []
+         , age = 59
+         , idNumber = Just (Passport (1234, 567820))
+         , parents = Set.empty }
+
+person3 :: Person
+person3 =
+  Person { firstName = "Alena"
+         , lastName = "Arama"
+         , formerLastNames = []
+         , age = 84
+         , idNumber = Just (Passport (1234, 186183))
+         , parents = Set.empty }
+
+person4 = (createChild (Set.fromList [person1, person2]) "Artem" "Verbistkiy") {age = 32}
+
+person5 = (createChild (Set.fromList [person3]) "Elena" "Arama") {age = 60}
+
+person6 = (createChild (Set.fromList [person5]) "Ekaterina" "Arama") {age = 28}
+
+person7 = (createChild (Set.fromList [person4, person6]) "Anna" "Verbitskaia") {age = 28}
+
+person8 = (createChild (Set.fromList [person3]) "Anna" "Trott") {age = 58}
+
+setAll = Set.fromList [person1, person2, person3, person4, person5, person6, person7, person8]
+
+unit_ancestors = do
+  ancestors 1 person7 @?= Set.fromList [person4, person6]
+  ancestors 2 person7 @?= Set.fromList [person1, person2, person5]
+  ancestors 3 person7 @?= Set.fromList [person3]
+  ancestors 1 person4 @?= Set.fromList [person1, person2]
+  ancestors 2 person6 @?= Set.fromList [person3]
+
+unit_greatestAncestor = do
+  greatestAncestor person1 @?= person1
+  greatestAncestor person7 @?= person3
+  greatestAncestor person4 @?= person2
+  greatestAncestor person8 @?= person3
+  greatestAncestor person6 @?= person3
+
+unit_descendants = do
+  descendants person1 setAll @?= Tree person1 (Set.fromList[Tree person4 (Set.fromList [Tree person7 Set.empty])])
+  descendants person2 setAll @?= Tree person2 (Set.fromList[Tree person4 (Set.fromList [Tree person7 Set.empty])])
+  descendants person4 setAll @?= Tree person4 (Set.fromList [Tree person7 Set.empty])
+  descendants person7 setAll @?= Tree person7 Set.empty
+  descendants person3 setAll @?= Tree person3 (Set.fromList[(Tree person5 (Set.fromList [Tree person6 (Set.fromList[Tree person7 Set.empty])])), (Tree person8 Set.empty)])
+  descendants person8 setAll @?= Tree person8 Set.empty
