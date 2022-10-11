2a95eb7d7eb1cdd911bff1711166e1777a0da85c
jay_from_future <jay.from.future@gmail.com>
2022-10-11 18:32:29 +0200

    Implement all except descendants

--- src/Person.hs
+++ src/Person.hs
@@ -1,10 +1,19 @@
 module Person where
 
 import qualified Data.Set as Set
+import Data.Maybe (fromJust, isNothing, isJust)
 
-data Tree = ???
+-- (1) дерево состоит из людей и их детей
+-- (2) дети тоже деревья, которые состоят из (1)
+data Tree a = Tree {
+    node :: a,
+    children :: Set.Set (Tree a)
+  }
+  deriving (Show, Eq)
 
-data Document = ???
+data Document = Passport (Int, Int) -- Номер паспорта состоит из серии и номера, выдается в 14 лет.
+  | BirthCertificate Int -- Номер свидетельства о рождении.
+  deriving (Show, Eq, Ord)
 
 -- Тип данных для человека
 data Person = Person
@@ -12,25 +21,66 @@ data Person = Person
   , lastName :: String          -- Фамилия, должна быть непустой
   , formerLastNames :: [String] -- Предыдущие фамилии, если фамилия менялась
   , age :: Int                  -- Возраст, должен быть неотрицательным
-  , idNumber :: Maybe Document  -- Какое-то удостоверение личности
-  , parents :: ??? Person       -- Родители данного человека. Выбрать подходящий контейнер.
+  , document :: Maybe Document  -- Какое-то удостоверение личности
+  , parents :: Maybe (Person,Person)  -- Родители данного человека. Выбрать подходящий контейнер.
+  -- Nothing - у человека нет родителей
+  -- Just(a,b) - у человека два родителя (a и b)
+  -- Just(a,a) - у человека один родитель (a)
   }
-  deriving (Show, Eq)
+  deriving (Show, Eq, Ord)
 
 -- Создание ребенка данных родителей
-createChild :: ??? Person -> Person
-createChild = undefined
+createChild :: (Person, Person) -> Person -> Person
+createChild (parent1, parent2) person = Person {
+  firstName = firstName person,
+  lastName = lastName person,
+  formerLastNames = formerLastNames person,
+  age = age person,
+  document = document person,
+  parents = Just (parent1, parent2)
+}
 
 -- Самый далекий предок данного человека.
 -- Если на одном уровне иерархии больше одного предка -- вывести самого старшего из них.
 -- Если на одном уровне иерархии больше одного предка одного максимального возраста -- вывести любого из них
 greatestAncestor :: Person -> Person
-greatestAncestor = undefined
+greatestAncestor person =
+  if isNothing (parents person)
+    then person
+    else
+      let personParents = fromJust(parents person) in
+      let a = greatestAncestor (fst personParents)
+          b =  greatestAncestor (snd personParents)
+      in if age a > age b then a else b
 
 -- Предки на одном уровне иерархии.
 ancestors :: Int -> Person -> Set.Set Person
-ancestors = undefined
+ancestors 0 person =
+    if isNothing (parents person)
+    then Set.empty
+    else
+      let personParents = fromJust(parents person) in
+        Set.fromList [fst personParents, snd personParents]
+
+ancestors level person =
+  if isNothing (parents person)
+    then Set.empty
+    else
+      let personParents = fromJust(parents person) in
+      let f = ancestors (level - 1) (fst personParents)
+          s = ancestors (level - 1) (snd personParents)
+      in Set.union f s
+
 
 -- Возвращает семейное древо данного человека, описывающее его потомков.
 descendants :: Person -> Set.Set Person -> Tree Person
-descendants = undefined
+descendants currentPerson allPersons = undefined
+--   if allPersons == Set.empty
+--     then Tree currentPerson Set.empty
+--     else Tree currentPerson (filter (isParent currentPerson) allPersons)
+
+-- isParent :: Person -> Person -> Bool
+-- isParent parent person =
+--   isJust (parents person) && (
+--     let personParents = fromJust(parents person) in
+--       (parent == fst personParents) || ( parent == snd personParents))
--- test/Test/Person.hs
+++ test/Test/Person.hs
@@ -2,3 +2,162 @@ module Test.Person where
 
 import Test.Tasty.HUnit (Assertion, (@?=), assertBool)
 import Person
+import Data.Set as Set
+
+greatGrandMotherM :: Person
+greatGrandMotherM = Person
+    { firstName = "Galina",
+      lastName = "Pamfilova",
+      formerLastNames = [],
+      age = 101,
+      document = Just (Passport(3322, 555444)),
+      parents = Nothing
+    }
+
+grandMotherM :: Person
+grandMotherM = Person
+    { firstName = "Claudia",
+      lastName = "Sidorova",
+      formerLastNames = [],
+      age = 81,
+      document = Just (Passport(2222, 555444)),
+      parents = Just (greatGrandMotherM, greatGrandMotherM)
+    }
+
+grandFatherM :: Person
+grandFatherM = Person
+    { firstName = "Innokentii",
+      lastName = "Sidorov",
+      formerLastNames = [],
+      age = 85,
+      document = Just (Passport(3333, 666777)),
+      parents = Nothing
+    }
+
+mother :: Person
+mother = Person
+    { firstName = "Anna",
+      lastName = "Ivanova",
+      formerLastNames = [],
+      age = 51,
+      document = Just (Passport(2222, 444333)),
+      parents = Just (grandMotherM, grandFatherM)
+    }
+
+greatGrandMotherF :: Person
+greatGrandMotherF = Person
+    { firstName = "Eleanora",
+      lastName = "Ivanova",
+      formerLastNames = [],
+      age = 99,
+      document = Just (Passport(2233, 444555)),
+      parents = Nothing
+    }
+
+greatGrandFatherF :: Person
+greatGrandFatherF = Person
+    { firstName = "Zahar",
+      lastName = "Ivanov",
+      formerLastNames = [],
+      age = 105,
+      document = Just (Passport(1233, 144555)),
+      parents = Nothing
+    }
+
+grandMotherF :: Person
+grandMotherF = Person
+    { firstName = "Anastasija",
+      lastName = "Ivanova",
+      formerLastNames = [],
+      age = 78,
+      document = Just (Passport(2222, 999333)),
+      parents = Just(greatGrandMotherF, greatGrandFatherF)
+    }
+
+grandFatherF :: Person
+grandFatherF = Person
+    { firstName = "Petr",
+      lastName = "Ivanov",
+      formerLastNames = [],
+      age = 88,
+      document = Just (Passport(2222, 444888)),
+      parents = Nothing
+    }
+
+father :: Person
+father = Person
+    { firstName = "Ivan",
+      lastName = "Ivanov",
+      formerLastNames = [],
+      age = 59,
+      document = Just (Passport(3333, 444333)),
+      parents = Just(grandMotherF, grandFatherF)
+    }
+
+personWithoutParents :: Person
+personWithoutParents =
+  Person
+    { firstName = "Mark",
+      lastName = "Ivanov",
+      formerLastNames = [],
+      age = 21,
+      document = Just (Passport(1111, 444333)),
+      parents = Nothing
+    }
+
+personWithParents :: Person
+personWithParents =
+  Person
+    { firstName = "Mark",
+      lastName = "Ivanov",
+      formerLastNames = [],
+      age = 21,
+      document = Just (Passport(1111, 444333)),
+      parents = Just(mother, father)
+    }
+
+child1 :: Person
+child1 =
+  Person
+    { firstName = "Mark II",
+      lastName = "Ivanov",
+      formerLastNames = [],
+      age = 1,
+      document = Just (BirthCertificate 1111),
+      parents = Just(personWithoutParents, personWithoutParents)
+    }
+
+child2 :: Person
+child2 =
+  Person
+    { firstName = "Mark III",
+      lastName = "Ivanov",
+      formerLastNames = [],
+      age = 2,
+      document = Just (BirthCertificate 2222),
+      parents = Just(personWithoutParents, personWithoutParents)
+    }
+
+unit_createChild = do
+    personWithParents @?= createChild (mother, father) personWithoutParents
+
+unit_ancestors = do
+    Set.empty @?= ancestors 0 personWithoutParents
+    Set.empty @?= ancestors 3 personWithoutParents
+    Set.fromList [mother, father] @?= ancestors 0 personWithParents
+    Set.fromList [grandMotherM, grandFatherM, grandMotherF, grandFatherF] @?= ancestors 1 personWithParents
+    Set.fromList [greatGrandMotherM, greatGrandMotherF, greatGrandFatherF] @?= ancestors 2 personWithParents
+    Set.empty @?= ancestors 3 personWithParents
+
+unit_greatestAncestor = do
+    personWithoutParents @?= greatestAncestor personWithoutParents
+    greatGrandFatherF @?= greatestAncestor personWithParents
+
+-- unit_descendants = do
+    -- Tree personWithoutParents Set.empty @?= descendants personWithoutParents Set.empty
+    -- Tree personWithoutParents (Set.fromList [Tree child1]) @?= descendants personWithoutParents (Set.fromList [child1])
+    -- Descendant (Set.fromList [child1, child2]) @?= descendants personWithoutParents (Set.fromList [child1, child2])
+    -- Descendant (Set.fromList [personWithParents]) @?= descendants mother (Set.fromList [personWithParents, child1, child2])
+    -- Descendant (Set.fromList [mother, personWithParents]) @?= descendants grandMotherM (Set.fromList [mother, personWithParents, child1, child2])
+
+    
