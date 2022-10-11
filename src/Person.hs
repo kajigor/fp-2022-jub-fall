d6dbd4cd8aee520a83fc84e5402becfcb021fbba
mikrise2 <mikrise2@mail.ru>
2022-10-11 09:07:49 +0200

    done

--- src/Person.hs
+++ src/Person.hs
@@ -2,9 +2,16 @@ module Person where
 
 import qualified Data.Set as Set
 
-data Tree = ???
+data Tree a = Tree 
+  { person :: Person
+  , parent1 :: Maybe (Tree Person)
+  , parent2 :: Maybe (Tree Person)
+  , children :: [Person]
+  }
+  deriving (Show, Eq, Ord)
 
-data Document = ???
+data Document = BirthCertificate (String, Int) | Passport (Int, Int) 
+  deriving (Show, Eq, Ord)
 
 -- Тип данных для человека
 data Person = Person
@@ -12,25 +19,52 @@ data Person = Person
   , lastName :: String          -- Фамилия, должна быть непустой
   , formerLastNames :: [String] -- Предыдущие фамилии, если фамилия менялась
   , age :: Int                  -- Возраст, должен быть неотрицательным
-  , idNumber :: Maybe Document  -- Какое-то удостоверение личности
-  , parents :: ??? Person       -- Родители данного человека. Выбрать подходящий контейнер.
+  , document :: Maybe Document  -- Какое-то удостоверение личности
+  , parents :: (Maybe Person, Maybe Person)       -- Родители данного человека. Выбрать подходящий контейнер.
   }
-  deriving (Show, Eq)
+  deriving (Show, Eq, Ord)
 
 -- Создание ребенка данных родителей
-createChild :: ??? Person -> Person
-createChild = undefined
+createChild :: String -> String -> String -> Int -> (Maybe Person, Maybe Person) -> Person
+createChild firstName lastName series number (p1, p2) =
+  Person firstName lastName [] 0 (Just (BirthCertificate (series, number))) (p1,p2)
 
 -- Самый далекий предок данного человека.
 -- Если на одном уровне иерархии больше одного предка -- вывести самого старшего из них.
 -- Если на одном уровне иерархии больше одного предка одного максимального возраста -- вывести любого из них
 greatestAncestor :: Person -> Person
-greatestAncestor = undefined
+greatestAncestor person = result
+  where
+    greatestPersonWithDepth :: Person -> (Person, Int) 
+    greatestPersonWithDepth person1 = case parents person1 of
+      (Nothing, Nothing) -> (person1, 0)
+      (Just person2, Just person3) ->
+        let (pesron4, person2Depth) = greatestPersonWithDepth person2
+         in let (person5, person3Depth) = greatestPersonWithDepth person3
+             in if (person2Depth, age pesron4) > (person3Depth, age person5)
+                  then (pesron4, person2Depth + 1)
+                  else (person5, person3Depth + 1)
+      (_, Just person2) -> let (pesron4,person2Depth) = greatestPersonWithDepth person2 in (pesron4, person2Depth + 1)
+      (Just person2, _) -> let (pesron4, person2Depth) = greatestPersonWithDepth person2 in (pesron4, person2Depth + 1)
+    (result, _) = greatestPersonWithDepth person
 
 -- Предки на одном уровне иерархии.
 ancestors :: Int -> Person -> Set.Set Person
-ancestors = undefined
+ancestors 0 person = Set.fromList [person]
+ancestors n person = 
+  let f = maybe Set.empty (ancestors (n - 1)) (fst (parents person))
+      m = maybe Set.empty (ancestors (n - 1)) (snd (parents person))
+  in Set.union f m
 
 -- Возвращает семейное древо данного человека, описывающее его потомков.
 descendants :: Person -> Set.Set Person -> Tree Person
-descendants = undefined
+descendants person list = parentTree person (Set.toList (Set.filter (isChild person) list))
+  where
+    parentTree :: Person -> [Person] -> Tree Person
+    parentTree person list = case parents person of
+      (Just person1, Just person2) -> Tree person (Just ( parentTree person1 [person])) (Just ( parentTree person2 [person])) list
+      (Just person1, Nothing) -> Tree person (Just (parentTree person1 [person])) Nothing list
+      (Nothing, Just person2) -> Tree person Nothing (Just (parentTree person2 [person])) list
+      (Nothing, Nothing) -> Tree person Nothing Nothing list
+
+    isChild parent child = fst (parents child) == Just parent || snd  (parents child) == Just parent
--- test/Test/Person.hs
+++ test/Test/Person.hs
@@ -2,3 +2,103 @@ module Test.Person where
 
 import Test.Tasty.HUnit (Assertion, (@?=), assertBool)
 import Person
+import qualified Data.Set as Set
+
+person1 :: Person
+person1 = Person {
+    firstName = "Oleg",
+    lastName = "Makeev",
+    formerLastNames = [],
+    age = 29,
+    document = Just (Passport (77, 9993299)),
+    parents = (Nothing, Nothing)
+}
+
+person2 :: Person
+person2 = Person {
+    firstName = "Oleg",
+    lastName = "Veshiy",
+    formerLastNames = ["Vsevidyashiy"],
+    age = 23,
+    document = Just (Passport (77, 77777777)),
+    parents = (Nothing, Nothing)
+}
+
+person3 :: Person
+person3 = Person {
+    firstName = "Machael",
+    lastName = "Miringov",
+    formerLastNames = [],
+    age = 82,
+    document = Just (Passport (99, 99999999)),
+    parents = (Nothing, Nothing)
+}
+
+person4 :: Person
+person4 = Person {
+    firstName = "Timur",
+    lastName = "Malanin",
+    formerLastNames = ["Podkast"],
+    age = 93,
+    document = Just (Passport (10, 1010101010)),
+    parents = (Just person1, Just person2)
+}
+
+person5 :: Person
+person5 = Person {
+    firstName = "Daniil",
+    lastName = "Karol",
+    formerLastNames = ["Buntar", "Meduzu--mne-v-rot"],
+    age = 23,
+    document = Just (Passport (123, 123123123)),
+    parents = (Just person3, Nothing)
+}
+
+person6 :: Person
+person6 = Person {
+    firstName = "Peson",
+    lastName = "Personovich",
+    formerLastNames = [],
+    age = 12,
+    document = Just (Passport (232, 123123123)),
+    parents = (Just person4, Just person5)
+}
+
+unit_create_child = do
+    let child1 = createChild "1" "2" "SS" 13132 (Nothing, Nothing)
+    let child2 = createChild "3" "4" "QS" 32132 (Just person1, Nothing)
+    let child3 = createChild "5" "6" "FF" 22424 (Nothing, Just person2)
+    let child4 = createChild "1" "2" "SS" 86868 (Just person1, Just person2)
+
+    assertBool "wrong child" (age child1 == 0 && formerLastNames child1 == [] && parents child1 == (Nothing, Nothing))
+    assertBool "wrong child" (age child2 == 0 && formerLastNames child2 == [] && parents child2 == (Just person1, Nothing))
+    assertBool "wrong child" (age child3 == 0 && formerLastNames child3 == [] && parents child3 == (Nothing, Just person2))
+    assertBool "wrong child" (age child4 == 0 && formerLastNames child4 == [] && parents child4 == (Just person1, Just person2))
+
+unit_greatest_ancestor = do
+    greatestAncestor person6 @?= person3
+    greatestAncestor person5 @?= person3
+    greatestAncestor person4 @?= person1
+    greatestAncestor person3 @?= person3
+    greatestAncestor person2 @?= person2
+    greatestAncestor person1 @?= person1
+
+unit_ancestors = do
+    ancestors 2 person6 @?= Set.fromList [person1, person2, person3]
+    ancestors 1 person6 @?= Set.fromList [person4, person5]
+    ancestors 0 person6 @?= Set.fromList [person6]
+
+unit_descendants = do
+    let tree1 = Tree person1 Nothing Nothing [person4]
+    let tree2 = Tree person2 Nothing Nothing [person4]
+    let tree3 = Tree person3 Nothing Nothing [person5]
+    let tree4 = Tree person4 (Just tree1) (Just tree2) [person6]
+    let tree5 = Tree person5 (Just tree3) Nothing [person6]
+    let tree6 = Tree person6 (Just tree4) (Just tree5) []
+
+    descendants person1 (Set.fromList [person4]) @?= tree1
+    descendants person2 (Set.fromList [person4]) @?= tree2
+    descendants person3 (Set.fromList [person5]) @?= tree3
+    descendants person4 (Set.fromList [person6]) @?= tree4
+    descendants person5 (Set.fromList [person6]) @?= tree5
+    descendants person6 (Set.fromList [person1, person2, person4]) @?= tree6
