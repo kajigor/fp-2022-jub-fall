5501d1be5a55e90b748257e2b536e968e640f9b6
ereminaeliz <eremina.eliz2014@gmail.com>
2022-10-11 17:42:29 +0300

    tasks completed

--- src/Person.hs
+++ src/Person.hs
@@ -1,36 +1,91 @@
-module Person where
+module Person (createChild, greatestAncestor, ancestors, descendants) where
 
 import qualified Data.Set as Set
 
-data Tree = ???
+data Tree = Tree
+  { thisPerson :: Person
+  , allDescendants :: Set.Set Tree } -- Для каждого ребёнка будет своё дерево, что-то типа рекурсии,
+                                       -- то есть количество деревьев в множестве - количество детей
+  deriving (Show, Eq, Ord)
 
-data Document = ???
+data BirthCertificate = BirthCertificate
+  { birthCertSeries :: String
+  , birthCertNumber :: Int }
+  deriving (Show, Eq, Ord)
+
+data Passport = Passport
+  { passportSeries :: Int
+  , passportNumber :: Int }
+  deriving (Show, Eq, Ord)
+
+data Document = BirthCertificate_ BirthCertificate | Passport_ Passport deriving (Show, Eq, Ord)
 
 -- Тип данных для человека
 data Person = Person
-  { firstName :: String         -- Имя, должно быть непустым
-  , lastName :: String          -- Фамилия, должна быть непустой
-  , formerLastNames :: [String] -- Предыдущие фамилии, если фамилия менялась
-  , age :: Int                  -- Возраст, должен быть неотрицательным
-  , idNumber :: Maybe Document  -- Какое-то удостоверение личности
-  , parents :: ??? Person       -- Родители данного человека. Выбрать подходящий контейнер.
+  { firstName :: String                     -- Имя, должно быть непустым
+  , lastName :: String                      -- Фамилия, должна быть непустой
+  , formerLastNames :: [String]             -- Предыдущие фамилии, если фамилия менялась
+  , age :: Int                              -- Возраст, должен быть неотрицательным
+  , idDocument :: Document                  -- Какое-то удостоверение личности
+  , parents :: (Maybe Person, Maybe Person) -- Родители данного человека
   }
-  deriving (Show, Eq)
+  deriving (Show, Eq, Ord)
 
 -- Создание ребенка данных родителей
-createChild :: ??? Person -> Person
-createChild = undefined
+createChild :: Maybe Person -> Maybe Person -> String -> String -> Document -> Person -- Родитель 1, Родитель 2, Имя, Фамилия, Документ
+createChild (Just parent1) (Just parent2) firstName_ lastName_ document =
+  Person { firstName = firstName_
+  , lastName = lastName_
+  , formerLastNames = []
+  , age = 0
+  , idDocument = document
+  , parents = (Just parent1, Just parent2) }
+createChild (Just parent1) Nothing firstName_ lastName_ document =
+  Person { firstName = firstName_
+  , lastName = lastName_
+  , formerLastNames = []
+  , age = 0
+  , idDocument = document
+  , parents = (Just parent1, Nothing) }
+createChild Nothing (Just parent2) firstName_ lastName_ document =
+  Person { firstName = firstName_
+  , lastName = lastName_
+  , formerLastNames = []
+  , age = 0
+  , idDocument = document
+  , parents = (Nothing, Just parent2) }
+createChild Nothing Nothing _ _ _ = undefined
 
 -- Самый далекий предок данного человека.
 -- Если на одном уровне иерархии больше одного предка -- вывести самого старшего из них.
 -- Если на одном уровне иерархии больше одного предка одного максимального возраста -- вывести любого из них
 greatestAncestor :: Person -> Person
-greatestAncestor = undefined
+greatestAncestor person_ =
+  go person_ (parents person_) where
+    go person (Nothing, Nothing) = person
+    go _ (Just parent1, Nothing) = go parent1 (parents parent1)
+    go _ (Nothing, Just parent2) = go parent2 (parents parent2)
+    go _ (Just parent1, Just parent2) | age ancestor1 > age ancestor2 = ancestor1
+                              | otherwise = ancestor2 where
+                                ancestor1 = go parent1 (parents parent1)
+                                ancestor2 = go parent2 (parents parent2)
 
 -- Предки на одном уровне иерархии.
 ancestors :: Int -> Person -> Set.Set Person
-ancestors = undefined
+ancestors level person =
+  go level (Just person) Set.empty where
+    go 0 (Just person_) acc = Set.insert person_ acc
+    go _ (Nothing) acc = acc
+    go level_ (Just person_) acc = Set.union (go (level_ - 1) (fst (parents person_)) acc) (go (level_ - 1) (snd (parents person_)) acc)
 
 -- Возвращает семейное древо данного человека, описывающее его потомков.
-descendants :: Person -> Set.Set Person -> Tree Person
-descendants = undefined
+descendants :: Person -> Set.Set Person -> Tree
+-- Выбираем детей данного человека из множества всех людей, для них выполняем рекурсивно эту же функцию (ищем потомков)
+descendants person allPeople = Tree person (Set.map (`descendants` allPeople) (findChildren person allPeople)) where
+  findChildren person_ allPeople_ = Set.filter (isChild person_) allPeople_ where
+    isChild parent child =
+      go parent (parents child) where
+        go _ (Nothing, Nothing) = False
+        go parent_ (Just parent1, Nothing) = parent_ == parent1
+        go parent_ (Nothing, Just parent2) = parent_ == parent2
+        go parent_ (Just parent1, Just parent2) = parent_ == parent1 || parent_ == parent2
--- test/Test/Person.hs
+++ test/Test/Person.hs
@@ -2,3 +2,129 @@ module Test.Person where
 
 import Test.Tasty.HUnit (Assertion, (@?=), assertBool)
 import Person
+import qualified Data.Set as Set
+
+-- grandparent 1
+jenny :: Person
+jenny =
+  Person { firstName = "Jenny"
+         , lastName = "James"
+         , formerLastNames = []
+         , age = 71
+         , idDocument = Passport_ (Passport { passportSeries = 6298, passportNumber = 052017 })
+         , parents = (Nothing, Nothing) }
+
+-- grandparent 2 (greatestAncestor)
+david :: Person
+david =
+  Person { firstName = "David"
+         , lastName = "Bale"
+         , formerLastNames = []
+         , age = 81
+         , idDocument = Passport_ (Passport { passportSeries = 2091, passportNumber = 492081 })
+         , parents = (Nothing, Nothing) }
+
+-- grandparent 3
+slobodan :: Person
+slobodan =
+  Person { firstName = "Slobodan"
+         , lastName = "Blažić"
+         , formerLastNames = []
+         , age = 75
+         , idDocument = Passport_ (Passport { passportSeries = 1035, passportNumber = 205643 })
+         , parents = (Nothing, Nothing) }
+
+-- grandparent 4
+nadezda :: Person
+nadezda =
+  Person { firstName = "Nadezda"
+         , lastName = "Topalski"
+         , formerLastNames = []
+         , age = 74
+         , idDocument = Passport_ (Passport { passportSeries = 9346, passportNumber = 179345 })
+         , parents = (Nothing, Nothing) }
+
+-- parent 1
+sibi :: Person
+sibi =
+  Person { firstName = "Sibi"
+         , lastName = "Blažić"
+         , formerLastNames = []
+         , age = 52
+         , idDocument = Passport_ (Passport { passportSeries = 1234, passportNumber = 567890 })
+         , parents = (Just slobodan, Just nadezda) }
+
+-- parent 2
+christian :: Person
+christian =
+  Person { firstName = "Christian"
+         , lastName = "Bale"
+         , formerLastNames = []
+         , age = 48
+         , idDocument = Passport_ (Passport { passportSeries = 3567, passportNumber = 891011 })
+         , parents = (Just david, Just jenny) }
+
+
+-- child 1
+emmeline :: Person
+emmeline =
+  Person { firstName = "Emmeline"
+         , lastName = "Bale"
+         , formerLastNames = []
+         , age = 17
+         , idDocument = Passport_ (Passport { passportSeries = 5678, passportNumber = 910112 })
+         , parents = (Just sibi, Just christian) }
+
+-- child 2
+joseph :: Person
+joseph =
+  Person { firstName = "Joseph"
+         , lastName = "Bale"
+         , formerLastNames = []
+         , age = 8
+         , idDocument = BirthCertificate_ (BirthCertificate { birthCertSeries = "V5", birthCertNumber = 301802 })
+         , parents = (Just sibi, Just christian) }
+
+-- child 3
+michael :: Person
+michael =
+  Person { firstName = "Michael"
+         , lastName = "Bale"
+         , formerLastNames = []
+         , age = 0
+         , idDocument = BirthCertificate_ BirthCertificate { birthCertSeries = "M7", birthCertNumber = 201476 }
+         , parents = (Just sibi, Just christian)}
+
+unit_createChild = do
+  createChild (Just sibi) (Just christian) "Michael" "Bale" (BirthCertificate_ BirthCertificate { birthCertSeries = "M7", birthCertNumber = 201476 }) @?= michael
+
+unit_greatestAncestor = do
+  greatestAncestor joseph @?= david
+  greatestAncestor sibi @?= slobodan
+  greatestAncestor emmeline @?= david
+  greatestAncestor christian @?= david
+  greatestAncestor david @?= david
+
+unit_ancestors = do
+  ancestors 1 jenny @?= Set.empty
+  ancestors 1 sibi @?= Set.fromList [slobodan, nadezda]
+  ancestors 2 sibi @?= Set.empty
+  ancestors 1 christian @?= Set.fromList [david, jenny]
+  ancestors 2 christian @?= Set.empty
+  ancestors 1 emmeline @?= Set.fromList [sibi, christian]
+  ancestors 2 emmeline @?= Set.fromList [slobodan, nadezda, david, jenny]
+  ancestors 3 emmeline @?= Set.empty
+  ancestors 1 joseph @?= Set.fromList [sibi, christian]
+  ancestors 2 joseph @?= Set.fromList [slobodan, nadezda, david, jenny]
+  ancestors 3 joseph @?= Set.empty
+
+allPeople = Set.fromList [jenny, david, slobodan, nadezda, sibi, christian, emmeline, joseph, michael]
+
+unit_descendants = do
+  thisPerson (descendants david allPeople) @?= david
+  Set.size (allDescendants (descendants david allPeople)) @?= 1
+  Set.size (allDescendants (descendants slobodan allPeople)) @?= 1
+  thisPerson (descendants emmeline allPeople) @?= emmeline
+  Set.size (allDescendants (descendants emmeline allPeople)) @?= 0
+  Set.size (allDescendants (descendants sibi allPeople)) @?= 3
+  Set.size (allDescendants (descendants christian allPeople)) @?= 3
