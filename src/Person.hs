d5036ce15dbc10fd9d89d391a688ba8d39f70ccb
AliceGl <alice2003b@gmail.com>
2022-10-11 17:07:44 +0200

    done

--- src/Person.hs
+++ src/Person.hs
@@ -2,9 +2,12 @@ module Person where
 
 import qualified Data.Set as Set
 
-data Tree = ???
+data Tree a = Node a (Set.Set (Tree a))
+    deriving (Show, Eq, Ord)
 
-data Document = ???
+data Document = Passport (Int, Int)   -- Номер паспорта: состоит из серии и номера.
+    | BirthSertificate ([Char], Int)  -- Номер свидетельства о рождении: состоит из серии и номера.
+    deriving (Show, Eq, Ord)
 
 -- Тип данных для человека
 data Person = Person
@@ -13,24 +16,32 @@ data Person = Person
   , formerLastNames :: [String] -- Предыдущие фамилии, если фамилия менялась
   , age :: Int                  -- Возраст, должен быть неотрицательным
   , idNumber :: Maybe Document  -- Какое-то удостоверение личности
-  , parents :: ??? Person       -- Родители данного человека. Выбрать подходящий контейнер.
+  , parents :: Set.Set Person   -- Родители данного человека. Сет, так как порядок родителей нам не важен.
   }
-  deriving (Show, Eq)
+  deriving (Show, Eq, Ord)
 
 -- Создание ребенка данных родителей
-createChild :: ??? Person -> Person
-createChild = undefined
+createChild :: Set.Set Person -> String -> String -> Person
+createChild p f l = Person {firstName = f, lastName = l, formerLastNames = [],
+                                                          age = 0, idNumber = Nothing, parents = p}  -- При рождении у ребёнка ещё нет документа
 
 -- Самый далекий предок данного человека.
 -- Если на одном уровне иерархии больше одного предка -- вывести самого старшего из них.
 -- Если на одном уровне иерархии больше одного предка одного максимального возраста -- вывести любого из них
 greatestAncestor :: Person -> Person
-greatestAncestor = undefined
+greatestAncestor p = getLast (greatestAncestorRecursion p) -- Воспользуемся вспомогательной функцией, чтобы рекурсивно найти самого далекого предка
+          where getLast (_, _, c) = c
+
+greatestAncestorRecursion :: Person -> (Int, Int, Person)
+greatestAncestorRecursion p = if Set.null (parents p) then (0, -(age p), p)
+          else decreaseFirst (Set.findMin (Set.map greatestAncestorRecursion (parents p)))
+          where decreaseFirst (a, b, c) = (a - 1, b, c)
 
 -- Предки на одном уровне иерархии.
 ancestors :: Int -> Person -> Set.Set Person
-ancestors = undefined
+ancestors 0 p = Set.singleton p
+ancestors h p = Set.unions (Set.map (ancestors (h - 1)) (parents p))
 
 -- Возвращает семейное древо данного человека, описывающее его потомков.
 descendants :: Person -> Set.Set Person -> Tree Person
-descendants = undefined
+descendants p s = Node p (Set.map (`descendants` s) (Set.filter (Set.member p . parents) s))
--- test/Test/Person.hs
+++ test/Test/Person.hs
@@ -1,4 +1,198 @@
 module Test.Person where
 
+import qualified Data.Set as Set
 import Test.Tasty.HUnit (Assertion, (@?=), assertBool)
 import Person
+
+grandparent1 :: Person
+grandparent1 = 
+    Person { firstName = "Ivan"
+           , lastName = "Ivanov"
+           , formerLastNames = []
+           , age = 50
+           , idNumber = Just (Passport (4619, 672543))
+           , parents = Set.empty
+    }
+
+grandparent2 :: Person
+grandparent2 = 
+    Person { firstName = "Maria"
+           , lastName = "Ivanova"
+           , formerLastNames = []
+           , age = 48
+           , idNumber = Just (Passport (5039, 540577))
+           , parents = Set.empty
+    }
+
+grandparent3 :: Person
+grandparent3 = 
+    Person { firstName = "Petr"
+           , lastName = "Petrov"
+           , formerLastNames = []
+           , age = 59
+           , idNumber = Just (Passport (5346, 120358))
+           , parents = Set.empty
+    }
+
+grandparent4 :: Person
+grandparent4 = 
+    Person { firstName = "Daria"
+           , lastName = "Petrova"
+           , formerLastNames = []
+           , age = 61
+           , idNumber = Just (Passport (3520, 947767))
+           , parents = Set.empty
+    }
+
+parent1 :: Person
+parent1 = 
+    Person { firstName = "Vasily"
+           , lastName = "Vasilev"
+           , formerLastNames = []
+           , age = 51
+           , idNumber = Just (Passport (9146, 809405))
+           , parents = Set.empty
+    }
+
+parent2 :: Person
+parent2 = 
+    Person { firstName = "Artem"
+           , lastName = "Ivanov"
+           , formerLastNames = []
+           , age = 24
+           , idNumber = Just (Passport (6099, 905955))
+           , parents = Set.fromList [grandparent1, grandparent2]
+    }
+
+parent3 :: Person
+parent3 = 
+    Person { firstName = "Anna"
+           , lastName = "Ivanova"
+           , formerLastNames = []
+           , age = 30
+           , idNumber = Just (Passport (8910, 284189))
+           , parents = Set.fromList [grandparent1, grandparent2]
+    }
+
+parent4 :: Person
+parent4 = 
+    Person { firstName = "Tatiana"
+           , lastName = "Petrova"
+           , formerLastNames = []
+           , age = 35
+           , idNumber = Just (Passport (2701, 124948))
+           , parents = Set.fromList [grandparent3, grandparent4]
+    }
+
+parent5 :: Person
+parent5 = 
+    Person { firstName = "Terenty"
+           , lastName = "Petrov"
+           , formerLastNames = []
+           , age = 30
+           , idNumber = Just (Passport (6757, 504453))
+           , parents = Set.fromList [grandparent3, grandparent4]
+    }
+
+parent6 :: Person
+parent6 = 
+    Person { firstName = "Koza"
+           , lastName = "Sidorova"
+           , formerLastNames = []
+           , age = 23
+           , idNumber = Just (Passport (2402, 678882))
+           , parents = Set.empty
+    }
+
+parent7 :: Person
+parent7 = 
+    Person { firstName = "Alex"
+           , lastName = "Shmidt"
+           , formerLastNames = []
+           , age = 21
+           , idNumber = Just (Passport (9088, 932009))
+           , parents = Set.empty
+    }
+
+child1 :: Person
+child1 =
+    Person { firstName = "Ivan"
+           , lastName = "Vasilev"
+           , formerLastNames = []
+           , age = 0
+           , idNumber = Nothing
+           , parents = Set.fromList [parent1, parent2]
+    }
+
+child2 :: Person
+child2 =
+    Person { firstName = "Arina"
+           , lastName = "Ivanova"
+           , formerLastNames = []
+           , age = 0
+           , idNumber = Nothing
+           , parents = Set.fromList [parent3]
+    }
+
+child3 :: Person
+child3 =
+    Person { firstName = "Julia"
+           , lastName = "Petrova"
+           , formerLastNames = []
+           , age = 13
+           , idNumber = Just (BirthSertificate ("IV-ЖИ", 437920))
+           , parents = Set.fromList [parent4]
+    }
+
+child4 :: Person
+child4 =
+    Person { firstName = "Vasily"
+           , lastName = "Ivanov-Petrov"
+           , formerLastNames = []
+           , age = 4
+           , idNumber = Just (BirthSertificate ("II-ЗЧ", 255915))
+           , parents = Set.fromList [parent3, parent4]
+    }
+
+child5 :: Person
+child5 =
+    Person { firstName = "EAX-25"
+           , lastName = "Petrov"
+           , formerLastNames = []
+           , age = 0
+           , idNumber = Nothing
+           , parents = Set.fromList [parent5, parent6, parent7]
+    }
+
+people = Set.fromList [grandparent1, grandparent2, grandparent3, grandparent4, parent1, parent2, parent3
+                        , parent4, parent5, parent6, parent7, child1, child2, child3, child4, child5]
+
+tree1 = Node child5 Set.empty
+tree2 = Node parent5 (Set.singleton tree1)
+tree3 = Node parent4 (Set.fromList [Node child3 Set.empty, Node child4 Set.empty])
+tree4 = Node grandparent3 (Set.fromList [tree2, tree3])
+
+unit_createChild = do
+  createChild (Set.fromList [parent1, parent2]) "Ivan" "Vasilev" @?= child1
+  createChild (Set.fromList [parent3]) "Arina" "Ivanova" @?= child2
+  createChild (Set.fromList [parent5, parent6, parent7]) "EAX-25" "Petrov" @?= child5
+
+unit_greatestAncestor = do
+    greatestAncestor child2 @?= grandparent1
+    greatestAncestor child4 @?= grandparent4
+    greatestAncestor parent7 @?= parent7
+    greatestAncestor parent2 @?= grandparent1
+    greatestAncestor child1 @?= grandparent1
+
+unit_ancestors = do
+    ancestors 0 child3 @?= Set.fromList [child3]
+    ancestors 1 child3 @?= Set.fromList [parent4]
+    ancestors 2 child3 @?= Set.fromList [grandparent3, grandparent4]
+    ancestors 3 child3 @?= Set.empty
+    ancestors 2 child4 @?= Set.fromList [grandparent1, grandparent2, grandparent3, grandparent4]
+
+unit_descendants = do
+    descendants child5 people @?= tree1
+    descendants parent5 people @?= tree2
+    descendants parent4 people @?= tree3
+    descendants grandparent3 people @?= tree4
