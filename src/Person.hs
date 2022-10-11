fe019489a216563b9a33043c336dc2a2150fb08d
ArtemKar123 <wartemtem@yandex.ru>
2022-10-11 15:55:00 +0200

    hw5

--- src/Person.hs
+++ src/Person.hs
@@ -1,36 +1,142 @@
+{-# LANGUAGE InstanceSigs #-}
+
 module Person where
 
-import qualified Data.Set as Set
+import           Data.Foldable
+import           Data.Function
+import qualified Data.List     as List
+import qualified Data.Set      as Set
+import           Prelude
 
-data Tree = ???
+data Tree t
+  = Leaf t
+  | Node t (Set.Set (Tree t))
+  deriving (Show, Eq, Ord)
 
-data Document = ???
+data Document
+  = Adult
+      { passportNumber         :: (Int, Int)
+      , birthCertificateNumber :: (String, String, Int)
+      }
+  | Minor
+      { certificateNumber :: (String, String, Int) -- 2 заглавные латинские буквы, 2 заглавные буквы кирилицы, 6 цифр
+      }
+  deriving (Ord)
+
+data SurnameStrategy
+  = Parent1
+  | Parent2
+  | Combined
+  deriving (Enum)
+
+instance Show Document where
+  show :: Document -> String
+  show (Adult a b) = show a ++ show b
+  show (Minor x)   = show x
+
+instance Eq Document where
+  (==) :: Document -> Document -> Bool
+  (==) (Adult x1 y1) (Adult x2 y2) = x1 == x2 && y1 == y2
+  (==) (Minor x) (Minor y) = x == y
+  (==) (Adult _ xCertificateNumber) (Minor yCertificateNumber) =
+    xCertificateNumber == yCertificateNumber
+  (==) (Minor yCertificateNumber) (Adult _ xCertificateNumber) =
+    xCertificateNumber == yCertificateNumber
 
 -- Тип данных для человека
-data Person = Person
-  { firstName :: String         -- Имя, должно быть непустым
-  , lastName :: String          -- Фамилия, должна быть непустой
-  , formerLastNames :: [String] -- Предыдущие фамилии, если фамилия менялась
-  , age :: Int                  -- Возраст, должен быть неотрицательным
-  , idNumber :: Maybe Document  -- Какое-то удостоверение личности
-  , parents :: ??? Person       -- Родители данного человека. Выбрать подходящий контейнер.
-  }
-  deriving (Show, Eq)
+data Person =
+  Person
+    { firstName       :: String -- Имя, должно быть непустым
+    , lastName        :: String -- Фамилия, должна быть непустой
+    , formerLastNames :: [String] -- Предыдущие фамилии, если фамилия менялась
+    , age             :: Int -- Возраст, должен быть неотрицательным
+    , idNumber        :: Maybe Document -- Какое-то удостоверение личности
+    , parents         :: (Maybe Person, Maybe Person) -- Родители данного человека. Выбрать подходящий контейнер.
+    }
+  deriving (Show, Eq, Ord)
 
 -- Создание ребенка данных родителей
-createChild :: ??? Person -> Person
-createChild = undefined
+createChild ::
+     Maybe Person
+  -> Maybe Person
+  -> String
+  -> Maybe SurnameStrategy
+  -> Maybe Document
+  -> Person
+createChild p1 p2 name strat document =
+  Person
+    { firstName = name
+    , lastName = surnameHelper p1 p2 strat
+    , formerLastNames = []
+    , age = 0
+    , idNumber = documentHelper document
+    , parents = (p1, p2)
+    }
+  where
+    surnameHelper ::
+         Maybe Person -> Maybe Person -> Maybe SurnameStrategy -> String
+    surnameHelper a b _strat =
+      case (a, b) of
+        (Just x, Just y) ->
+          case _strat of
+            Nothing       -> lastName x ++ "-" ++ lastName y
+            Just Combined -> lastName x ++ "-" ++ lastName y
+            Just Parent1  -> lastName x
+            Just Parent2  -> lastName y
+        (Just x, Nothing) -> lastName x
+        (Nothing, Just y) -> lastName y
+        (Nothing, Nothing) -> "Ivanov" -- Let it be default surname, 'cause why not
+    documentHelper :: Maybe Document -> Maybe Document
+    documentHelper doc =
+      case doc of
+        (Just (Adult _ _certificateNumber)) -> Just (Minor _certificateNumber)
+        _                                   -> doc
 
 -- Самый далекий предок данного человека.
 -- Если на одном уровне иерархии больше одного предка -- вывести самого старшего из них.
 -- Если на одном уровне иерархии больше одного предка одного максимального возраста -- вывести любого из них
-greatestAncestor :: Person -> Person
-greatestAncestor = undefined
+emptyPerson :: Person
+emptyPerson = Person "" "" [] 0 Nothing (Nothing, Nothing)
+
+greatestAncestor :: Person -> (Person, Int)
+greatestAncestor person = helper (Just person) 0
+  where
+    helper :: Maybe Person -> Int -> (Person, Int)
+    helper Nothing _ = (emptyPerson, 0)
+    helper (Just p) acc =
+      case parents p of
+        (Nothing, Nothing) -> (p, acc)
+        (p1, p2) ->
+          maximumBy
+            (compare `on` (\(_person, _acc) -> (_acc, age _person)))
+            [helper p1 (acc + 1), helper p2 (acc + 1)]
 
 -- Предки на одном уровне иерархии.
 ancestors :: Int -> Person -> Set.Set Person
-ancestors = undefined
+ancestors lvl person = helper (Just person) lvl
+  where
+    helper :: Maybe Person -> Int -> Set.Set Person
+    helper Nothing _ = Set.empty
+    helper (Just _person) 0 = Set.singleton _person
+    helper (Just _person) l = do
+      let (p1, p2) = parents _person
+      Set.union (helper p1 (l - 1)) (helper p2 (l - 1))
 
 -- Возвращает семейное древо данного человека, описывающее его потомков.
-descendants :: Person -> Set.Set Person -> Tree Person
-descendants = undefined
+descendants :: Set.Set Person -> Person -> Tree Person
+descendants peopleSet person = do
+  case length childrenSet of
+    0 -> Leaf person
+    _ ->
+      Node
+        person
+        (Set.fromList (map (descendants peopleSet) (toList childrenSet)))
+  where
+    childrenSet = Set.filter isChild peopleSet
+    isChild :: Person -> Bool
+    isChild p =
+      case parents p of
+        (Just x, Just y)   -> person == x || person == y
+        (Nothing, Just y)  -> person == y
+        (Just x, Nothing)  -> person == x
+        (Nothing, Nothing) -> False
--- test/Test/Person.hs
+++ test/Test/Person.hs
@@ -1,4 +1,159 @@
 module Test.Person where
 
-import Test.Tasty.HUnit (Assertion, (@?=), assertBool)
-import Person
+import qualified Data.Set         as Set
+import           Person           (Document (..), Person (..),
+                                   SurnameStrategy (..), Tree (..), ancestors,
+                                   createChild, descendants, greatestAncestor)
+import           Test.Tasty.HUnit (assertBool)
+
+pGosling :: Person
+pGosling =
+  Person
+    { firstName = "Ryan"
+    , lastName = "Gosling"
+    , formerLastNames = []
+    , age = 41
+    , idNumber = Nothing
+    , parents = (Nothing, Nothing)
+    }
+
+pEmma :: Person
+pEmma =
+  Person
+    { firstName = "Emma"
+    , lastName = "Stone"
+    , formerLastNames = []
+    , age = 33
+    , idNumber = Nothing
+    , parents = (Nothing, Nothing)
+    }
+
+pBurunov :: Person
+pBurunov =
+  Person
+    { firstName = "Sergei"
+    , lastName = "Burunov"
+    , formerLastNames = []
+    , age = 45
+    , idNumber = Nothing
+    , parents = (Nothing, Nothing)
+    }
+
+pDr :: Person
+pDr =
+  Person
+    { firstName = "Doctor"
+    , lastName = "Katsuragi"
+    , formerLastNames = []
+    , age = 48
+    , idNumber = Nothing
+    , parents = (Nothing, Nothing)
+    }
+
+pMisato :: Person
+pMisato =
+  createChild
+    (Just pDr)
+    Nothing
+    "Misato"
+    Nothing
+    (Just (Minor ("XV", "АБ", 123456)))
+
+pMiron :: Person
+pMiron =
+  createChild
+    (Just pMisato)
+    (Just pGosling)
+    "Miron"
+    (Just Combined)
+    (Just (Minor ("IV", "ГБ", 123456)))
+
+pAnna :: Person
+pAnna =
+  createChild
+    (Just pEmma)
+    (Just pBurunov)
+    "Anna"
+    (Just Parent2)
+    (Just (Minor ("IV", "ГБ", 654321)))
+
+pIvan :: Person
+pIvan =
+  createChild
+    (Just pMiron)
+    (Just pAnna)
+    "Ivan"
+    (Just Parent1)
+    (Just (Minor ("IV", "ГБ", 000999)))
+
+pOleg :: Person
+pOleg =
+  createChild
+    (Just pMiron)
+    (Just pAnna)
+    "Oleg"
+    (Just Parent1)
+    (Just (Minor ("IV", "ГБ", 999000)))
+
+{-
+    Dr.Katsuragi
+        |
+        |
+Misato Katsuragi-----Rayan Gosling            Emma Stone-------Sergei Burunov
+                  |                                        |
+                  |                                        |
+                Miron Katsuragi-Gosling-----------------Anna Burunov
+                                                |
+                                                |
+                                               / \
+                                              /   \
+                                             /     \
+                                           Ivan   Oleg
+                                        Katsuragi-Gosling
+-}
+unit_lastnames = do
+  assertBool "single parent" (lastName pMisato == "Katsuragi")
+  assertBool "double surname" (lastName pMiron == "Katsuragi-Gosling")
+  assertBool "second parent surname" (lastName pAnna == "Burunov")
+  assertBool
+    "double surname transfered from p1 to children"
+    (lastName pMiron == "Katsuragi-Gosling")
+  assertBool "siblings have same surname" (lastName pIvan == lastName pOleg)
+
+unit_greatest_ancestor = do
+  assertBool "self" (greatestAncestor pDr == (pDr, 0))
+  assertBool "age based" (greatestAncestor pAnna == (pBurunov, 1))
+  assertBool "level based" (greatestAncestor pIvan == (pDr, 3))
+  assertBool "siblings" (greatestAncestor pIvan == greatestAncestor pOleg)
+
+unit_ancestors = do
+  assertBool "self" (ancestors 0 pDr == Set.fromList [pDr])
+  assertBool "too high" (ancestors 2 pMisato == Set.empty)
+  assertBool
+    "normal test"
+    (ancestors 2 pIvan == Set.fromList [pMisato, pGosling, pEmma, pBurunov])
+
+unit_tree = do
+  let allPeopleSet =
+        Set.fromList
+          [pGosling, pEmma, pBurunov, pDr, pMisato, pMiron, pAnna, pIvan, pOleg]
+  assertBool "self" (descendants allPeopleSet pIvan == Leaf pIvan)
+  assertBool
+    "1 level"
+    (descendants allPeopleSet pMiron ==
+     Node pMiron (Set.fromList [Leaf pIvan, Leaf pOleg]))
+  let (Node _ tree1) = descendants allPeopleSet pMiron
+  let (Node _ tree2) = descendants allPeopleSet pAnna
+  assertBool "parent results are equal" (tree1 == tree2)
+  assertBool
+    "big test"
+    (descendants allPeopleSet pDr ==
+     Node
+       pDr
+       (Set.fromList
+          [ Node
+              pMisato
+              (Set.fromList
+                 [Node pMiron (Set.fromList [Leaf pIvan, Leaf pOleg])])
+          ]))
+--   assertBool String Bool
