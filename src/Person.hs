81e8f7aef2aed67382a709dfb193d5e6f25b0cbb
Mark Ipatov <markthehopeful@gmail.com>
2022-10-12 22:31:44 +0200

    HW05 is done

--- src/Person.hs
+++ src/Person.hs
@@ -1,10 +1,15 @@
 module Person where
 
 import qualified Data.Set as Set
+import qualified Data.Maybe as Maybe
 
-data Tree = ???
+data Tree a = Empty
+            | Node a [Tree a]
+            deriving (Show, Eq, Ord)
 
-data Document = ???
+data Document = Passport (Int, Int)
+              | BirthCert Int
+              deriving (Show, Eq, Ord)
 
 -- Тип данных для человека
 data Person = Person
@@ -12,25 +17,42 @@ data Person = Person
   , lastName :: String          -- Фамилия, должна быть непустой
   , formerLastNames :: [String] -- Предыдущие фамилии, если фамилия менялась
   , age :: Int                  -- Возраст, должен быть неотрицательным
-  , idNumber :: Maybe Document  -- Какое-то удостоверение личности
-  , parents :: ??? Person       -- Родители данного человека. Выбрать подходящий контейнер.
-  }
-  deriving (Show, Eq)
+  , document :: Maybe Document  -- Какое-то удостоверение личности
+  , parents :: (Maybe Person, Maybe Person)       -- Родители данного человека. Пара это просто и относительно удобно, хоть и привносит проблемы с порядком родителей.
+  } -- We still need to somehow use persons without parents, i.e. in tests. Also we believe that there are only two parents for a child
+  deriving (Show, Eq, Ord) -- На самом деле с каждой правкой этот код всё больше становится пороховой бочкой, что держится на честном слове
+
+-- emptyDebugPerson :: Person
+-- emptyDebugPerson = Person {firstName = "Bad", lastName = "Design", formerLastNames = [], age = 0, document = Nothing, parents = (Nothing, Nothing)}
 
 -- Создание ребенка данных родителей
-createChild :: ??? Person -> Person
-createChild = undefined
+createChild :: Person -> Person -> String -> Person -- Parents and the name of the child
+createChild parent1 parent2 name = Person { firstName = name, lastName = (lastName parent1) ++ ('-' : (lastName parent2)), formerLastNames = [], age = 0, document = Nothing, parents = ((Just parent1), (Just parent2))}
 
 -- Самый далекий предок данного человека.
 -- Если на одном уровне иерархии больше одного предка -- вывести самого старшего из них.
 -- Если на одном уровне иерархии больше одного предка одного максимального возраста -- вывести любого из них
 greatestAncestor :: Person -> Person
-greatestAncestor = undefined
+greatestAncestor person = Maybe.fromMaybe undefined (snd (greatestAncestorWithLevel (1, Just person))) -- at least one person is present
+  where
+    greatestAncestorWithLevel :: (Int, Maybe Person) -> (Int, Maybe Person)
+    greatestAncestorWithLevel (_, Nothing) = (0, Nothing)
+    greatestAncestorWithLevel (n, Just person) = if (parents person) == (Nothing, Nothing) then (n, Just person) else max (greatestAncestorWithLevel (n + 1, fst (parents person))) (greatestAncestorWithLevel (n + 1, snd (parents person)))
 
 -- Предки на одном уровне иерархии.
 ancestors :: Int -> Person -> Set.Set Person
-ancestors = undefined
+ancestors level person = maybeAncestors level (Just person)
+
+maybeAncestors :: Int -> Maybe Person -> Set.Set Person
+maybeAncestors _ Nothing = Set.empty
+maybeAncestors 0 (Just person) = Set.singleton person
+maybeAncestors level (Just person) = Set.union (maybeAncestors (level - 1) (fst (parents person))) (maybeAncestors (level - 1) (snd (parents person)))
+
+isChildOf :: Person -> Person -> Bool
+isChildOf parent possibleChild = (fst (parents possibleChild)) == (Just parent) || (snd (parents possibleChild)) == (Just parent)
 
 -- Возвращает семейное древо данного человека, описывающее его потомков.
-descendants :: Person -> Set.Set Person -> Tree Person
-descendants = undefined
+-- Поменял порядок аргументов для использования descendants people в Set.map
+descendants :: Set.Set Person -> Person -> Tree Person
+descendants people person = Node person (Set.elems (Set.map (descendants people) (Set.filter (isChildOf person) people)))
+
--- test/Test/Person.hs
+++ test/Test/Person.hs
@@ -1,4 +1,48 @@
 module Test.Person where
 
+import qualified Data.Set as Set
 import Test.Tasty.HUnit (Assertion, (@?=), assertBool)
 import Person
+
+person0 :: Person
+person0 =
+  Person {firstName = "Oldest", lastName = "Unreal", formerLastNames = ["Real"], age = 6000000, document = Nothing, parents = (Nothing, Nothing)}
+
+person1 :: Person
+person1 = 
+  Person {firstName = "Test", lastName = "Sample", formerLastNames = [], age = 1337, document = Just (Passport (1234, 567890)), parents = (Just person0, Nothing)}
+
+
+person2 :: Person
+person2 = 
+  Person {firstName = "Check", lastName = "Text", formerLastNames = [], age = 800, document = Just (Passport (2233, 444000)), parents = (Nothing, Nothing)}
+
+child1_2_1 :: Person
+child1_2_1 =
+  Person {firstName = "Lorem", lastName = "Sample-Text", formerLastNames = [], age = 0, document = Nothing, parents = (Just person1, Just person2)}
+
+child1_2_2 :: Person
+child1_2_2 =
+  Person {firstName = "Ipsum", lastName = "Text-Sample", formerLastNames = [], age = 0, document = Nothing, parents = (Just person2, Just person1)}
+
+leute :: Set.Set Person
+leute = Set.fromList [person0, person1, person2, child1_2_1, child1_2_2]
+
+unit_createChild = do
+  createChild person1 person2 "Lorem" @?= child1_2_1
+  createChild person2 person1 "Ipsum" @?= child1_2_2
+
+unit_greatestAncestor = do
+  greatestAncestor child1_2_1 @?= person0
+  greatestAncestor (greatestAncestor child1_2_2) @?= person0
+
+unit_ancestors = do
+  ancestors 0 child1_2_1 @?= Set.fromList [child1_2_1]
+  ancestors 1 child1_2_1 @?= Set.fromList [person1, person2]
+  ancestors 2 child1_2_2 @?= Set.fromList [person0]
+
+
+unit_descendants = do
+  descendants leute child1_2_1 @?= Node child1_2_1 []
+  descendants leute child1_2_2 @?= Node child1_2_2 []
+  descendants leute person2 @?= Node person2 [(Node child1_2_2 []), (Node child1_2_1 [])]
