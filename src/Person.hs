c03af6f9cf452833f6c62ce2235fa7b165d545bd
RodionfromHSE <90181319+RodionfromHSE@users.noreply.github.com>
2022-10-12 18:09:08 +0200

    Done!

--- src/Person.hs
+++ src/Person.hs
@@ -1,10 +1,14 @@
+{-# LANGUAGE RecordWildCards #-}
 module Person where
 
 import qualified Data.Set as Set
 
-data Tree = ???
+data Tree a = Leaf {val :: a} | LeftNode {left :: Tree a, val :: a} | RightNode {val :: a, right :: Tree a}
+              | Node {left :: Tree a, val :: a, right :: Tree a} deriving (Show, Eq)
 
-data Document = ???
+data Document = BirthCert {id :: Int} | Passport {ser :: Int, num :: Int} deriving (Show, Eq)
+
+data Parents = Parents {father :: Maybe Person, mother :: Maybe Person} deriving (Show, Eq)
 
 -- Тип данных для человека
 data Person = Person
@@ -12,25 +16,51 @@ data Person = Person
   , lastName :: String          -- Фамилия, должна быть непустой
   , formerLastNames :: [String] -- Предыдущие фамилии, если фамилия менялась
   , age :: Int                  -- Возраст, должен быть неотрицательным
-  , idNumber :: Maybe Document  -- Какое-то удостоверение личности
-  , parents :: ??? Person       -- Родители данного человека. Выбрать подходящий контейнер.
+  , document :: Document        -- Какое-то удостоверение личности
+  , parents :: Parents       -- Родители данного человека. Выбрать подходящий контейнер.
   }
-  deriving (Show, Eq)
+  deriving (Eq)
+instance Show Person where
+  show Person{firstName = name} = name
+
+instance Ord Person where
+  (<=) per1 per2 = age per1 <= age per2
+
 
 -- Создание ребенка данных родителей
-createChild :: ??? Person -> Person
-createChild = undefined
+createChild :: Person -> Person -> String -> Document -> Person
+createChild father mother name doc@(BirthCert _) = Person {firstName = name, lastName = lastName father,
+                                                                formerLastNames = [], age = 0, document = doc,
+                                                                parents = Parents (Just father) (Just mother)}
+createChild _ _ _ _ = error "You should provide Birth Certificate for your child"
 
 -- Самый далекий предок данного человека.
 -- Если на одном уровне иерархии больше одного предка -- вывести самого старшего из них.
 -- Если на одном уровне иерархии больше одного предка одного максимального возраста -- вывести любого из них
 greatestAncestor :: Person -> Person
-greatestAncestor = undefined
+greatestAncestor person = snd (go (0,person)) where
+  go :: (Int, Person) -> (Int, Person)
+  go (n, Person {parents=(Parents (Just father) (Just mother))}) = max (go (n+1, father)) $ go (n+1, mother)
+  go (n, Person {parents=(Parents Nothing (Just mother))}) = go (n+1, mother)
+  go (n, Person {parents=(Parents (Just father) Nothing)}) = go (n+1, father)
+  go (n, person) = (n, person)
 
 -- Предки на одном уровне иерархии.
 ancestors :: Int -> Person -> Set.Set Person
-ancestors = undefined
+ancestors level per = Set.fromList $ go [] level (Just per) where
+  go :: [Person] -> Int -> Maybe Person -> [Person]
+  go acc 0 (Just per) = per : acc
+  go acc _ Nothing = acc
+  go acc n (Just per) = go acc (n-1) (father $ parents per) ++ go acc (n-1) (mother $ parents per)
 
 -- Возвращает семейное древо данного человека, описывающее его потомков.
-descendants :: Person -> Set.Set Person -> Tree Person
-descendants = undefined
+-- Me
+-- ├─ Father
+-- │  ├─ Grandpa
+-- │  └─ Granny
+-- └─ Mother
+descendants :: Person -> Tree Person
+descendants person@Person {parents=(Parents (Just father) (Just mother))} = Node (descendants father) person (descendants mother)
+descendants person@Person {parents=(Parents (Just father) Nothing)} = LeftNode (descendants father) person
+descendants person@Person {parents=(Parents Nothing (Just mother))} = RightNode person (descendants mother)
+descendants person = Leaf person
--- test/Test/Person.hs
+++ test/Test/Person.hs
@@ -1,4 +1,63 @@
+{-# LANGUAGE NumericUnderscores #-}
 module Test.Person where
 
 import Test.Tasty.HUnit (Assertion, (@?=), assertBool)
 import Person
+import qualified Data.Set as Set
+import Data.Maybe 
+
+
+createPerson :: String -> String -> Int -> Document -> Maybe Person -> Maybe Person -> Person
+createPerson firstName lastName age doc father mother = Person {
+    firstName = firstName,
+    lastName = lastName,
+    formerLastNames = [],
+    age = age,
+    document = doc,
+    parents = Parents father mother
+}
+
+myGrandMotherM = createPerson "myMother'sMother" "" 1000 (Passport 0001 1234_5678) Nothing Nothing 
+myGrandMotherF = createPerson "myFather'sMother" "" 90 (Passport 0002 1234_5678) Nothing Nothing 
+myGrandFatherM = createPerson "myMother'sFather" "" 999_999 (Passport 0003 1234_5678) Nothing Nothing 
+myGrandFatherF = createPerson "myFather'sFather" "" 91 (Passport 0004 1234_5678) Nothing Nothing 
+
+myFather = createPerson "myFather" "myFather'sSurname" 10 (Passport 0005 1234_0000) (Just myGrandFatherF) (Just myGrandMotherF) 
+myMother = createPerson "myMother" "myMother'sSurname" 20 (Passport 0006 1234_0000) (Just myGrandFatherM) (Just myGrandMotherM) 
+
+me = createPerson "me" "mySurname" 200 (Passport 0000 0000_0000) (Just myFather) (Just myMother)
+myPedigree = Node (Node (Leaf myGrandFatherF) myFather (Leaf myGrandMotherF)) me (Node (Leaf myGrandFatherM) myMother (Leaf myGrandMotherM))
+
+orphan = createPerson "lone" "butHappy" 7 (Passport 6666 6666_6666) Nothing Nothing
+
+hasOnlyFather = createPerson "name" "father'sSurname" 13 (Passport 6666 0001_0001) (Just myFather) Nothing
+hasOnlyFatherPedigree = LeftNode (Node (Leaf myGrandFatherF) myFather (Leaf myGrandMotherF)) hasOnlyFather
+hasOnlyMother = createPerson "myFather" "wentForCigarettes" 13 (Passport 6666 1001_1001) Nothing (Just myMother)
+
+son = createPerson "son" "" 21 (Passport 6688 0001_0001) (Just father') Nothing
+father' = createPerson "father" "" 21 (Passport 6688 1001_1001) Nothing Nothing
+sonFatherPedigree = LeftNode (Leaf father') son
+
+child :: Person
+child = createPerson "SmallAndSweety" "myFather'sSurname" 0 (BirthCert 123) (Just myFather) (Just myMother)
+
+
+unit_createChild = do
+    createChild myFather myMother "SmallAndSweety" (BirthCert 123) @?= child
+
+unit_greatestAncestor = do
+    greatestAncestor hasOnlyFather @?= myGrandFatherF
+    greatestAncestor hasOnlyMother @?= myGrandFatherM
+    greatestAncestor me @?= myGrandFatherM
+
+unit_ancestors = do
+    ancestors 2 me @?= Set.fromList [myGrandFatherF, myGrandMotherF, myGrandFatherM, myGrandMotherM]
+    ancestors 1 me @?= Set.fromList [myFather, myMother]
+    ancestors 0 me @?= Set.fromList [me]
+
+
+
+unit_descendants = do
+    descendants me @?= myPedigree
+    descendants hasOnlyFather @?= hasOnlyFatherPedigree
+    descendants son @?= sonFatherPedigree
