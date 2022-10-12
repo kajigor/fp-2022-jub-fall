d9a52d7f1d4fce7d3a5fca2c6498a7526cdb073e
alexflen <77319251+alexflen@users.noreply.github.com>
2022-10-12 21:13:58 +0200

    'done'

--- src/Person.hs
+++ src/Person.hs
@@ -2,9 +2,13 @@ module Person where
 
 import qualified Data.Set as Set
 
-data Tree = ???
+data Tree a = Tree
+  { element :: a
+  , children :: Set.Set (Tree a)
+  }
+  deriving (Show, Eq, Ord)
 
-data Document = ???
+data Document = BirthCertificate (Int, String, Int) deriving (Show, Eq, Ord)
 
 -- Тип данных для человека
 data Person = Person
@@ -13,24 +17,88 @@ data Person = Person
   , formerLastNames :: [String] -- Предыдущие фамилии, если фамилия менялась
   , age :: Int                  -- Возраст, должен быть неотрицательным
   , idNumber :: Maybe Document  -- Какое-то удостоверение личности
-  , parents :: ??? Person       -- Родители данного человека. Выбрать подходящий контейнер.
+  , parents :: (Maybe Person, Maybe Person) -- Родители данного человека. Выбрать подходящий контейнер.
   }
-  deriving (Show, Eq)
+  deriving (Show, Eq, Ord)
 
 -- Создание ребенка данных родителей
-createChild :: ??? Person -> Person
-createChild = undefined
+createChild :: (Maybe Person, Maybe Person) -> String -> Person
+createChild parents name =
+  case (fst parents) of
+      Nothing ->
+        case (snd parents) of
+          Nothing -> 
+            Person { firstName = name
+                   , lastName = "undefined"
+                   , formerLastNames = []
+                   , age = 0
+                   , idNumber = Just $ BirthCertificate (0, "aa", 000000)
+                   , parents = parents}
+          Just parent2 ->
+            Person { firstName = name
+                   , lastName = (lastName parent2)
+                   , formerLastNames = []
+                   , age = 0
+                   , idNumber = Just $ BirthCertificate (0, "aa", 000000)
+                   , parents = parents}
+      Just parent1 ->
+        Person { firstName = name
+                , lastName = (lastName parent1)
+                , formerLastNames = []
+                , age = 0
+                , idNumber = Just $ BirthCertificate (0, "aa", 000000)
+                , parents = parents} -- always take the first parent lastname
 
 -- Самый далекий предок данного человека.
 -- Если на одном уровне иерархии больше одного предка -- вывести самого старшего из них.
 -- Если на одном уровне иерархии больше одного предка одного максимального возраста -- вывести любого из них
+
+deeperAndOlder :: (Person, Int) -> (Person, Int) -> (Person, Int)
+deeperAndOlder pair1 pair2 = 
+  if ((snd pair1) < (snd pair2) || ((snd pair1) == (snd pair2) && (age $ fst $ pair1) > (age $ fst $ pair2)))
+    then pair1
+    else pair2
+
 greatestAncestor :: Person -> Person
-greatestAncestor = undefined
+greatestAncestor person = fst $ greatestAncestorWithHeight person 0
+  where 
+    greatestAncestorWithHeight :: Person -> Int -> (Person, Int)
+    greatestAncestorWithHeight person height =
+      case (fst $ parents $ person) of
+        Nothing ->
+          case (snd $ parents $ person) of
+            Nothing -> (person, 0)
+            Just parent2 -> greatestAncestorWithHeight parent2 (height + 1)
+        Just parent1 ->
+          case (snd $ parents $ person) of
+            Nothing -> greatestAncestorWithHeight parent1 (height + 1)
+            Just parent2 -> deeperAndOlder (greatestAncestorWithHeight parent1 (height + 1)) (greatestAncestorWithHeight parent2 (height + 1))
 
 -- Предки на одном уровне иерархии.
 ancestors :: Int -> Person -> Set.Set Person
-ancestors = undefined
+ancestors level person = ancestorsWithHeight person 0 level
+  where 
+    ancestorsWithHeight :: Person -> Int -> Int -> Set.Set Person
+    ancestorsWithHeight person height level =
+      if height == level 
+        then Set.singleton person
+      else 
+        case (fst $ parents $ person) of
+        Nothing ->
+          case (snd $ parents $ person) of
+            Nothing -> Set.empty
+            Just parent2 -> ancestorsWithHeight parent2 (height + 1) level
+        Just parent1 ->
+          case (snd $ parents $ person) of
+            Nothing -> ancestorsWithHeight parent1 (height + 1) level
+            Just parent2 -> Set.union (ancestorsWithHeight parent1 (height + 1) level) (ancestorsWithHeight parent2 (height + 1) level)
+
 
 -- Возвращает семейное древо данного человека, описывающее его потомков.
 descendants :: Person -> Set.Set Person -> Tree Person
-descendants = undefined
+descendants person peopleSet = Tree person (Set.map descendantsWithoutSet (Set.filter isParent peopleSet))
+  where
+    isParent :: Person -> Bool
+    isParent probablyChild = ((fst $ parents $ probablyChild) == (Just $ person)) || ((snd $ parents $ probablyChild) == (Just $ person))
+    descendantsWithoutSet :: Person -> Tree Person 
+    descendantsWithoutSet person1 = descendants person1 peopleSet 
--- test/Test/Person.hs
+++ test/Test/Person.hs
@@ -1,4 +1,122 @@
 module Test.Person where
 
+import qualified Data.Set as Set
 import Test.Tasty.HUnit (Assertion, (@?=), assertBool)
 import Person
+
+person1 :: Person
+person1 =
+  Person { firstName = "Gosha"
+         , lastName = "Ivanov"
+         , formerLastNames = []
+         , age = 29
+         , idNumber = Just $ BirthCertificate (1, "aa", 567890)
+         , parents = (Nothing, Nothing) }
+
+person2 :: Person
+person2 =
+  Person { firstName = "Maria"
+         , lastName = "Vernova"
+         , formerLastNames = []
+         , age = 30
+         , idNumber = Just $ BirthCertificate (1, "ab", 399393)
+         , parents = (Nothing, Nothing) }
+
+person12child :: Person
+person12child =
+  Person { firstName = "Misha"
+         , lastName = "Ivanov"
+         , formerLastNames = []
+         , age = 0
+         , idNumber = Just $ BirthCertificate (0, "aa", 000000)
+         , parents = (Just $ person1, Just $ person2) }
+
+person21child :: Person
+person21child =
+  Person { firstName = "Katya"
+         , lastName = "Vernova"
+         , formerLastNames = []
+         , age = 0
+         , idNumber = Just $ BirthCertificate (0, "aa", 000000)
+         , parents = (Just $ person2, Just $ person1) }
+
+personGranny :: Person
+personGranny =
+  Person { firstName = "Liza"
+         , lastName = "Nekovich"
+         , formerLastNames = []
+         , age = 68
+         , idNumber = Just $ BirthCertificate (2, "ss", 768493)
+         , parents = (Nothing, Nothing) }
+
+person3 :: Person
+person3 =
+  Person { firstName = "Yulia"
+         , lastName = "Nekovich"
+         , formerLastNames = []
+         , age = 42
+         , idNumber = Just $ BirthCertificate (9, "kf", 289389)
+         , parents = (Just $ personGranny, Nothing) }
+
+person4 :: Person
+person4 =
+  Person { firstName = "Yuriy"
+         , lastName = "Belchenko"
+         , formerLastNames = []
+         , age = 41
+         , idNumber = Just $ BirthCertificate (7, "dh", 238495)
+         , parents = (Nothing, Nothing) }
+
+person_3child :: Person
+person_3child =
+  Person { firstName = "Sasha"
+         , lastName = "Nekovich"
+         , formerLastNames = []
+         , age = 0
+         , idNumber = Just $ BirthCertificate (0, "aa", 000000)
+         , parents = (Nothing, Just $ person3) }
+
+person4_child :: Person
+person4_child =
+  Person { firstName = "Lilia"
+         , lastName = "Belchenko"
+         , formerLastNames = []
+         , age = 0
+         , idNumber = Just $ BirthCertificate (0, "aa", 000000)
+         , parents = (Just $ person4, Nothing) }
+
+person4__3child :: Person
+person4__3child =
+  Person { firstName = "Eugene"
+         , lastName = "Belchenko"
+         , formerLastNames = []
+         , age = 0
+         , idNumber = Just $ BirthCertificate (0, "aa", 000000)
+         , parents = (Just $ person4_child, Just $ person_3child) }
+
+allPeople :: Set.Set Person
+allPeople = Set.fromList [person1, person2, person3, person4, person12child, person21child, person4_child, person_3child, person4__3child, personGranny]
+
+unit_createChild = do
+  createChild (Just $ person1, Just $ person2) "Misha" @?= person12child
+  createChild (Just $ person2, Just $ person1) "Katya" @?= person21child
+  createChild (Nothing, Just $ person3) "Sasha" @?= person_3child
+  createChild (Just $ person4, Nothing) "Lilia" @?= person4_child
+  createChild (Just $ person4_child, Just $ person_3child) "Eugene" @?= person4__3child
+
+unit_greatestAncestor = do
+    greatestAncestor person4__3child @?= personGranny
+    greatestAncestor person4_child @?= person4
+    greatestAncestor person_3child @?= personGranny
+    greatestAncestor person12child @?= person2
+
+unit_ancestors = do
+    ancestors 1 person12child @?= Set.fromList [person1, person2]
+    ancestors 2 person4__3child @?= Set.fromList [person3, person4]
+    ancestors 3 person4__3child @?= Set.fromList [personGranny]
+
+unit_descendants = do
+    descendants person12child (allPeople) @?= Tree person12child Set.empty
+    descendants person1 (allPeople) @?= Tree person1 (Set.fromList [(Tree person12child Set.empty), (Tree person21child Set.empty)])
+    descendants personGranny (allPeople) @?= Tree personGranny (Set.fromList [Tree person3 (Set.fromList [Tree person_3child (Set.fromList [Tree person4__3child Set.empty])])])
+    descendants person4 (allPeople) @?= Tree person4 (Set.fromList [Tree person4_child (Set.fromList [Tree person4__3child Set.empty])])
