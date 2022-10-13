4a007ffe4589ca8f6a195835eaacaf15a79130cf
Kirill Ivanov <kirdmiv@users.noreply.github.com>
2022-10-13 04:30:28 +0300

    Done

--- src/Person.hs
+++ src/Person.hs
@@ -2,9 +2,12 @@ module Person where
 
 import qualified Data.Set as Set
 
-data Tree = ???
+data Tree a = Tree { node :: a, children :: [Tree a] } deriving (Show, Eq)
 
-data Document = ???
+instance Ord a => Ord (Tree a) where
+  tree1 <= tree2 = node tree1 <= node tree2
+
+data Document = Passport { series :: Int, number :: Int } | BirthCertificate { series_number :: Int } deriving (Show, Eq)
 
 -- Тип данных для человека
 data Person = Person
@@ -13,24 +16,67 @@ data Person = Person
   , formerLastNames :: [String] -- Предыдущие фамилии, если фамилия менялась
   , age :: Int                  -- Возраст, должен быть неотрицательным
   , idNumber :: Maybe Document  -- Какое-то удостоверение личности
-  , parents :: ??? Person       -- Родители данного человека. Выбрать подходящий контейнер.
-  }
-  deriving (Show, Eq)
+  , parents :: (Maybe Person, Maybe Person) -- Родители данного человека. Считаем, что родителей всегда 2, так говорили на уроках биологии. Причем некоторых может не быть в системе, поэтому Maybe.
+  } deriving (Show, Eq)
+
+instance Ord Person where
+  person1 <= person2 = age person1 <= age person2
+
+-- instance Eq Person where
+--   person1 == person2 | idNumber person1 == Nothing && idNumber person2 == Nothing = 
+--                                                 firstName person1 ++ lastName person1 == firstName person2 ++ lastName person2
+--                      | idNumber person1 == Nothing = False
+--                      | idNumber person2 == Nothing = False
+--                      | otherwise = idNumber person1 == idNumber person2
 
 -- Создание ребенка данных родителей
-createChild :: ??? Person -> Person
-createChild = undefined
+-- считаем, что основной кейс -- рождение человека, документов пока нет. Должны быть другие функции, меняющин ту или иную характеристику.
+createChild :: String -> String -> Maybe Person -> Maybe Person -> Person
+createChild firstName lastName firstParent secondParent =
+  Person {
+    firstName = firstName,
+    lastName = lastName,
+    formerLastNames = [],
+    age = 0,
+    idNumber = Nothing,
+    parents = (firstParent, secondParent)
+  }
 
 -- Самый далекий предок данного человека.
 -- Если на одном уровне иерархии больше одного предка -- вывести самого старшего из них.
 -- Если на одном уровне иерархии больше одного предка одного максимального возраста -- вывести любого из них
 greatestAncestor :: Person -> Person
-greatestAncestor = undefined
+greatestAncestor person = snd (greatestAncestor' (0, person) (0, person)) where
+  greatestAncestor' (x, person1) (y, person2) | parents person2 == (Nothing, Nothing) && x > y = (x, person1)
+                                              | parents person2 == (Nothing, Nothing) && x < y = (y, person2)
+                                              | parents person2 == (Nothing, Nothing) && age person1 > age person2 = (x, person1)
+                                              | parents person2 == (Nothing, Nothing) = (y, person2)
+  greatestAncestor' (x, person1) (y, Person { parents = (Just parent, Nothing) })
+                                      = greatestAncestor' (x, person1) (y + 1, parent)
+  greatestAncestor' (x, person1) (y, Person { parents = (Nothing, Just parent) })
+                                      = greatestAncestor' (x, person1) (y + 1, parent)
+  greatestAncestor' (x, person1) (y, Person { parents = (Just parent1, Just parent2) })
+                                      = greatestAncestor' (greatestAncestor' (x, person1) (y + 1, parent1)) (y + 1, parent2)
+
 
 -- Предки на одном уровне иерархии.
 ancestors :: Int -> Person -> Set.Set Person
-ancestors = undefined
+ancestors level person = ancestors' level (Just person) Set.empty where
+  ancestors' _ Nothing result = result
+  ancestors' 0 (Just person) result = Set.insert person result
+  ancestors' level (Just person) result = ancestors' (level - 1) (fst (parents person)) (ancestors' (level - 1) (snd (parents person)) result)
 
 -- Возвращает семейное древо данного человека, описывающее его потомков.
-descendants :: Person -> Set.Set Person -> Tree Person
-descendants = undefined
+descendants :: Set.Set Person -> Person -> Tree Person
+descendants persons person = Tree {
+                               node = person,
+                               children = Set.elems (Set.map (descendants persons) (Set.filter (isChild person) persons))
+                             }
+
+sameAs :: Maybe Person -> Person -> Bool
+sameAs Nothing _ = False
+sameAs (Just p1) p2 = p1 == p2
+
+isChild :: Person -> Person -> Bool
+isChild parent Person { parents = (p1, p2) } | p1 `sameAs` parent || p2 `sameAs` parent = True
+                                               | otherwise = False
--- test/Test/Person.hs
+++ test/Test/Person.hs
@@ -2,3 +2,80 @@ module Test.Person where
 
 import Test.Tasty.HUnit (Assertion, (@?=), assertBool)
 import Person
+import Data.Set
+
+createPassport :: Int -> Int -> Document
+createPassport s n = Passport { series = s, number = n }
+
+createBirtCert :: Int -> Document
+createBirtCert sn = BirthCertificate { series_number = sn }
+
+createPerson :: String -> String -> Int -> Maybe Document -> (Maybe Person, Maybe Person) -> Person
+createPerson name surname age doc parents = Person {
+                                                firstName = name,
+                                                lastName = surname,
+                                                formerLastNames = [],
+                                                age = age,
+                                                idNumber = doc,
+                                                parents = parents
+                                            }
+
+kirill :: Person
+kirill = createPerson "Kirill" "Ivanov" 19 (Just (createPassport 1 1)) (Nothing, Nothing)
+masha :: Person
+masha = createPerson "Masha" "Ivanchura" 20 (Just (createPassport 1 2)) (Nothing, Nothing)
+mark :: Person
+mark = createPerson "Mark" "Prudnikov" 54 (Just (createPassport 1 3)) (Nothing, Nothing)
+petr :: Person
+petr = createPerson "Peter" "Tsvetkov" 100500 (Just (createPassport 1 3)) (Nothing, Nothing)
+sasha :: Person
+sasha = createPerson "Sanya" "Tukmachev" 12 (Just (createPassport 1 1111)) (Just kirill, Just masha)
+ivan :: Person
+ivan = createPerson "Ivan" "Orlov" 121 (Just (createPassport 1212 21)) (Just mark, Just petr)
+t1 :: Person
+t1 = createPerson "Test" "One" 13 (Just (createPassport 122 21)) (Nothing, Just ivan)
+t2 :: Person
+t2 = createPerson "Test" "two" 14 (Just (createPassport 122 22)) (Nothing, Just ivan)
+t3 :: Person
+t3 = createPerson "Tset" "Aboba" 5 (Just (createPassport 133322 22)) (Just t1, Just t2)
+kid :: Person
+kid = createPerson "Ilya" "Kid" 0 Nothing (Just kirill, Just masha)
+
+unit_createChild :: Assertion
+unit_createChild = do
+    assertBool "Child!" (createChild "Ilya" "Kid" (Just kirill) (Just masha) == kid)
+
+unit_greatestAncestor :: IO ()
+unit_greatestAncestor = do
+    greatestAncestor kirill @?= kirill
+    greatestAncestor ivan @?= petr
+    greatestAncestor sasha @?= masha
+    greatestAncestor t1 @?= petr
+    greatestAncestor t3 @?= petr
+    greatestAncestor kid @?= masha
+
+unit_ancestors :: IO ()
+unit_ancestors = do
+    ancestors 0 kirill @?= fromList [kirill]
+    ancestors 1 sasha @?= fromList [kirill, masha]
+    ancestors 2 t1 @?= fromList [petr, mark]
+    ancestors 3 t3 @?= fromList [petr, mark]
+    ancestors 1000 kirill @?= fromList []
+
+persons :: Set Person
+persons = fromList [kirill, masha, sasha, petr, ivan, mark, t1, t2, t3, kid]
+
+unit_descendants :: IO ()
+unit_descendants = do
+    descendants persons t3 @?= Tree { node = t3, children = [] }
+    descendants persons kirill @?= Tree {
+        node = kirill,
+        children = [Tree {node = kid, children = []}, Tree {node = sasha, children = []}]
+    }    
+    descendants persons petr @?= Tree {
+        node = petr,
+        children = [Tree {node = ivan, children = [
+            Tree { node = t1, children=[Tree {node = t3, children = []}] },
+            Tree { node = t2, children=[Tree {node = t3, children = []}] }
+        ]}]
+    }
