3bd4fdcbf6a36bcdda0d5b2ca180f06519c73b8b
Timur05032003 <t.malanin@gmail.com>
2022-10-13 20:31:12 +0200

    done

--- src/Person.hs
+++ src/Person.hs
@@ -1,10 +1,13 @@
 module Person where
 
 import qualified Data.Set as Set
+import Data.Maybe
 
-data Tree a = Leaf a | OneChild a (Tree a) | TwoChildren a (Tree a) (Tree a) deriving (Show, Eq)
+data Tree a = Node a (Set.Set (Tree a))
+              deriving(Show, Eq, Ord)
 
-data Document = Passport (Int, Int) | Bc (Int, String) deriving (Show, Eq)
+
+data Document = Passport (Int, Int) | Bc (Int, String) deriving (Show, Eq, Ord)
 
 -- Тип данных для человека
 data Person = Person
@@ -15,29 +18,34 @@ data Person = Person
   , idNumber :: Maybe Document  -- Какое-то удостоверение личности
   , parents :: (Maybe Person, Maybe Person)       -- Родители данного человека. Выбрать подходящий контейнер.
   }
-  deriving (Show, Eq)
+  deriving (Show, Eq, Ord)
 
 -- Создание ребенка данных родителей
-createChild :: (Maybe Person, Maybe Person) Person -> String -> Person
+createChild :: (Maybe Person, Maybe Person) -> String -> Person
 createChild (Just father, Just mother) name = Person name (lastName father) [] 0 Nothing (Just father, Just mother)
 createChild (Just father, Nothing) name = Person name (lastName father) [] 0 Nothing (Just father, Nothing)
 createChild (Nothing, Just mother) name = Person name (lastName mother) [] 0 Nothing (Nothing, Just mother)
-createChild (Nothing, Nothing) name = Person name "Sirota" [] 0 Nothing Nothing
+createChild (Nothing, Nothing) name = Person name "Sirota" [] 0 Nothing (Nothing, Nothing)
 
--- Самый далекий предок данного человека.
--- Если на одном уровне иерархии больше одного предка -- вывести самого старшего из них.
--- Если на одном уровне иерархии больше одного предка одного максимального возраста -- вывести любого из них
-greatestAncestor :: Person -> Person
-greatestAncestor = undefined
+-- -- Самый далекий предок данного человека.
+-- -- Если на одном уровне иерархии больше одного предка -- вывести самого старшего из них.
+-- -- Если на одном уровне иерархии больше одного предка одного максимального возраста -- вывести любого из них
+-- greatestAncestor :: Person -> Person
 
--- Предки на одном уровне иерархии.
-ancestors :: Int -> Person -> Set.Set Person
-ancestors = undefined
+
+
+-- -- Предки на одном уровне иерархии.
+-- ancestors :: Int -> Person -> Set.Set Person
+
+
+isChild :: Person -> Person -> Bool
+isChild person child = fst (parents child) == Just person || snd (parents child) == Just person
+
 
 -- Возвращает семейное древо данного человека, описывающее его потомков.
-descendants :: Person -> Tree Person
-descendants person = case parents person of
-  (Nothing, Nothing) -> Leaf person
-  (Just father, Nothing) -> OneChild person (descendants father)
-  (Nothing, Just mother) -> OneChild person (descendants mother)
-  (Just father, Just mother) -> TwoChildren person (descendants father) (descendants mother)
+descendants :: Set.Set Person -> Person -> Tree Person
+descendants people person =
+    let children = Set.filter (isChild person) people in
+        if (null children)
+            then Node person Set.empty
+        else Node person (Set.map (descendants people) children)
--- test/Test/Person.hs
+++ test/Test/Person.hs
@@ -1,4 +1,73 @@
 module Test.Person where
-
 import Test.Tasty.HUnit (Assertion, (@?=), assertBool)
 import Person
+import qualified Data.Set as Set
+
+
+person1 :: Person
+person1 =
+  Person { firstName = "Father"
+         , lastName = "Papa"
+         , formerLastNames = []
+         , age = 29
+         , idNumber = Just (Passport (123456, 123456))
+         , parents = (Nothing, Nothing) }
+
+person2 :: Person
+person2 =
+  Person { firstName = "Mother"
+         , lastName = "Mama"
+         , formerLastNames = []
+         , age = 30
+         , idNumber = Just (Passport (1234, 1234))
+         , parents = (Nothing, Nothing) }
+
+
+person12child :: Person
+person12child =
+  Person { firstName = "Natasha"
+         , lastName = "Papa"
+         , formerLastNames = []
+         , age = 0
+         , idNumber = Nothing
+         , parents = (Just person1, Just person2) }
+
+childNoParents :: Person
+childNoParents =
+  Person { firstName = "Masha"
+         , lastName = "Sirota"
+         , formerLastNames = []
+         , age = 0
+         , idNumber = Nothing
+         , parents = (Nothing, Nothing) }
+
+
+person3 :: Person
+person3 =
+  Person { firstName = "Oleg"
+         , lastName = "Neoleg"
+         , formerLastNames = []
+         , age = 14
+         , idNumber = Just (Passport (1234, 1234))
+         , parents = (Nothing, Just person1) }
+
+person4 :: Person
+person4 =
+  Person { firstName = "Sasha"
+         , lastName = "Polyak"
+         , formerLastNames = []
+         , age = 14
+         , idNumber = Just (Passport (1234, 1234))
+         , parents = (Nothing, Nothing) }
+
+unit_createChild = do
+  let child' = createChild (Just person1, Just person2) "Natasha"
+  child' @?= person12child
+  let childNoParents' = createChild (Nothing, Nothing) "Masha"
+  childNoParents' @?= childNoParents
+
+people = Set.fromList [person1, person2, person3, person4,
+                       person12child, childNoParents]
+
+unit_descendants = do
+  descendants people person3 @?= Node person3 Set.empty
