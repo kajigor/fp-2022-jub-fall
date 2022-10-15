030c59bbb5571da1f69aa8d91ca2089272d0e99a
EgorShibaev <mister.egor03@gmail.com>
2022-10-15 14:13:44 +0200

    done

--- src/Shape.hs
+++ src/Shape.hs
@@ -15,11 +15,11 @@ data PointT = PointD Double Double
 -- * mconcat foldr (<>) mempty
 instance Monoid PointT where
   mempty :: PointT
-  mempty = undefined
+  mempty = PointD 0 0
 
 instance Semigroup PointT where
   (<>) :: PointT -> PointT -> PointT
-  (<>) = undefined
+  (<>) (PointD x1 y1) (PointD x2 y2) = PointD (x1 + x2) (y1 + y2)
 
 -- Фигуры
 data Shape = Circle PointT Double    -- Круг характеризуется координатой центра и радиусом
@@ -30,14 +30,21 @@ data Shape = Circle PointT Double    -- Круг характеризуется 
 -- Передвигает фигуру на x по горизонтали и на y по вертикали
 -- Реализовать, используя то, что PointT -- моноид
 slideShape :: Shape -> PointT -> Shape
-slideShape = undefined
+slideShape (Circle c r) p = Circle (c <> p) r
+slideShape (Rectangle p1 p2) p = Rectangle (p1 <> p) (p2 <> p)
+slideShape (Overlay s1 s2) p = Overlay (slideShape s1 p) (slideShape s2 p)
 
 -- Второй аргумент задает последовательность сдвигов фигуры.
 moveShapeAround :: Shape -> [PointT] -> Shape
-moveShapeAround = undefined
+moveShapeAround shape lst = slideShape shape $ foldr (<>) mempty lst
 
 -- Является ли Shape полугруппой? А моноидом?
 -- Реализовать инстансы, если является. Иначе -- обосновать.
+-- Единственная операция, на которую у меня хватает вображения:
+-- (<>) s1 s2 = Overlay s1 s2
+-- но она не ассоциативна, так как 
+-- (rect1 <> rect2) <> rec3 = Overlay (Overlay rect1 rect2) rect3
+-- rect1 <> (rect2 <> rec3) = Overlay rect1 (Overlay rect2 rect3)
 instance Semigroup Shape where
   (<>) :: Shape -> Shape -> Shape
   (<>) = undefined
--- src/SortedList.hs
+++ src/SortedList.hs
@@ -8,8 +8,12 @@ data SortedList a = SortedList { getSortedList :: [a] }
 -- <> должен сохранять отсортированность списка
 instance Ord a => Semigroup (SortedList a) where
   (<>) :: SortedList a -> SortedList a -> SortedList a
-  (<>) = undefined
+  (<>) sl1 sl2 = case (getSortedList sl1, getSortedList sl2) of
+    (l, []) -> SortedList l
+    ([], l) -> SortedList l
+    (list1@(h1:xs1), list2@(h2:xs2)) | h1 < h2 -> SortedList $ h1 : (getSortedList $ SortedList xs1 <> SortedList list2)
+                                     | otherwise -> SortedList $ h2 : (getSortedList $ SortedList list1 <> SortedList xs2)
 
 instance Ord a => Monoid (SortedList a) where
   mempty :: SortedList a
-  mempty = undefined
+  mempty = SortedList []
