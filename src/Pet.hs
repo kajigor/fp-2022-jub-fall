7eedfbc9865603bc1d44e37f25d998f51a419675
Mark Ipatov <markthehopeful@gmail.com>
2022-10-17 23:24:19 +0200

    HW06 is done

--- src/Pet.hs
+++ src/Pet.hs
@@ -96,10 +96,16 @@ data Pet = Pet
   }
   deriving (Show, Eq)
 
+comparePerson :: Person -> Person -> Ordering
+comparePerson = (compare `on` lastName) <> (compare `on` firstName) <> (compare `on` identification)
+
+comparePets :: Pet -> Pet -> Ordering
+comparePets = (comparePerson `on` owner) <> (compare `on` species) <> (compare `on` name)
+
 -- Отсортируйте питомцев по их хозяевам:
 -- * Хозяева упорядочиваются сначала по фамилии, затем по имени.
 -- * В случае, если два человека -- тезки, упорядочиваем по номеру документа.
 -- * Если у одного хозяина больше одного питомца, сортируйте их сначала по типу, потом по имени.
 -- Сортировку стоит делать при помощи функции sortBy из Data.List
 sortPets :: [Pet] -> [Pet]
-sortPets = undefined
+sortPets = sortBy comparePets
--- src/Shape.hs
+++ src/Shape.hs
@@ -15,32 +15,45 @@ data PointT = PointD Double Double
 -- * mconcat foldr (<>) mempty
 instance Monoid PointT where
   mempty :: PointT
-  mempty = undefined
+  mempty = PointD 0 0
 
 instance Semigroup PointT where
   (<>) :: PointT -> PointT -> PointT
-  (<>) = undefined
+  (<>) (PointD x1 y1) (PointD x2 y2) = PointD (x1 + x2) (y1 + y2)
 
 -- Фигуры
 data Shape = Circle PointT Double    -- Круг характеризуется координатой центра и радиусом
            | Rectangle PointT PointT -- Прямоугольник со сторонами, параллельными координатным осям, характеризуется координатами двух углов
            | Overlay Shape Shape     -- Фигура, получающаяся наложением друг на друга двух других фигур
-           deriving (Show, Eq)
+           deriving (Show)
+
+asArrayOfPrimitives :: Shape -> [Shape]
+asArrayOfPrimitives (Overlay shape1 shape2) = (asArrayOfPrimitives shape1) ++ (asArrayOfPrimitives shape2) -- ineffective :(
+asArrayOfPrimitives x = [ x ]
+
+instance Eq Shape where
+  (==) (Circle point1 radius1) (Circle point2 radius2) = (point1 == point2) && (radius1 == radius2)
+  (==) (Rectangle point11 point12) (Rectangle point21 point22) = (point11 == point21) && (point12 == point22)
+  (==) (Overlay shape11 shape12) (Overlay shape21 shape22) = (asArrayOfPrimitives (Overlay shape11 shape12)) == (asArrayOfPrimitives (Overlay shape21 shape22))
+  (==) _ _ = False
 
 -- Передвигает фигуру на x по горизонтали и на y по вертикали
 -- Реализовать, используя то, что PointT -- моноид
 slideShape :: Shape -> PointT -> Shape
-slideShape = undefined
+slideShape (Circle point radius) shift = Circle (point <> shift) radius
+slideShape (Rectangle point1 point2) shift = Rectangle (point1 <> shift) (point2 <> shift)
+slideShape (Overlay shape1 shape2) shift = Overlay (slideShape shape1 shift) (slideShape shape2 shift)
 
 -- Второй аргумент задает последовательность сдвигов фигуры.
 moveShapeAround :: Shape -> [PointT] -> Shape
-moveShapeAround = undefined
+moveShapeAround shape shifts = slideShape shape (mconcat shifts)
 
 -- Является ли Shape полугруппой? А моноидом?
 -- Реализовать инстансы, если является. Иначе -- обосновать.
 instance Semigroup Shape where
   (<>) :: Shape -> Shape -> Shape
-  (<>) = undefined
+  (<>) shape1 shape2 = Overlay shape1 shape2	-- But now we need to modify Shape.Eq so Overlay would become associative 
 
-instance Monoid Shape where
-  mempty = undefined
+-- Is it unobvious how to implement an "empty" Shape. It can be solved by adding a fictional, special `Empty` Shape constructor, but it would be something weird
+-- instance Monoid Shape where
+--   mempty = undefined
