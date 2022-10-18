ddf56fd6e5150047a001fa966dccba39fd05f47c
Oleg Makeev <blablahuman@Olegs-Mac-mini.local>
2022-10-18 17:15:20 +0200

    Done

--- src/SortedList.hs
+++ src/SortedList.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE InstanceSigs #-}
 module SortedList where
+import Data.List
 
 -- Тип данных, представляющий собой отсортированный список
 data SortedList a = SortedList { getSortedList :: [a] }
@@ -8,7 +9,7 @@ data SortedList a = SortedList { getSortedList :: [a] }
 -- <> должен сохранять отсортированность списка
 instance Ord a => Semigroup (SortedList a) where
   (<>) :: SortedList a -> SortedList a -> SortedList a
-  (<>) a b = undefined
+  (<>) a b = SortedList (sort (getSortedList a ++ getSortedList b))
 
 instance Ord a => Monoid (SortedList a) where
   mempty :: SortedList a
