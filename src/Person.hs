c3a2e7f8d446bc7b07a6bea0769b2e954ea2031b
meramiha <62883073+meramiha@users.noreply.github.com>
2022-10-19 15:50:49 +0200

    done

--- src/Person.hs
+++ src/Person.hs
@@ -1,10 +1,15 @@
-module Person where
-
+module Person (Person (..), Tree (..), Document (..), createChild, greatestAncestor, ancestors, descendants) where
 import qualified Data.Set as Set
+import Data.Foldable
 
-data Tree = ???
+data Tree a = Leaf a [Tree a]
+  | Node a (Maybe (Tree a)) (Maybe (Tree a)) [Tree a]
+  deriving (Show, Eq)
 
-data Document = ???
+data Document = Passport (Int, Int) | BirthCertificate (String, Int)
+  deriving (Show, Eq, Ord)
+
+
 
 -- Тип данных для человека
 data Person = Person
@@ -13,24 +18,64 @@ data Person = Person
   , formerLastNames :: [String] -- Предыдущие фамилии, если фамилия менялась
   , age :: Int                  -- Возраст, должен быть неотрицательным
   , idNumber :: Maybe Document  -- Какое-то удостоверение личности
-  , parents :: ??? Person       -- Родители данного человека. Выбрать подходящий контейнер.
+  , parents :: (Maybe Person, Maybe Person)   -- Родители данного человека. Выбрать подходящий контейнер.
   }
-  deriving (Show, Eq)
+  deriving (Show, Eq, Ord)
 
 -- Создание ребенка данных родителей
-createChild :: ??? Person -> Person
-createChild = undefined
+createChild :: Maybe Person -> Maybe Person -> String -> String -> Maybe Document -> Person
+createChild mother father firstName lastName idNumber = Person { firstName = firstName, lastName = lastName, formerLastNames = [], age = 0, idNumber = idNumber, parents = ( mother, father)}
 
 -- Самый далекий предок данного человека.
 -- Если на одном уровне иерархии больше одного предка -- вывести самого старшего из них.
 -- Если на одном уровне иерархии больше одного предка одного максимального возраста -- вывести любого из них
 greatestAncestor :: Person -> Person
-greatestAncestor = undefined
+greatestAncestor p = snd (maxi $ list p) where
+  list = getLeafs . getTree 
+  maxi:: [(Int, Person)] -> (Int, Person)
+  maxi [] = (-1, nilPerson)
+  maxi (x:xs) = do
+    let acc = (maxi xs)
+    if fst x > fst acc || (fst x == fst acc && (age $ snd x) >= (age $ snd acc)) then
+      x
+    else acc
 
 -- Предки на одном уровне иерархии.
 ancestors :: Int -> Person -> Set.Set Person
-ancestors = undefined
+ancestors num p = Set.fromList $ map (\(i, x) -> x) $ filter (\(i, x) -> i == num)(getNodes . getTree $ p)
 
 -- Возвращает семейное древо данного человека, описывающее его потомков.
 descendants :: Person -> Set.Set Person -> Tree Person
-descendants = undefined
+-- descendants = undefined
+descendants p everyone = Leaf p (map (\x -> descendants x everyone) (children p everyone)) where
+  children :: Person -> Set.Set Person -> [Person]
+  children p everyone = toList $ Set.filter (\s -> Just p == fst (parents s) || Just p == snd (parents s)) everyone
+
+
+-- utility --
+
+
+nilPerson = Person {firstName = "Null", lastName = "Null", formerLastNames = [], age = -1,idNumber = Nothing, parents = (Nothing, Nothing) }
+
+getTree:: Person -> Tree Person
+getTree p = do
+  case parents p of
+    (Nothing, Nothing) -> Leaf p []
+    (Just m, Nothing) -> Node p (Just $ getTree m) Nothing []
+    (Nothing, Just f) -> Node p Nothing (Just $ getTree f) []
+    (Just m, Just f) -> Node p (Just $ getTree m) (Just $ getTree f) []
+
+getLeafs:: Tree Person -> [(Int, Person)]
+getLeafs t = go [] (Just t) 0 where
+  go:: [(Int, Person)] -> Maybe (Tree Person) -> Int -> [(Int, Person)]
+  go acc Nothing _ = acc
+  go acc (Just (Leaf p [])) num = (num, p):acc
+  go acc (Just (Node k m f [])) num = (go acc m (num+1)) ++ (go acc f (num+1))
+
+getNodes:: Tree Person -> [(Int, Person)]
+getNodes t = go [] (Just t) 0 where
+  go:: [(Int, Person)] -> Maybe (Tree Person) -> Int -> [(Int, Person)]
+  go acc Nothing _ = acc
+  go acc (Just (Leaf p [])) num = (num, p):acc
+  go acc (Just (Node k m f [])) num = (num, k):(go acc m (num+1)) ++ (go acc f (num+1))
+
--- test/Test/Person.hs
+++ test/Test/Person.hs
@@ -2,3 +2,110 @@ module Test.Person where
 
 import Test.Tasty.HUnit (Assertion, (@?=), assertBool)
 import Person
+import qualified Data.Set as Set
+import Data.Foldable
+
+person1 :: Person
+person1 =
+  Person { firstName = "Kate"
+         , lastName = "Verbitskaia"
+         , formerLastNames = []
+         , age = 29
+         , idNumber = Just $ Passport (1234, 567890)
+         , parents = (Nothing, Nothing) }
+
+person2 :: Person
+person2 =
+  Person { firstName = "Kate"
+         , lastName = "Verbitskaia"
+         , formerLastNames = []
+         , age = 42
+         , idNumber = Just $ Passport (9876, 543210)
+         , parents = (Nothing, Nothing) }
+
+person3 :: Person
+person3 =
+  Person { firstName = "Kate"
+         , lastName = "Smith"
+         , formerLastNames = []
+         , age = 21
+         , idNumber = Just $ Passport (2121, 212121)
+         , parents = ((Just person4), Nothing) }
+
+person4 :: Person
+person4 =
+  Person { firstName = "Maria"
+         , lastName = "Verbitskaia"
+         , formerLastNames = []
+         , age = 83
+         , idNumber = Just $ Passport (1111, 111111)
+         , parents = (Nothing, (Just person5)) }
+
+person5 :: Person
+person5 =
+  Person { firstName = "Maria"
+         , lastName = "Verbitskaia"
+         , formerLastNames = []
+         , age = 83
+         , idNumber = Just $ Passport (1111, 111111)
+         , parents = (Nothing, Nothing) }
+
+child1 :: Person
+child1 =
+  Person { firstName = "Ivan"
+         , lastName = "Ivanov"
+         , formerLastNames = []
+         , age = 0
+         , idNumber = Just $ BirthCertificate ("ab", 123456)
+         , parents = ((Just person1), (Just person2)) }
+
+child2 :: Person
+child2 =
+  Person { firstName = "Masha"
+         , lastName = "Ivanova"
+         , formerLastNames = []
+         , age = 0
+         , idNumber = Just $ BirthCertificate ("cd", 987654)
+         , parents = (Nothing, Nothing) }
+child3 :: Person
+child3 =
+  Person { firstName = "Masha"
+         , lastName = "Ivanova"
+         , formerLastNames = []
+         , age = 0
+         , idNumber = Just $ BirthCertificate ("ef", 121212)
+         , parents = ((Just person1), (Just person2)) }
+
+
+unit_createChild = do
+  createChild (Just person1) (Just person2) "Ivan" "Ivanov" (Just $ BirthCertificate ("ab", 123456)) @?= child1
+  createChild Nothing Nothing "Masha" "Ivanova" (Just $ BirthCertificate ("cd", 987654)) @?= child2
+
+unit_greatestAncestor = do
+  greatestAncestor child1 @?= person2
+  greatestAncestor child2 @?= child2
+  greatestAncestor person3 @?= person5
+
+unit_ancestors = do
+  ancestors 0 child1 @?= Set.fromList [child1]
+  ancestors 1 child1 @?= Set.fromList [person1, person2]
+  ancestors 2 child1 @?= Set.fromList []
+  ancestors 0 person3 @?= Set.fromList [person3]
+  ancestors 1 person3 @?= Set.fromList [person4]
+  ancestors 2 person3 @?= Set.fromList [person5]
+  ancestors 3 person3 @?= Set.fromList []
+
+everyone :: Set.Set Person
+everyone = Set.fromList [person1, person2, person3, person4, person5, child1, child2, child3]
+
+unit_descendants = do
+  descendants child1 everyone @?= Leaf child1 []
+  descendants person1 everyone @?= Leaf person1 [(Leaf child1 []), (Leaf child3 [])]
+  descendants person1 everyone @?= Leaf person1 [descendants child1 everyone, descendants child3 everyone]
+  descendants person2 everyone @?= Leaf person2 [(Leaf child1 []), (Leaf child3 [])]
+
+  descendants person3 everyone @?= Leaf person3 []
+  descendants person4 everyone @?= Leaf person4 [Leaf person3 []]
+  descendants person5 everyone @?= Leaf person5 [Leaf person4 [Leaf person3 []]]
+
+
