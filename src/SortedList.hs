0ea508e8c1e88640d2b3fc5657b5c4993df6461a
kabachok-vpanike <90154942+kabachok-vpanike@users.noreply.github.com>
2022-10-19 22:55:10 +0200

    Update SortedList.hs

--- src/SortedList.hs
+++ src/SortedList.hs
@@ -1,15 +1,21 @@
 {-# LANGUAGE InstanceSigs #-}
+
 module SortedList where
 
+import GHC.Read (list)
+
 -- Тип данных, представляющий собой отсортированный список
-data SortedList a = SortedList { getSortedList :: [a] }
-                  deriving (Show, Eq)
+data SortedList a = SortedList {getSortedList :: [a]}
+  deriving (Show, Eq)
 
 -- <> должен сохранять отсортированность списка
 instance Ord a => Semigroup (SortedList a) where
   (<>) :: SortedList a -> SortedList a -> SortedList a
-  (<>) = undefined
+  (<>) (SortedList []) l2 = l2
+  (<>) l1 (SortedList []) = l1
+  (<>) (SortedList (l1 : ls1)) (SortedList (l2 : ls2)) | l1 > l2 = SortedList (l2 : getSortedList (SortedList (l1 : ls1) <> SortedList ls2))
+  (<>) (SortedList (l1 : ls1)) (SortedList (l2 : ls2)) = SortedList (l1 : getSortedList (SortedList ls1 <> SortedList (l2 : ls2)))
 
 instance Ord a => Monoid (SortedList a) where
   mempty :: SortedList a
-  mempty = undefined
+  mempty = SortedList []
