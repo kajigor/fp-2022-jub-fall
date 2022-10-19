e323f81b96eb0d4b213bbbf5b974dbf7be8e8934
Mirong1707 <31445859+Mirong1707@users.noreply.github.com>
2022-10-19 23:43:04 +0200

    Update SortedList.hs

--- src/SortedList.hs
+++ src/SortedList.hs
@@ -8,8 +8,15 @@ data SortedList a = SortedList { getSortedList :: [a] }
 -- <> должен сохранять отсортированность списка
 instance Ord a => Semigroup (SortedList a) where
   (<>) :: SortedList a -> SortedList a -> SortedList a
-  (<>) = undefined
+  (<>) arr0 arr1 = SortedList {getSortedList = join (getSortedList arr0) (getSortedList arr1)}
+
+join :: Ord a => [a] -> [a] -> [a]
+join x [] = x
+join [] y = y
+join (x : xs) (y : ys)
+  | x > y = (y : join (x : xs) ys)
+  | otherwise = (x : join xs (y : ys))
 
 instance Ord a => Monoid (SortedList a) where
   mempty :: SortedList a
-  mempty = undefined
+  mempty = SortedList []
