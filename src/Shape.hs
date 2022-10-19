53ae850cac0f2740e0ec7f428f60e66747fdcafb
RodionfromHSE <90181319+RodionfromHSE@users.noreply.github.com>
2022-10-19 23:38:52 +0200

    Shape fix

--- src/Shape.hs
+++ src/Shape.hs
@@ -1,19 +1,25 @@
 {-# LANGUAGE InstanceSigs #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+
 module Shape where
 
 -- Точка на плоскости
 data PointT = PointD Double Double
-            deriving (Show, Eq)
+  deriving (Show, Eq)
 
 -- В современных версиях ghc Monoid уже не содержит бинарную операцию mappend,
 -- mappend живет в классе Semigroup и называется <>.
 -- Любой инстанс моноида должен быть инстансом Semigroup (Semigroup a => Monoid a),
 -- а операция <> и mempty подчиняется тем же законам:
+
 -- * mempty <> a = a
+
 -- * a <> mempty = a
+
 -- * a <> (b <> c) = (a <> b) <> c
+
 -- * mconcat foldr (<>) mempty
+
 instance Monoid PointT where
   mempty :: PointT
   mempty = PointD 0 0
@@ -23,10 +29,11 @@ instance Semigroup PointT where
   (<>) (PointD x0 y0) (PointD x1 y1) = PointD (x0 + x1) $ y0 + y1
 
 -- Фигуры
-data Shape = Circle PointT Double    -- Круг характеризуется координатой центра и радиусом
-           | Rectangle PointT PointT -- Прямоугольник со сторонами, параллельными координатным осям, характеризуется координатами двух углов
-           | Overlay Shape Shape     -- Фигура, получающаяся наложением друг на друга двух других фигур
-           deriving (Show, Eq)
+data Shape
+  = Circle PointT Double -- Круг характеризуется координатой центра и радиусом
+  | Rectangle PointT PointT -- Прямоугольник со сторонами, параллельными координатным осям, характеризуется координатами двух углов
+  | Overlay Shape Shape -- Фигура, получающаяся наложением друг на друга двух других фигур
+  deriving (Show, Eq)
 
 -- Передвигает фигуру на x по горизонтали и на y по вертикали
 -- Реализовать, используя то, что PointT -- моноид
@@ -39,17 +46,10 @@ slideShape (Overlay sh1 sh2) p = Overlay (slideShape sh1 p) $ slideShape sh2 p
 moveShapeAround :: Shape -> [PointT] -> Shape
 moveShapeAround = foldl slideShape
 
-
-emptyCircle :: Shape
-emptyCircle = Circle (PointD 0 0) 0
 -- Является ли Shape полугруппой? А моноидом?
 -- Реализовать инстансы, если является. Иначе -- обосновать.
 instance Semigroup Shape where
   (<>) :: Shape -> Shape -> Shape
-  (<>) a b | a == emptyCircle = b
-           | b == emptyCircle = a
-           | otherwise = Overlay a b
+  (<>) = Overlay
 
-
-instance Monoid Shape where
-  mempty = emptyCircle
+-- Realization of Monoid isn't possible for our set and operation (Overlay) cause all figures have non-zero square => a <> mempty != a
