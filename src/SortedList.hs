7e91d5add948e4ddda438ff3df6d248cdf0425e6
gottagofaster236 <lr.soft.now@gmail.com>
2022-10-20 15:42:02 +0200

    Merge lists for O(N) time.

--- src/SortedList.hs
+++ src/SortedList.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE InstanceSigs, BangPatterns #-}
 module SortedList where
 import Data.List (sort)
 
@@ -9,7 +9,14 @@ data SortedList a = SortedList { getSortedList :: [a] }
 -- <> должен сохранять отсортированность списка
 instance Ord a => Semigroup (SortedList a) where
   (<>) :: SortedList a -> SortedList a -> SortedList a
-  (SortedList s1) <> (SortedList s2) = SortedList (sort (s1 ++ s2))
+  (SortedList s1) <> (SortedList s2) =
+      SortedList $ merge s1 s2
+    where
+      merge [] s2 = s2
+      merge s1 [] = s1
+      merge (h1:t1) (h2:t2) | h1 < h2 = h1 : merge t1 (h2:t2)
+      merge (h1:t1) (h2:t2) = h2 : merge (h1:t1) t2
+
 
 instance Ord a => Monoid (SortedList a) where
   mempty :: SortedList a
