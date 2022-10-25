1bd66d599561448a0f0106e23e58c95fc1393f38
Polina Musina <pmusina03@gmail.com>
2022-10-25 20:40:08 +0200

    done

--- test/Test/Expr.hs
+++ test/Test/Expr.hs
@@ -19,10 +19,12 @@ shouldBeResult result exp =
 
 unit_genExpr :: IO ()
 unit_genExpr = do
-    let results = [Right 13, Right (-42), Right 0.5
-                  , Left DivisionByZero, Left LogOfZero, Left LogOfNegativeNumber]
+    let results = [Right 13, Right (-42), Right 0.5,
+                  Left DivisionByZero, Left LogOfZero, Left LogOfNegativeNumber, 
+                  Left RootOfNonpositiveNumber, Left RootOfZeroDegree] 
     mapM_ check results
   where
     check :: Either ArithmeticError Double -> IO ()
     check result =
       mapM_ (uncurry shouldBeResult) [ (eval e, result) | e <- take 100 $ generateExprByResult result]
+      
