830ccc50746ab4f6eaaa044f4e12eef7b88380d6
RodionfromHSE <90181319+RodionfromHSE@users.noreply.github.com>
2022-10-25 20:17:49 +0200

    3 points

--- src/Expr.hs
+++ src/Expr.hs
@@ -2,204 +2,74 @@ module Expr where
 
 -- Тип данных для выражений.
 -- Каждое выражение это либо целое число, либо деление двух выражений, либо логарифм другого выражения.
-data Expr = Val Double
-          | Div Expr Expr
-          | Log Expr
-          deriving (Show, Eq)
+data BinOp = Add | Sub | Mul | Div | Root deriving (Show, Eq)
 
--- Пример выражения в нашем абстрактном синтаксисе
--- (12 / log 10) / 3
-expr :: Expr
-expr = Div (Div (Val 12) (Log (Val 10))) (Val 3)
+data UnOp = Log | Exp deriving (Show, Eq)
 
--- Выражение, вычисление которого должно приводить к ошибке.
--- Содержит деление на 0 и логарифм от отрицательного числа.
--- (6 / log (-1)) / 0
-exprErr :: Expr
-exprErr = Div (Div (Val 6) (Log (Val (-1)))) (Val 0)
+data Expr
+  = Val Double
+  | BinOp BinOp Expr Expr
+  | UnOp UnOp Expr
+  deriving (Show, Eq)
 
--- Частично определенный интерпретатор выражений.
-partialEval :: Expr -> Double
-partialEval (Val v) = v
-partialEval (Div x y) = partialEval x / partialEval y
-partialEval (Log x) = log (partialEval x)
+data ArithmeticError
+  = DivisionByZero
+  | RootOfDegreeZero
+  | RootOfNegativeNumber
+  | LogOfNonPositiveNumber
+  deriving (Show, Eq)
 
--- Вычисление корректного выражения с нормальным результатом
--- *Expr> partialEval expr
--- 1.737177927613007
+addEither :: Double -> Double -> Either ArithmeticError Double
+addEither x y = Right $ x + y
 
--- Вычисление выражения с делением на 0 приводит к результату NaN.
--- Хоть это и не исключение, пробрасываемое до пользователя, это все же ошибка,
--- которую хочется явно отлавливать и цивилизованно сообщать о ней пользователю.
--- *Expr> partialEval exprErr
--- NaN
+subEither :: Double -> Double -> Either ArithmeticError Double
+subEither x y = Right $ x - y
 
--- Этот интерпретатор будет возвращать Nothing в случае деления на 0
--- или вычисления логарифма от неположительного числа
-evalMaybe :: Expr -> Maybe Double
-evalMaybe (Val v) = Just v
-evalMaybe (Div x y) =
-  case evalMaybe x of
-    -- Проверяем, не произошло ли ошибки при вычислении левого подвыражения
-    Just x' ->
-      case evalMaybe y of
-        -- Проверяем, не произошло ли ошибки при вычислении правого подвыражения
-        Just y' -> totalDivMaybe x' y'
-        -- В случае ошибки можем только сообщать об ошибке
-        Nothing -> Nothing
-    Nothing -> Nothing
-evalMaybe (Log x) =
-  case evalMaybe x of
-    Just x' -> totalLogMaybe x'
-    Nothing -> Nothing
+mulEither :: Double -> Double -> Either ArithmeticError Double
+mulEither x y = Right $ x * y
 
-totalDivMaybe :: Double -> Double -> Maybe Double
-totalDivMaybe x y | y == 0 = Nothing
-                  | otherwise = Just $ x / y
+divEither :: Double -> Double -> Either ArithmeticError Double
+divEither x y
+  | y == 0 = Left DivisionByZero
+  | otherwise = Right $ x / y
 
-totalLogMaybe :: Double -> Maybe Double
-totalLogMaybe x | x <= 0 = Nothing
-                | otherwise = Just $ log x
+rootEither :: Double -> Double -> Either ArithmeticError Double
+rootEither d val
+  | d == 0 = Left RootOfDegreeZero
+  | val < 0 = Left RootOfNegativeNumber
+  | otherwise = Right $ exp $ d * log val
 
--- В случае ошибки теперь мы явно возвращаем Nothing,
--- а в случае успеха результат вычисления выражения заворачивается в конструктор Just
--- *Expr> evalMaybe expr
--- Just 1.737177927613007
--- *Expr> evalMaybe exprErr
--- Nothing
+logEither :: Double -> Either ArithmeticError Double
+logEither x
+  | x <= 0 = Left LogOfNonPositiveNumber
+  | otherwise = Right $ log x
 
--- Что если мы хотим предоставлять пользователю информативное сообщение об ошибке?
--- В этом случае имеет смысл воспользоваться типом данных Either e a = Left e | Right a.
--- Конструктор Right заворачивает корректное значение, в то время как Left -- ошибку.
--- Также мы можем создать специальный тип для ошибки ArithmeticError, чтобы явно
--- различать, какая именно произошла ошибка: деление на 0 или логарифм от неположительного числа
+expEither :: Double -> Either ArithmeticError Double
+expEither x = Right $ exp x
 
-data ArithmeticError = DivisionByZero
-                     | LogOfZero
-                     | LogOfNegativeNumber
-                     deriving (Show, Eq)
+evalBin :: (Double -> Double -> Either ArithmeticError Double) -> Expr -> Expr -> Either ArithmeticError Double
+evalBin op x y = do
+  x' <- eval x
+  y' <- eval y
+  op x' y'
 
-evalEither :: Expr -> Either ArithmeticError Double
--- Этот интерпретатор будет возвращать Nothing в случае деления на 0
--- или вычисления логарифма от неположительного числа
-evalEither (Val v) = Right v
-evalEither (Div x y) =
-    case evalEither x of
-      Right x' ->
-        case evalEither y of
-          Right y' -> totalDivEither x' y'
-          Left err -> Left err
-      Left err  -> Left err
-evalEither (Log x) =
-    case evalEither x of
-      Right x' -> totalLogEither x'
-      Left err -> Left err
-
-totalDivEither :: Double -> Double -> Either ArithmeticError Double
-totalDivEither x y | y == 0 = Left DivisionByZero
-                   | otherwise = Right $ x / y
-
-totalLogEither :: Double -> Either ArithmeticError Double
-totalLogEither x | x == 0 = Left LogOfZero
-                 | x < 0 = Left LogOfNegativeNumber
-                 | otherwise = Right $ log x
-
-expr1 :: Expr
-expr1 = Log (Val 0)
-
-expr2 :: Expr
-expr2 = Log (Val (-1))
-
-expr3 :: Expr
-expr3 = Div (Val 1) (Val 0)
-
--- *Expr> evalEither expr
--- Right 1.737177927613007
--- *Expr> evalEither expr1
--- Left LogOfZero
--- *Expr> evalEither expr2
--- Left LogOfNegativeNumber
--- *Expr> evalEither expr3
--- Left DivisionByZero
-
--- В коде evalMaybe и evalEither очень много повторяющейся логики: мы проверяем, что какое-то подвыражение
--- вычисляется без ошибок, и если это так, то продолжаем вычисление выражения.
--- Если происходит ошибка, мы о ней сообщаем.
--- Эту повторяющуюся логику было бы неплохо абстрагировать, что мы можем сделать,
--- используя bindMaybe, returnMaybe, bindEither и returnEither.
-
-bindMaybe :: Maybe a -> (a -> Maybe b) -> Maybe b
-bindMaybe value f =
-  case value of
-    Just x -> f x
-    Nothing -> Nothing
-
-returnMaybe :: a -> Maybe a
-returnMaybe x = Just x
-
-evalMaybe' :: Expr -> Maybe Double
-evalMaybe' (Val v) = returnMaybe v
-evalMaybe' (Div x y) =
-    evalMaybe' x `bindMaybe` \x' ->
-    evalMaybe' y `bindMaybe` \y' ->
-    totalDivMaybe x' y'
-evalMaybe' (Log x) =
-    evalMaybe' x `bindMaybe` \x' ->
-    totalLogMaybe x'
-
-
-bindEither :: Either e a -> (a -> Either e b) -> Either e b
-bindEither value f =
-  case value of
-    Right x -> f x
-    Left err -> Left err
-
-returnEither :: a -> Either e a
-returnEither x = Right x
-
-evalEither' :: Expr -> Either ArithmeticError Double
-evalEither' (Val v) = returnEither v
-evalEither' (Div x y) =
-    evalEither' x `bindEither` \x' ->
-    evalEither' y `bindEither` \y' ->
-    totalDivEither x' y'
-evalEither' (Log x) =
-    evalEither' x `bindEither` \x' ->
-    totalLogEither x'
-
--- Класс типов Monad предоставляет функции >>= (bind) и return ровно с тем функционалом, который нам необходим.
--- Monad и Either e являются инстансами класса Monad, поэтому мы можем использовать функции из него:
-
-evalMaybe'' :: Expr -> Maybe Double
-evalMaybe'' (Val v) = return v
-evalMaybe'' (Div x y) =
-    evalMaybe'' x >>= \x' ->
-    evalMaybe'' y >>= \y' ->
-    totalDivMaybe x' y'
-evalMaybe'' (Log x) =
-    evalMaybe'' x `bindMaybe` \x' ->
-    totalLogMaybe x'
-
-evalEither'' :: Expr -> Either ArithmeticError Double
-evalEither'' (Val v) = return v
-evalEither'' (Div x y) =
-    evalEither'' x >>= \x' ->
-    evalEither'' y >>= \y' ->
-    totalDivEither x' y'
-evalEither'' (Log x) =
-    evalEither'' x >>= \x' ->
-    totalLogEither x'
+evalUn :: (Double -> Either ArithmeticError Double) -> Expr -> Either ArithmeticError Double
+evalUn op x = do
+  x' <- eval x
+  op x'
 
 -- В Haskell есть do-нотация, которая позволяет выражать то же самое чуть более приятно:
 eval :: Expr -> Either ArithmeticError Double
 eval (Val n) = return n
-eval (Div x y) = do
-  x' <- eval x         -- eval x >>= \x' ->
-  y' <- eval y         -- eval y >>= \y' ->
-  totalDivEither x' y' -- totalDivEither x' y'
-eval (Log x) = do
-  x' <- eval x         -- eval x >>= \x' ->
-  totalLogEither x'    -- totalLogEither x'
+eval (UnOp op x) = case op of
+  Log -> evalUn logEither x
+  Exp -> evalUn expEither x
+eval (BinOp op x y) = case op of
+  Add -> evalBin addEither x y
+  Sub -> evalBin subEither x y
+  Mul -> evalBin mulEither x y
+  Div -> evalBin divEither x y
+  Root -> evalBin rootEither x y
 
 -- Функция принимает на вход результат вычисления арифметического выражения с учетом потенциальных ошибок
 -- и генерирует выражения, которые к этому результату вычисляются.
@@ -207,3 +77,11 @@ eval (Log x) = do
 generateExprByResult :: Either ArithmeticError Double -> [Expr]
 generateExprByResult = undefined
 
+expr1 :: Expr
+expr1 = UnOp Log (Val 0)
+
+expr2 :: Expr
+expr2 = UnOp Log (Val (-1))
+
+expr3 :: Expr
+expr3 = BinOp Div (Val 1) (Val 0)
