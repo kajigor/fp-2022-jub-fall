9722876223abd36ce0664270acc4dca114096ca0
Sleepwalking <alex_shef_31@mail.ru>
2022-11-08 12:36:48 +0300

    done

--- src/Lambda.hs
+++ src/Lambda.hs
@@ -1,11 +1,18 @@
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE InstanceSigs #-}
 module Lambda where
+import Data.Maybe
+import Data.List
+import Data.Tuple
+import Data.Char
 
 -- Тип для лямбда-термов.
 -- Параметризуем типом переменной.
-data Lambda a = Var a
-              | App (Lambda a) (Lambda a)
-              | Abs a (Lambda a)
+data Lambda a = Var a 
+                | App (Lambda a) (Lambda a)
+                | Abs a (Lambda a)
+                deriving Eq
+
 
 -- true ≡ λx.λy.x
 true = Abs "x" (Abs "y" (Var "x"))
@@ -14,13 +21,13 @@ true = Abs "x" (Abs "y" (Var "x"))
 false = Abs "x" (Abs "y" (Var "y"))
 
 -- and ≡ λp.λq.p q p
-and = Abs "p" (Abs "q" (App (App (Var "p") (Var "q")) (Var "p")))
+and' = Abs "p" (Abs "q" (App (App (Var "p") (Var "q")) (Var "p")))
 
 -- or ≡ λp.λq.p p q
-or = Abs "p" (Abs "q" (App (App (Var "p") (Var "p")) (Var "q")))
+or' = Abs "p" (Abs "q" (App (App (Var "p") (Var "p")) (Var "q")))
 
 -- not ≡ λp.p FALSE TRUE
-not = Abs "p" (App (App (Var "p") false) true)
+not' = Abs "p" (App (App (Var "p") false) true)
 
 -- ifThenElse ≡ λp.λa.λb.p a b
 ifThenElse = Abs "p" (Abs "a" (Abs "b" (App (App (Var "p") (Var "a")) (Var "b"))))
@@ -50,53 +57,143 @@ successor = Abs "n" (Abs "f" (Abs "x" (App (Var "f") (App (App (Var "n") (Var "f
 add' = Abs "m" (Abs "n" (App (App (Var "m") successor) (Var "n")))
 
 -- mult ≡ λm.λn.λf.m (n f)
-mult = Abs "m" (Abs "n" (App (Var "m") (App (Var "n") (Var "f"))))
+mult = Abs "m" (Abs "n" (Abs "f" (App (Var "m") (App (Var "n") (Var "f")))))
 
 -- mult' ≡ λm.λn.m (add n) 0
 mult' = Abs "m" (Abs "n" (App (App (Var "m") (App add (Var "n"))) zero))
 
 -- Красивая печать без лишних скобок.
 instance {-# OVERLAPS #-} Show (Lambda String) where
-  show = undefined
+  show (Var a) = a
+  show (Abs x term) = "λ" ++ x ++ "." ++ (show term)
+  show (App (Var a) (Var b)) = a ++ " " ++ b
+  show (App (Var a) b) = a ++ " (" ++ (show b) ++ ")"
+  show (App (Abs x lambda) (Var c)) = "(" ++ (show (Abs x lambda)) ++ ") " ++ c   
+  show (App (Abs x lambda) c) = "(" ++ (show (Abs x lambda)) ++ ") (" ++ (show c) ++ ")"
+  show (App a (Var c)) = (show a) ++ " " ++ c   
+  show (App a c) = (show a) ++ " (" ++ (show c) ++ ")"
 
+ 
 instance {-# OVERLAPPABLE #-} Show a => Show (Lambda a) where
-  show = undefined
+  show (Var a) = show a
+  show (Abs x term) = "λ" ++ (show x) ++ "." ++ (show term)
+  show (App (Var a) (Var b)) = (show a) ++ " " ++ (show b)
+  show (App (Var a) b) = (show a) ++ " (" ++ (show b) ++ ")"
+  show (App (Abs x lambda) (Var c)) = "(" ++ (show (Abs x lambda)) ++ ") " ++ (show c)   
+  show (App (Abs x lambda) c) = "(" ++ (show (Abs x lambda)) ++ ") (" ++ (show c) ++ ")"
+  show (App a (Var c)) = (show a) ++ " " ++ (show c)   
+  show (App a c) = (show a) ++ " (" ++ (show c) ++ ")"
+
+class Freshable a where 
+  getFresh :: [a] -> a
+
+available = Data.List.map (\x -> [x]) (['a'..'z'] ++ ['A'..'Z'])
+instance Freshable String where
+  getFresh :: [String] -> String
+  getFresh lst = (fromMaybe "" (find (\x -> not (x `elem` lst)) available))
 
 -- Выберите подходящий тип для подстановок.
-data Subst a
+data Subst a = SubPair a (Lambda a)
 
 -- Проверка термов на альфа-эквивалентность.
 alphaEq :: Eq a => Lambda a -> Lambda a -> Bool
-alphaEq = undefined
+alphaEq first second = helpfunction (toDeBruijn first) (toDeBruijn second)
+  where helpfunction :: DeBruijn -> DeBruijn -> Bool
+        helpfunction (VarDB x) (VarDB y) = (x == y)
+        helpfunction (AppDB ax ay) (AppDB bx by) = ((helpfunction ax bx) && (helpfunction ay by))
+        helpfunction (AbsDB av) (AbsDB bv) = (helpfunction av bv)
+        helpfunction _ _ = False
+
+getVars :: Lambda a -> [a]
+getVars (Var x) = [x]
+getVars (App a b) = (getVars a) ++ (getVars b)
+getVars (Abs x a) = [x] ++ (getVars a)
+
+cas_with_closed :: Eq a => Freshable a => Lambda a -> Subst a -> [a] -> Lambda a
+cas_with_closed (Var a) (SubPair x subst) closed | (a == x) = subst
+                                                 | otherwise = (Var a)
+cas_with_closed (App a b) subst closed = (App (cas_with_closed a subst closed) (cas_with_closed b subst closed)) 
+cas_with_closed (Abs x a) subst closed = (Abs y (cas_with_closed (cas_with_closed a (SubPair x (Var y)) (y : closed)) subst (y : closed)))
+  where y = (getFresh closed)
 
 -- Capture-avoiding substitution.
-cas :: Lambda a -> Subst a -> Lambda a
-cas = undefined
-
+cas :: Eq a => Freshable a => Lambda a -> Subst a -> Lambda a
+cas lambda (SubPair x subst) = cas_with_closed lambda (SubPair x subst) closed
+  where closed = (getVars lambda) ++ (getVars subst) 
+        
 -- Возможные стратегии редукции (о них расскажут 7 ноября).
 data Strategy = CallByValue | CallByName | NormalOrder | ApplicativeOrder
 
 -- Интерпретатор лямбда термов, учитывающий стратегию.
-eval :: Strategy -> Lambda a -> Lambda a
-eval = undefined
+eval :: Eq a => Freshable a => Strategy -> Lambda a -> Lambda a
+eval CallByValue lambda = evalCallByValue lambda 
+eval CallByName lambda = evalCallByName lambda 
+eval NormalOrder lambda = evalNormalOrder lambda 
+eval ApplicativeOrder lambda = evalApplicativeOrder lambda 
+
+evalCallByName :: Eq a => Freshable a => Lambda a -> Lambda a
+evalCallByName (App a b) = helpfunction (evalCallByName a) b
+  where helpfunction :: Eq a => Freshable a => Lambda a -> Lambda a -> Lambda a 
+        helpfunction (Abs x first) second = (evalCallByName (cas_with_closed first (SubPair x second) ((getVars (Abs x first)) ++ (getVars second))))
+        helpfunction first second = (App first second)
+evalCallByName lambda = lambda
+
+evalNormalOrder :: Eq a => Freshable a =>Lambda a -> Lambda a
+evalNormalOrder (Var x) = (Var x)
+evalNormalOrder (Abs x a) = (Abs x (evalNormalOrder a))
+evalNormalOrder (App a b) = helpfunction (evalCallByName a) b
+  where helpfunction :: Eq a => Freshable a => Lambda a -> Lambda a -> Lambda a
+        helpfunction (Abs x first) second = (evalNormalOrder (cas_with_closed first (SubPair x second) ((getVars (Abs x first)) ++ (getVars second))))
+        helpfunction first second = (App (evalNormalOrder first) (evalNormalOrder second))
+
+evalCallByValue :: Eq a => Freshable a => Lambda a -> Lambda a
+evalCallByValue (App a b) = helpfunction (evalCallByValue a) (evalCallByValue b)
+  where helpfunction :: Eq a => Freshable a => Lambda a -> Lambda a -> Lambda a
+        helpfunction (Abs x first) second = (evalCallByValue (cas_with_closed first (SubPair x second) ((getVars (Abs x first)) ++ (getVars second))))
+        helpfunction first second = (App first second)  
+evalCallByValue lambda = lambda
+
+evalApplicativeOrder :: Eq a => Freshable a => Lambda a -> Lambda a
+evalApplicativeOrder (App a b) = helpfunction (evalApplicativeOrder a) (evalApplicativeOrder b)
+  where helpfunction :: Eq a => Freshable a => Lambda a -> Lambda a -> Lambda a
+        helpfunction (Abs x first) second = (evalApplicativeOrder (cas_with_closed first (SubPair x second) ((getVars (Abs x first)) ++ (getVars second))))
+        helpfunction first second = (App first second)
+evalApplicativeOrder (Var x) = (Var x)
+evalApplicativeOrder (Abs x a) = (Abs x (evalApplicativeOrder a))
 
 -- ДеБрауновское представление лямбда-термов
 data DeBruijn = VarDB Int
               | AbsDB DeBruijn
               | AppDB DeBruijn DeBruijn
+              deriving Eq
 
 -- Красивая печать без лишних скобок.
 instance Show DeBruijn where
-  show = undefined
+  show (VarDB a) = show a
+  show (AbsDB term) = "λ" ++ "." ++ (show term)
+  show (AppDB (VarDB a) (VarDB b)) = (show a) ++ " " ++ (show b)
+  show (AppDB (VarDB a) b) = (show a) ++ " (" ++ (show b) ++ ")"
+  show (AppDB (AbsDB lambda) (VarDB c)) = "(" ++ (show (AbsDB lambda)) ++ ") " ++ (show c)   
+  show (AppDB (AbsDB lambda) c) = "(" ++ (show (AbsDB lambda)) ++ ") (" ++ (show c) ++ ")"
+  show (AppDB a (VarDB c)) = (show a) ++ " " ++ (show c)  
+  show (AppDB a c) = (show a) ++ " (" ++ (show c) ++ ")"
 
 -- λx. λy. x ≡ λ λ 2
 -- λx. λy. λz. x z (y z) ≡ λ λ λ 3 1 (2 1)
 -- λz. (λy. y (λx. x)) (λx. z x) ≡ λ (λ 1 (λ 1)) (λ 2 1)
 
 -- Преобразовать обычные лямбда-термы в деБрауновские
-toDeBruijn :: Lambda a -> DeBruijn
-toDeBruijn = undefined
+toDeBruijn :: Eq a => Lambda a -> DeBruijn
+toDeBruijn term = helpfunction term [] 0
+  where helpfunction :: Eq a => Lambda a -> [(a, Int)] -> Int -> DeBruijn
+        helpfunction (Var x) lst h = VarDB (subtract (snd (fromMaybe (x, -10) (find (\p -> (fst p) == x) lst))) h)
+        helpfunction (App f g) lst h  = AppDB (helpfunction f lst h) (helpfunction g lst h)
+        helpfunction (Abs x g) lst h = AbsDB (helpfunction g ((x, h) : lst) (succ h))
 
 -- Преобразовать деБрауновские лямбда-термы в обычные.
-fromDeBruijn :: DeBruijn -> Lambda a
-fromDeBruijn = undefined
+fromDeBruijn :: DeBruijn -> Lambda String
+fromDeBruijn term = helpfunction term [] 0
+  where helpfunction :: DeBruijn -> [String] -> Int -> Lambda String
+        helpfunction (VarDB x) lst h = Var (lst!!(h - x))
+        helpfunction (AppDB f g) lst h = App (helpfunction f lst h) (helpfunction g lst h)
+        helpfunction (AbsDB g) lst h = Abs (getFresh lst) (helpfunction g (lst ++ [getFresh lst]) (succ h))
--- test/Test/Lambda.hs
+++ test/Test/Lambda.hs
@@ -1,1 +1,147 @@
 module Test.Lambda where
+import Test.HUnit (Assertion, assertBool, (@?=))
+
+import Lambda
+
+unit_show_string :: Assertion
+unit_show_string = do
+    (show true) @?= "λx.λy.x"
+    (show add) @?= "λm.λn.λf.λx.m f (n f x)"
+    (show mult) @?= "λm.λn.λf.m (n f)"
+    (show or') @?= "λp.λq.p p q"
+    (show (App (Abs "x" (App (Var "t") (Var "x"))) (Abs "y" (Var "y")))) @?= "(λx.t x) (λy.y)"
+    (show (App (App (Var "t") (Var "x")) (Abs "y" (Var "y")))) @?= "t x (λy.y)"
+
+
+-- λx.x
+term1 = Abs "x" (Var "x")
+
+-- λy.y
+term2 = Abs "y" (Var "y")
+
+-- λx.λy.x
+term3 = Abs "x" (Abs "y" (Var "x"))
+
+-- x (λx.x) (λx.λy.x)
+term4 = Abs "y" (Abs "x" (App (App (Var "x") (Abs "x" (Var "x"))) (Abs "x" (Abs "y" (Var "x")))))
+
+-- x (λt.t) (λa.λb.a)
+term5 = Abs "y" (Abs "x" (App (App (Var "x") (Abs "t" (Var "t"))) (Abs "a" (Abs "b" (Var "a")))))
+
+-- a (λt.t) (λx.λy.x)
+term6 = Abs "a" (Abs "w" (App (App (Var "a") (Abs "t" (Var "t"))) (Abs "x" (Abs "y" (Var "x")))))
+
+unit_alpha_eq :: Assertion
+unit_alpha_eq = do
+    (alphaEq term1 term2) @?= True
+    (alphaEq term1 term3) @?= False
+    (alphaEq term3 term4) @?= False
+    (alphaEq term5 term4) @?= True
+    (alphaEq term5 term6) @?= False
+
+term1DeBrujin = AbsDB (VarDB 1)
+
+term3DeBrujin = AbsDB (AbsDB (VarDB 2))
+
+term4DeBrujin = AbsDB (AbsDB (AppDB (AppDB (VarDB 1) (AbsDB (VarDB 1))) (AbsDB (AbsDB (VarDB 2)))))
+
+term6DeBruijn = AbsDB (AbsDB (AppDB (AppDB (VarDB 2) (AbsDB (VarDB 1))) (AbsDB (AbsDB (VarDB 2)))))
+
+unit_to_DeBruijn :: Assertion
+unit_to_DeBruijn = do
+    (toDeBruijn term1) @?= term1DeBrujin
+    (toDeBruijn term2) @?= term1DeBrujin
+    (toDeBruijn term3) @?= term3DeBrujin
+    (toDeBruijn term4) @?= term4DeBrujin
+    (toDeBruijn term5) @?= term4DeBrujin
+    (toDeBruijn term6) @?= term6DeBruijn
+    ((toDeBruijn term4) == term6DeBruijn) @?= False
+
+
+unit_from_DeBruijn :: Assertion
+unit_from_DeBruijn = do
+    (alphaEq (fromDeBruijn term1DeBrujin) term1) @?= True
+    (alphaEq term2 (fromDeBruijn term1DeBrujin)) @?= True
+    (alphaEq term3 (fromDeBruijn term3DeBrujin)) @?= True
+    (alphaEq term4 (fromDeBruijn term4DeBrujin)) @?= True
+    (alphaEq term5 (fromDeBruijn term4DeBrujin)) @?= True
+    (alphaEq term6 (fromDeBruijn term6DeBruijn)) @?= True
+    (alphaEq term4 (fromDeBruijn term6DeBruijn)) @?= False
+
+
+unit_show_string_DeBruin :: Assertion
+unit_show_string_DeBruin = do
+    (show (toDeBruijn true)) @?= "λ.λ.2"
+    (show (toDeBruijn add)) @?= "λ.λ.λ.λ.4 2 (3 2 1)"
+    (show (toDeBruijn mult)) @?= "λ.λ.λ.3 (2 1)"
+    (show (toDeBruijn or')) @?= "λ.λ.2 2 1"
+    (show term6DeBruijn) @?= "λ.λ.2 (λ.1) (λ.λ.2)"
+
+k = (Abs "x" (Abs "y" (Var "x")))
+i = (Abs "x" (Var "x"))
+s = (Abs "x" (Abs "y" (Abs "z" (App (App (Var "x") (Var "z")) (App (Var "y") (Var "z"))))))
+
+-- (λx.x x) ((λx.λy.x) (λx.x) (λx.λy.λz.x z (y z)) (λx.λy.λz.x z (y z)))
+term_eval1 = App (Abs "x" (App (Var "x") (Var "x"))) (App (App (App k i) s) s)
+-- (λy.λx.y x ((λx.x) x)) ((λx.λy.x) (λx.λy.λz.x z (y z)) (λx.x))
+term_eval2 = App (Abs "y" (Abs "x" (App (App (Var "y") (Var "x")) (App i (Var "x"))))) (App (App k s) i)
+-- (λf.λx.λy.f x (f y)) ((λx.λy.x) (λx.x) (λx.λy.λz.x z (y z)))
+term_eval3 = App (Abs "f" (Abs "x" (Abs "y" (App (App (Var "f") (Var "x")) (App (Var "f") (Var "y")))))) (App (App k i) s)
+-- (λx.(λx.λy.x) (λx.x) (λx.λy.λz.x z (y z)) x) ((λx.λy.λz.x z (y z)) x)
+term_eval4 = App (Abs "x" (App (App (App k i) s) (Var "x"))) (App s (Var "x"))
+
+-- λy.λz.(λx.λy.λz.x z (y z)) z (y z)
+term_eval1_ByValue = Abs "y" (Abs "z" (App (App s (Var "z")) (App (Var "y") (Var "z"))))
+-- λx.(λx.λy.λz.x z (y z)) x ((λx.x) x)
+term_eval2_ByValue = Abs "x" (App (App s (Var "x")) (App i (Var "x")))
+-- λx.λy.(λx.x) x ((λx.x) y)
+term_eval3_ByValue = Abs "x" (Abs "y" (App (App i (Var "x")) (App i (Var "y"))))
+-- λy.λz.x z (y z)
+term_eval4_ByValue = (Abs "y" (Abs "z" (App (App (Var "x") (Var "z")) (App (Var "y") (Var "z")))))
+
+-- λy.λz.(λx.λy.x) (λx.x) (λx.λy.λz.x z (y z)) (λx.λy.λz.x z (y z)) z (y z)
+term_eval1_ByName = Abs "y" (Abs "z" (App (App (App (App (App k i) s) s) (Var "z")) (App (Var "y") (Var "z"))))
+-- λx.(λx.λy.x) (λx.λy.λz.x z (y z)) (λx.x) x ((λx.x) x)
+term_eval2_ByName = Abs "x" (App (App (App (App k s) i) (Var "x")) (App i (Var "x")))
+-- λx.λy.(λx.λy.x) (λx.x) (λx.λy.λz.x z (y z)) x ((λx.λy.x) (λx.x) (λx.λy.λz.x z (y z)) y)
+term_eval3_ByName =  Abs "x" (Abs "y" (App (App (App (App k i) s) (Var "x")) (App (App (App k i) s) (Var "y"))))
+-- λy.λz.x z (y z)
+term_eval4_ByName = (Abs "y" (Abs "z" (App (App (Var "x") (Var "z")) (App (Var "y") (Var "z")))))
+
+-- λy.λz.λb.z b (y z b)
+term_eval1_Normal = Abs "y" (Abs "z" (Abs "b" (App (App (Var "z") (Var "b")) (App (App (Var "y") (Var "z")) (Var "b")))))
+-- λx.λz.x z (x z)
+term_eval2_Normal = Abs "x" (Abs "z" (App (App (Var "x") (Var "z")) (App (Var "x") (Var "z"))))
+-- λx.λy.x y
+term_eval3_Normal = Abs "x" (Abs "y" (App (Var "x") (Var "y")))
+-- λy.λz.x z (y z)
+term_eval4_Normal = (Abs "y" (Abs "z" (App (App (Var "x") (Var "z")) (App (Var "y") (Var "z")))))
+
+-- λy.λz.λb.z b (y z b)
+term_eval1_Applicative = Abs "y" (Abs "z" (Abs "b" (App (App (Var "z") (Var "b")) (App (App (Var "y") (Var "z")) (Var "b")))))
+-- λx.λz.x z (x z)
+term_eval2_Applicative = Abs "x" (Abs "z" (App (App (Var "x") (Var "z")) (App (Var "x") (Var "z"))))
+-- λx.λy.x y
+term_eval3_Applicative = Abs "x" (Abs "y" (App (Var "x") (Var "y")))
+-- λy.λz.x z (y z)
+term_eval4_Applicative = (Abs "y" (Abs "z" (App (App (Var "x") (Var "z")) (App (Var "y") (Var "z")))))
+
+unit_eval :: Assertion
+unit_eval = do
+    (alphaEq (eval CallByValue term_eval1) term_eval1_ByValue) @?= True
+    (alphaEq (eval CallByValue term_eval2) term_eval2_ByValue) @?= True
+    (alphaEq (eval CallByValue term_eval3) term_eval3_ByValue) @?= True
+    (alphaEq (eval CallByValue term_eval4) term_eval4_ByValue) @?= True
+    (alphaEq (eval CallByName term_eval1) term_eval1_ByName) @?= True
+    (alphaEq (eval CallByName term_eval2) term_eval2_ByName) @?= True
+    (alphaEq (eval CallByName term_eval3) term_eval3_ByName) @?= True
+    (alphaEq (eval CallByName term_eval4) term_eval4_ByName) @?= True
+    (alphaEq (eval NormalOrder term_eval1) term_eval1_Normal) @?= True
+    (alphaEq (eval NormalOrder term_eval2) term_eval2_Normal) @?= True
+    (alphaEq (eval NormalOrder term_eval3) term_eval3_Normal) @?= True
+    (alphaEq (eval NormalOrder term_eval4) term_eval4_Normal) @?= True
+    (alphaEq (eval ApplicativeOrder term_eval1) term_eval1_Applicative) @?= True
+    (alphaEq (eval ApplicativeOrder term_eval2) term_eval2_Applicative) @?= True
+    (alphaEq (eval ApplicativeOrder term_eval3) term_eval3_Applicative) @?= True
+    (alphaEq (eval ApplicativeOrder term_eval4) term_eval4_Applicative) @?= True
+    
