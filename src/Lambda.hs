108c2df4487eb487bac3cc8d2c2315ff429c2109
msh2481 <mh2481@mail.ru>
2022-11-09 13:06:36 +0100

    Done

--- src/Lambda.hs
+++ src/Lambda.hs
@@ -1,14 +1,20 @@
-  {-# LANGUAGE FlexibleInstances #-}
-  {-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE UndecidableInstances #-}
+
 module Lambda where
-import Data.Maybe
-import Data.List
+
+import Data.Char
 import Data.Function
+import Data.List
+import Data.Maybe
+import qualified Data.Set as Set
+
 -- Тип для лямбда-термов.
 -- Параметризуем типом переменной.
-data Lambda a = Var a
-              | App (Lambda a) (Lambda a)
-              | Abs a (Lambda a)
+data Lambda a
+  = Var a
+  | App (Lambda a) (Lambda a)
+  | Abs a (Lambda a)
 
 -- true ≡ λx.λy.x
 true = Abs "x" (Abs "y" (Var "x"))
@@ -38,10 +44,10 @@ one = Abs "f" (Abs "x" (App (Var "f") (Var "x")))
 two = Abs "f" (Abs "x" (App (Var "f") (App (Var "f") (Var "x"))))
 
 -- three ≡ λf.λx.f (f (f x))
-three =  Abs "f" (Abs "x" (App (Var "f") (App (Var "f") (App (Var "f") (Var "x")))))
+three = Abs "f" (Abs "x" (App (Var "f") (App (Var "f") (App (Var "f") (Var "x")))))
 
 --  four ≡ λf.λx.f (f (f (f x)))
-four =  Abs "f" (Abs "x" (App (Var "f") (App (Var "f") (App (Var "f") (App (Var "f") (Var "x"))))))
+four = Abs "f" (Abs "x" (App (Var "f") (App (Var "f") (App (Var "f") (App (Var "f") (Var "x"))))))
 
 -- add ≡ λm.λn.λf.λx.m f (n f x)
 add = Abs "m" (Abs "n" (Abs "f" (Abs "x" (App (App (Var "m") (Var "f")) (App (App (Var "n") (Var "f")) (Var "x"))))))
@@ -53,7 +59,7 @@ successor = Abs "n" (Abs "f" (Abs "x" (App (Var "f") (App (App (Var "n") (Var "f
 add' = Abs "m" (Abs "n" (App (App (Var "m") successor) (Var "n")))
 
 -- mult ≡ λm.λn.λf.m (n f)
-mult = Abs "m" (Abs "n" (App (Var "m") (App (Var "n") (Var "f"))))
+mult = Abs "m" (Abs "n" (Abs "f" (App (Var "m") (App (Var "n") (Var "f")))))
 
 -- mult' ≡ λm.λn.m (add n) 0
 mult' = Abs "m" (Abs "n" (App (App (Var "m") (App add (Var "n"))) zero))
@@ -74,61 +80,114 @@ condWrap False s = s
 isApp :: Lambda a -> Bool
 isApp e = case e of
   App _ _ -> True
-  _         -> False
+  _ -> False
 
 isAbs :: Lambda a -> Bool
 isAbs e = case e of
   Abs _ _ -> True
-  _       -> False
+  _ -> False
 
 -- Красивая печать без лишних скобок.
 instance ShowProperly a => Show (Lambda a) where
   show expr = case expr of
-      Var s -> repr s
-      App x y ->  condWrap (isAbs x) (show x) ++ " " ++
-                  condWrap (isApp y || isAbs y) (show y)
-      Abs x y -> "\\" ++ repr x ++ "." ++ show y
+    Var s -> repr s
+    App x y ->
+      condWrap (isAbs x) (show x) ++ " "
+        ++ condWrap (isApp y || isAbs y) (show y)
+    Abs x y -> "\\" ++ repr x ++ "." ++ show y
+
+class Ord a => Fresh a where
+  candidates :: [a]
+  candidates = undefined
+
+  fresh :: Set.Set a -> a
+  fresh used = head $ filter isFree candidates
+    where
+      isFree x = x `Set.notMember` used
+
+instance Fresh String where
+  candidates = [f n | n <- [0 ..]]
+    where
+      alpha = 26
+      f n
+        | n < alpha = [chr (n + ord 'a')]
+        | otherwise = f (n `div` alpha) ++ f (n `rem` alpha)
 
 -- Выберите подходящий тип для подстановок.
-data Subst a
+data Subst a = Subst a (Lambda a)
 
 -- Проверка термов на альфа-эквивалентность.
 alphaEq :: Eq a => Lambda a -> Lambda a -> Bool
 alphaEq = (==) `on` (show . toDeBruijn)
 
+freeVariables :: Ord a => Lambda a -> Set.Set a
+freeVariables (Var x) = Set.singleton x
+freeVariables (App x y) = freeVariables x `Set.union` freeVariables y
+freeVariables (Abs x y) = freeVariables y `Set.difference` Set.singleton x
+
 -- Capture-avoiding substitution.
-cas :: Lambda a -> Subst a -> Lambda a
-cas = undefined
+cas :: Fresh a => Lambda a -> Subst a -> Lambda a
+cas (Var y) (Subst x m) =
+  if x == y
+    then m
+    else Var y
+cas (App x y) s = App (cas x s) (cas y s)
+cas (Abs y expr) (Subst x m)
+  | (y == x) = Abs y expr
+  | (y `Set.notMember` fvm) = Abs y (expr `cas` (Subst x m))
+  | otherwise = Abs z (expr `cas` (Subst y (Var z)) `cas` (Subst x m))
+  where
+    fvm = freeVariables m
+    z = fresh (x `Set.insert` fvm)
 
 -- Возможные стратегии редукции (о них расскажут 7 ноября).
 data Strategy = CallByValue | CallByName | NormalOrder | ApplicativeOrder
 
+isWeak :: Strategy -> Bool
+isWeak CallByValue = True
+isWeak CallByName = True
+isWeak _ = False
+
+-- Выполняет CAS на верхнем уровне, если можно, и снова запускает eval на результате
+evalApp :: Fresh a => Strategy -> Lambda a -> Lambda a -> Lambda a
+evalApp strategy (Abs x y) z = eval strategy (y `cas` Subst x z)
+evalApp strategy x y = App (eval strategy x) (eval strategy y)
+
 -- Интерпретатор лямбда термов, учитывающий стратегию.
-eval :: Strategy -> Lambda a -> Lambda a
-eval = undefined
+eval :: Fresh a => Strategy -> Lambda a -> Lambda a
+eval _ (Var x) = Var x
+eval strategy (Abs x y)
+  | isWeak strategy = Abs x y
+  | otherwise = Abs x (eval strategy y)
+eval CallByName (App x y) = evalApp CallByName (eval CallByName x) y
+eval NormalOrder (App x y) = evalApp NormalOrder (eval CallByName x) y
+eval CallByValue (App x y) = evalApp CallByValue (eval CallByValue x) (eval CallByValue y)
+eval ApplicativeOrder (App x y) = evalApp ApplicativeOrder (eval ApplicativeOrder x) (eval ApplicativeOrder y)
 
 isAppDB :: DeBruijn -> Bool
 isAppDB e = case e of
   AppDB _ _ -> True
-  _         -> False
+  _ -> False
 
 isAbsDB :: DeBruijn -> Bool
 isAbsDB e = case e of
   AbsDB _ -> True
-  _       -> False
+  _ -> False
 
 -- ДеБрауновское представление лямбда-термов
-data DeBruijn = VarDB Int
-              | AppDB DeBruijn DeBruijn
-              | AbsDB DeBruijn
+data DeBruijn
+  = VarDB Int
+  | AppDB DeBruijn DeBruijn
+  | AbsDB DeBruijn
 
 -- Красивая печать без лишних скобок.
 instance Show DeBruijn where
-   show expr = case expr of
-      VarDB s -> show s
-      AppDB x y ->  condWrap (isAbsDB x) (show x) ++ " " ++
-                    condWrap (isAppDB y || isAbsDB y) (show y)
-      AbsDB y -> "\\ " ++ show y
+  show expr = case expr of
+    VarDB s -> show s
+    AppDB x y ->
+      condWrap (isAbsDB x) (show x) ++ " "
+        ++ condWrap (isAppDB y || isAbsDB y) (show y)
+    AbsDB y -> "\\ " ++ show y
 
 -- λx. λy. x ≡ λ λ 2
 -- λx. λy. λz. x z (y z) ≡ λ λ λ 3 1 (2 1)
@@ -141,17 +200,21 @@ toDeBruijn = fromJust . f []
     f s (Var x) = do
       i <- elemIndex x s
       return $ VarDB (i + 1)
-    
     f s (App x y) = do
       x' <- f s x
       y' <- f s y
       return $ AppDB x' y'
-    
     f s (Abs x y) = do
-      y' <- f (x:s) y
+      y' <- f (x : s) y
       return $ AbsDB y'
-    
 
 -- Преобразовать деБрауновские лямбда-термы в обычные.
-fromDeBruijn :: DeBruijn -> Lambda a
-fromDeBruijn = undefined
+fromDeBruijn :: Fresh a =>  DeBruijn -> Lambda a
+fromDeBruijn = f []
+  where 
+    f :: Fresh a => [a] -> DeBruijn -> Lambda a
+    f stack (VarDB x) = Var $ fromJust $ listToMaybe $ drop (x - 1) stack
+    f stack (AppDB x y) = App (f stack x) (f stack y)
+    f stack (AbsDB y) = Abs x (f (x:stack) y)
+        where x = fresh $ Set.fromList stack
+        
--- test/Test/Lambda.hs
+++ test/Test/Lambda.hs
@@ -1,22 +1,57 @@
 module Test.Lambda where
+
+import qualified Data.Set as Set
+import Lambda
 import Test.Tasty
 import Test.Tasty.HUnit
-import Lambda
 
-unit_show :: IO()
+x :: Lambda [Char]
+x = Var "x"
+
+y :: Lambda [Char]
+y = Var "y"
+
+z :: Lambda [Char]
+z = Var "z"
+
+xy :: Lambda [Char]
+xy = App x y
+
+lxy :: Lambda [Char]
+lxy = Abs "x" y
+
+unit_show :: IO ()
 unit_show = do
-    show (Var "x") @?= "x"
-    show (App (Var "x") (Var "y")) @?= "x y"
-    show (Abs "x" (Var "y")) @?= "\\x.y"
-    show Lambda.true @?= "\\x.\\y.x"
-    show Lambda.and @?= "\\p.\\q.p q p"
-    show Lambda.two @?= "\\f.\\x.f (f x)"
-    show Lambda.mult' @?= "\\m.\\n.m ((\\m.\\n.\\f.\\x.m f (n f x)) n) (\\f.\\x.x)"
+  show x @?= "x"
+  show xy @?= "x y"
+  show lxy @?= "\\x.y"
+  show Lambda.true @?= "\\x.\\y.x"
+  show Lambda.and @?= "\\p.\\q.p q p"
+  show Lambda.two @?= "\\f.\\x.f (f x)"
+  show Lambda.mult' @?= "\\m.\\n.m ((\\m.\\n.\\f.\\x.m f (n f x)) n) (\\f.\\x.x)"
 
+unit_freeVariables :: IO ()
+unit_freeVariables = do
+  freeVariables x @?= Set.fromList ["x"]
+  freeVariables xy @?= Set.fromList ["x", "y"]
+  freeVariables lxy @?= Set.fromList ["y"]
+  freeVariables Lambda.true @?= Set.empty
+  freeVariables Lambda.and @?= Set.empty
+  freeVariables Lambda.two @?= Set.empty
+  freeVariables Lambda.mult' @?= Set.empty
 
--- λx. λy. x ≡ λ λ 2
--- λx. λy. λz. x z (y z) ≡ λ λ λ 3 1 (2 1)
--- λz. (λy. y (λx. x)) (λx. z x) ≡ λ (λ 1 (λ 1)) (λ 2 1)
+unit_fresh :: IO ()
+unit_fresh = do
+  take 33 (candidates :: [String]) @?= ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "ba", "bb", "bc", "bd", "be", "bf", "bg"]
+
+sxy :: Subst [Char]
+sxy = Subst "x" y
+
+sxz :: Subst [Char]
+sxz = Subst "x" z
+
+lzx :: Lambda [Char]
+lzx = App (Var "z") (Var "x")
 
 db1 :: Lambda String
 db1 = Abs "x" (Abs "y" (Var "x"))
@@ -36,20 +71,67 @@ db1'' = Abs "y" (Abs "z" (Var "z"))
 db2' :: Lambda String
 db2' = Abs "a" (Abs "b" (Abs "c" (App (App (Var "a") (Var "c")) (App (Var "b") (Var "c")))))
 
-unit_show_toDeBruijn :: IO()
-unit_show_toDeBruijn = do
-    show (toDeBruijn db1) @?= "\\ \\ 2"
-    show (toDeBruijn db1') @?= "\\ \\ 2"
-    show (toDeBruijn db1'') @?= "\\ \\ 1"
-    show (toDeBruijn db2) @?= "\\ \\ \\ 3 1 (2 1)"
-    show (toDeBruijn db2') @?= "\\ \\ \\ 3 1 (2 1)"
-    show (toDeBruijn db3) @?= "\\ (\\ 1 (\\ 1)) (\\ 2 1)"
+twotimestwo :: Lambda [Char]
+twotimestwo = App (App Lambda.mult Lambda.two) Lambda.two
 
+twotimestwo' :: Lambda [Char]
+twotimestwo' = App (App Lambda.mult' Lambda.two) Lambda.two
 
+onetimesone' :: Lambda [Char]
+onetimesone' = App (App Lambda.mult' Lambda.one) Lambda.one
 
-unit_alpha :: IO()
+unit_toDeBruijn :: IO ()
+unit_toDeBruijn = do
+  show (toDeBruijn db1) @?= "\\ \\ 2"
+  show (toDeBruijn db1') @?= "\\ \\ 2"
+  show (toDeBruijn db1'') @?= "\\ \\ 1"
+  show (toDeBruijn db2) @?= "\\ \\ \\ 3 1 (2 1)"
+  show (toDeBruijn db2') @?= "\\ \\ \\ 3 1 (2 1)"
+  show (toDeBruijn db3) @?= "\\ (\\ 1 (\\ 1)) (\\ 2 1)"
+
+back1 :: Lambda String
+back1 = fromDeBruijn $ toDeBruijn db1
+back2 :: Lambda String
+back2 = fromDeBruijn $ toDeBruijn db2
+back3 :: Lambda String
+back3 = fromDeBruijn $ toDeBruijn db3
+
+unit_fromDeBruijn :: IO ()
+unit_fromDeBruijn = do
+  show (toDeBruijn back1) @?= show (toDeBruijn db1)
+  show (toDeBruijn back2) @?= show (toDeBruijn db2)
+  show (toDeBruijn back3) @?= show (toDeBruijn db3)
+
+unit_alpha :: IO ()
 unit_alpha = do
-    alphaEq db1 db1' @?= True
-    alphaEq db1 db1'' @?= False
-    alphaEq db1 db2' @?= False
-    alphaEq db2 db2' @?= True
+  alphaEq db1 db1' @?= True
+  alphaEq db1 db1'' @?= False
+  alphaEq db1 db2' @?= False
+  alphaEq db2 db2' @?= True
+
+unit_cas :: IO ()
+unit_cas = do
+  show (cas x sxy) @?= "y"
+  show (cas xy sxy) @?= "y y"
+  show (cas lxy sxy) @?= "\\x.y"
+  show (cas (Abs "z" y) sxy) @?= "\\z.y"
+  show (cas (Abs "z" x) sxy) @?= "\\z.y"
+  show (cas (Abs "z" x) sxz) @?= "\\a.z"
+  show (cas (Abs "y" x) sxz) @?= "\\y.z"
+  show (cas (Abs "z" lzx) sxz) @?= "\\a.a z"
+  show (cas (Abs "f" (Abs "x" (Var "f"))) (Subst "a" (Var "f"))) @?= "\\b.\\x.b"
+
+unit_no :: IO ()
+unit_no = do
+  show (eval NormalOrder Lambda.successor) @?= "\\n.\\f.\\x.f (n f x)"
+  show (eval NormalOrder Lambda.mult) @?= "\\m.\\n.\\f.m (n f)"
+  show (eval NormalOrder Lambda.mult') @?= "\\m.\\n.m (\\a.\\f.\\x.n f (a f x)) (\\f.\\x.x)"
+  show (eval NormalOrder twotimestwo) @?= show Lambda.four
+  show (eval NormalOrder twotimestwo') @?= show Lambda.four
+
+unit_ao :: IO ()
+unit_ao = do
+  (show . toDeBruijn) (eval ApplicativeOrder Lambda.mult) @?= "\\ \\ \\ 3 (2 1)"
+  (show . toDeBruijn) (eval ApplicativeOrder Lambda.mult') @?= "\\ \\ 2 (\\ \\ \\ 4 2 (3 2 1)) (\\ \\ 1)"
+  (show . toDeBruijn) (eval ApplicativeOrder onetimesone') @?= (show . toDeBruijn) Lambda.one
+  (show . toDeBruijn) (eval ApplicativeOrder twotimestwo) @?= (show . toDeBruijn) Lambda.four
