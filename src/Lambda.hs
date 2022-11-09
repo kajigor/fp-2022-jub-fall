2159fe6b0706fbce5ba835b585c3133854174e2b
Ilya <iluxa.kholkin@gmail.com>
2022-11-09 17:40:00 +0100

    Initial commit

--- src/Lambda.hs
+++ src/Lambda.hs
@@ -1,11 +1,18 @@
 {-# LANGUAGE FlexibleInstances #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Redundant where" #-}
 module Lambda where
+import qualified Data.Map.Strict as Map
+import qualified Data.Set as Set
+import GHC.RTS.Flags (getDebugFlags)
+import Data.Maybe (isNothing)
 
 -- Тип для лямбда-термов.
 -- Параметризуем типом переменной.
 data Lambda a = Var a
               | App (Lambda a) (Lambda a)
               | Abs a (Lambda a)
+              deriving (Eq)
 
 -- true ≡ λx.λy.x
 true = Abs "x" (Abs "y" (Var "x"))
@@ -57,21 +64,46 @@ mult' = Abs "m" (Abs "n" (App (App (Var "m") (App add (Var "n"))) zero))
 
 -- Красивая печать без лишних скобок.
 instance {-# OVERLAPS #-} Show (Lambda String) where
-  show = undefined
+  show (Var x) = x
+  show (App (Abs x l1) l2) = "(" ++ show (Abs x l1) ++ ") " ++ show l2
+  show (App l1 (App l2 l3)) = show l1 ++ " (" ++ show (App l2 l3) ++ ")"
+  show (App l r) = show l ++ " " ++ show r
+  show (Abs l r) = "\\" ++ l ++ "." ++ show r
+
+-- data Lambda a = Var a
+--               | App (Lambda a) (Lambda a)
+--               | Abs a (Lambda a)
 
 instance {-# OVERLAPPABLE #-} Show a => Show (Lambda a) where
-  show = undefined
+  show (Var x) = show x
+  show (App (Abs x l1) l2) = "(" ++ show (Abs x l1) ++ ") " ++ show l2
+  show (App l1 (App l2 l3)) = show l1 ++ " (" ++ show (App l2 l3) ++ ")"
+  show (App l r) = show l ++ " " ++ show r
+  show (Abs l r) = "\\" ++ show l ++ "." ++ show r
 
 -- Выберите подходящий тип для подстановок.
-data Subst a
-
+data Subst a = Subst a (Lambda a) (Map.Map a a) -- переменная, заменитель, (map, которые рашает, на что заменять свободные переменные земенители при необходимости)
+                                                -- значения в Map подразумеваются отличными от всех переменных в лямбде, в которой заменяем
 -- Проверка термов на альфа-эквивалентность.
-alphaEq :: Eq a => Lambda a -> Lambda a -> Bool
-alphaEq = undefined
+alphaEq :: Ord a => Ord b => Lambda a -> Lambda b -> Bool -- Ord по причинам описанным в toDeBruijn
+alphaEq l r = toDeBruijn l == toDeBruijn r
+
+-- Множество свободных переменных 
+fv :: Ord a => Lambda a -> Set.Set a
+fv (Var x) = Set.singleton x
+fv (App l r) = Set.union (fv l) (fv r)
+fv (Abs x l) = Set.delete x (fv l)
 
 -- Capture-avoiding substitution.
-cas :: Lambda a -> Subst a -> Lambda a
-cas = undefined
+cas :: Ord a => Lambda a -> Subst a -> Lambda a
+cas (Var x) (Subst v s _) | x == v = s
+                          | otherwise = Var x
+cas (App l r) subst = App (cas l subst) (cas r subst)
+cas (Abs x l) (Subst v s m) | x == v = Abs x l
+                            | Set.notMember x (fv s) = Abs x (cas l (Subst v s m))
+                            | otherwise = Abs (m Map.! x) (cas (cas l (Subst x (Var (m Map.! x)) Map.empty)) (Subst v s m))
+                            -- можно передавать Map.empty, из-за предположения в 86 строчке
+    
 
 -- Возможные стратегии редукции (о них расскажут 7 ноября).
 data Strategy = CallByValue | CallByName | NormalOrder | ApplicativeOrder
@@ -84,19 +116,46 @@ eval = undefined
 data DeBruijn = VarDB Int
               | AbsDB DeBruijn
               | AppDB DeBruijn DeBruijn
+              deriving (Eq)
 
 -- Красивая печать без лишних скобок.
 instance Show DeBruijn where
-  show = undefined
+  show (VarDB x) = show x
+  show (AppDB (AbsDB l1) l2) = "(" ++ show (AbsDB l1) ++ ") " ++ show l2
+  show (AppDB l1 (AppDB l2 l3)) = show l1 ++  " (" ++ show (AppDB l2 l3) ++ ")"
+  show (AppDB l r) = show l ++ " " ++ show r
+  show (AbsDB r) = "\\ " ++ show r
 
 -- λx. λy. x ≡ λ λ 2
 -- λx. λy. λz. x z (y z) ≡ λ λ λ 3 1 (2 1)
 -- λz. (λy. y (λx. x)) (λx. z x) ≡ λ (λ 1 (λ 1)) (λ 2 1)
 
 -- Преобразовать обычные лямбда-термы в деБрауновские
-toDeBruijn :: Lambda a -> DeBruijn
-toDeBruijn = undefined
+toDeBruijn :: Ord a => Lambda a -> DeBruijn -- выбор: либо (Ord a) и Map, либо (Hashable a) и HashMap 
+                                            -- ну или хранить все в листе и делать все за квадрат
+                                            -- (если без Map, то застрелиться можно)
+toDeBruijn l = getDeBruijn l 0 Map.empty
+  where
+    getDeBruijn :: Ord a => Lambda a -> Int -> Map.Map a Int -> DeBruijn
+    getDeBruijn (Var x) depth m | isNothing (Map.lookup x m) = VarDB (-1) -- Свободная переменная
+                                | otherwise = VarDB (depth - (m Map.! x) + 1)
+    getDeBruijn (Abs x rest) depth m = AbsDB (getDeBruijn rest (depth + 1) (Map.insert x (depth + 1) m))
+    getDeBruijn (App l r) depth m = AppDB (getDeBruijn l depth m) (getDeBruijn r depth m)
+
+-- add ≡ λm.λn.λf.λx.m f (n f x)
+-- add = Abs "m" (Abs "n" (Abs "f" (Abs "x" (App (App (Var "m") (Var "f")) (App (App (Var "n") (Var "f")) (Var "x"))))))
 
 -- Преобразовать деБрауновские лямбда-термы в обычные.
-fromDeBruijn :: DeBruijn -> Lambda a
-fromDeBruijn = undefined
+fromDeBruijn :: DeBruijn -> Lambda Int
+fromDeBruijn d = fst $ fromDB d 0 0 Map.empty
+  where
+    fromDB :: DeBruijn -> Int -> Int -> Map.Map Int Int -> (Lambda Int, Int)
+    fromDB (VarDB x) depth _ m | isNothing (Map.lookup x m) = (Var (-1), 0) -- Свободная переменная
+                               | otherwise = (Var (m Map.! (depth - x + 1)), 0)
+    fromDB (AbsDB rest) depth lastID m = 
+      let (l, cnt) = fromDB rest (depth + 1) (lastID + 1) (Map.insert (depth + 1) (lastID + 1) m) in 
+        (Abs (lastID + 1) l, cnt + 1)
+    fromDB (AppDB l r) depth lastID m = 
+      let (curL, cnt) = fromDB l depth lastID m in
+      let (curR, res) = fromDB r depth (lastID + cnt) m in
+      (App curL curR, res)
--- test/Test/Lambda.hs
+++ test/Test/Lambda.hs
@@ -1,1 +1,112 @@
 module Test.Lambda where
+
+import Test.Tasty.HUnit (Assertion, (@?=))
+import Lambda
+import Hedgehog.Internal.Property (Diff(diffPrefix))
+import qualified Data.Map.Strict as Map
+import qualified Data.Set as Set
+
+unit_show :: Assertion
+unit_show = do
+  show true @?= "\\x.\\y.x"
+  show false @?= "\\x.\\y.y"
+  show Lambda.and @?= "\\p.\\q.p q p"
+  show Lambda.or @?= "\\p.\\q.p p q"
+  show zero @?= "\\f.\\x.x"
+  show four @?= "\\f.\\x.f (f (f (f x)))"
+  show add @?= "\\m.\\n.\\f.\\x.m f (n f x)"
+  show (App (Abs "x" (Var "x y")) (Var "z")) @?= "(\\x.x y) z"
+  show l4 @?= "\\x.x \\x.x"
+
+
+db1 = VarDB (-1)
+l1 = Var "x"
+r1 = Var (-1)
+cas1 = Var "y"
+
+-- λy. (λx. x y) (λz. y z) ≡ λ (λ 1 2) (λ 2 1)
+db2 = AbsDB (AppDB (AbsDB (AppDB (VarDB 1) (VarDB 2))) (AbsDB (AppDB (VarDB 2) (VarDB 1))))
+l2  = Abs "y" (App (Abs "x" (App (Var "x") (Var "y"))) (Abs "z" (App (Var "y") (Var "z"))))
+r2  = Abs 1 (App (Abs 2 (App (Var 2) (Var 1))) (Abs 3 (App (Var 1) (Var 3))))
+
+
+-- λz. (λy. y (λx. x)) (λx. z x) ≡ λ (λ 1 (λ 1)) (λ 2 1)
+db3 = AbsDB (AppDB( AbsDB (AppDB (VarDB 1) (AbsDB (VarDB 1)))) (AbsDB(AppDB (VarDB 2) (VarDB 1))))
+l3 = Abs "z" (App(Abs "y" (App (Var "y") (Abs "x" (Var "x")))) (Abs "x" (App (Var "z") (Var "x"))))
+r3 = Abs 1 (App(Abs 2 (App (Var 2) (Abs 3 (Var 3)))) (Abs 4 (App (Var 1) (Var 4))))
+
+-- λx.x (λx. x) ≡ λ 1 (λ 1)
+db4 = AbsDB (AppDB (VarDB 1) (AbsDB (VarDB 1)))
+l4 = Abs "x" (App (Var "x") (Abs "x" (Var "x")))
+-- λx.x (λy. y)
+l4_1 = Abs "x" (App (Var "x") (Abs "y" (Var "y")))
+r4 = Abs 1 (App (Var 1) (Abs 2 (Var 2)))
+
+unit_DeBruijne :: Assertion
+unit_DeBruijne = do
+  toDeBruijn l1 @?= db1 
+  toDeBruijn l2 @?= db2
+  toDeBruijn l3 @?= db3
+  toDeBruijn l4 @?= db4
+  toDeBruijn l4_1 @?= db4
+
+  fromDeBruijn db1 @?= r1
+  fromDeBruijn db2 @?= r2
+  fromDeBruijn db3 @?= r3
+  fromDeBruijn db4 @?= r4
+
+unit_alphaEq :: Assertion
+unit_alphaEq = do
+  alphaEq l1 r1 @?= True
+  alphaEq (fromDeBruijn db1) l1 @?= True
+  alphaEq l2 r2 @?= True
+  alphaEq (fromDeBruijn db2) l2 @?= True
+  alphaEq l3 r3 @?= True
+  alphaEq (fromDeBruijn  db3) l3 @?= True
+  alphaEq l4 r4 @?= True
+  alphaEq (fromDeBruijn  db4) l4 @?= True
+  alphaEq l4 l4_1 @?= True
+
+  alphaEq l1 r2 @?= False
+  alphaEq l1 l2 @?= False
+  alphaEq l1 r3 @?= False
+  alphaEq l1 l3 @?= False
+  alphaEq l1 l4 @?= False
+  alphaEq l1 r4 @?= False
+
+  alphaEq l2 r1 @?= False
+  alphaEq l2 l1 @?= False
+  alphaEq l2 r3 @?= False
+  alphaEq l2 l3 @?= False
+  alphaEq l2 l4 @?= False
+  alphaEq l2 r4 @?= False
+  
+  alphaEq l3 r2 @?= False
+  alphaEq l3 l2 @?= False
+  alphaEq l3 r1 @?= False
+  alphaEq l3 l1 @?= False
+  alphaEq l3 l4 @?= False
+  alphaEq l3 r4 @?= False
+
+  alphaEq l4 r2 @?= False
+  alphaEq l4 l2 @?= False
+  alphaEq l4 r1 @?= False
+  alphaEq l4 l1 @?= False
+  alphaEq l4 l3 @?= False
+  alphaEq l4 r3 @?= False
+
+
+-- (λx. z (λz. y x z)) (y (λy. y (λw. w y)))
+withFree1 = App (Abs "x" (App (Var "z") (Abs "z" (App (App (Var "y") (Var "x")) (Var "z"))))) (App (Var "y") (Abs "y" (App (Var "y") (Abs "w" (App (Var "w") (Var "y"))))))
+casMap1_0 = Map.fromList [("x", "s1"), ("w", "s2")]
+sub1_0 = Subst "y" (App (Var "x") (Var "w")) casMap1_0
+casResult1_0 = App (Abs "s1" (App (Var "z") (Abs "z" (App (App (App (Var "x") (Var "w")) (Var "s1")) (Var "z"))))) (App (App (Var "x") (Var "w")) (Abs "y" (App (Var "y") (Abs "w" (App (Var "w") (Var "y"))))))
+
+unit_cas :: Assertion
+unit_cas = do
+  fv withFree1 @?= Set.fromList ["y", "z"]
+
+  cas l1 (Subst "x" (Var "y") (Map.singleton "y" "z")) @?= cas1
+  cas l2 (Subst "y" (Var "Solzhenitsyn") (Map.singleton "Solzhenitsyn" "Gulag")) @?= l2
+
+  cas withFree1 sub1_0 @?= casResult1_0
