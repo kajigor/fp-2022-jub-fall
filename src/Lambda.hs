d671e63608ab86673ba8c93de173ffc508755d2f
meramiha <62883073+meramiha@users.noreply.github.com>
2022-11-09 22:21:09 +0100

    that was hard, and I didn't make it all the way

--- src/Lambda.hs
+++ src/Lambda.hs
@@ -57,10 +57,29 @@ mult' = Abs "m" (Abs "n" (App (App (Var "m") (App add (Var "n"))) zero))
 
 -- Красивая печать без лишних скобок.
 instance {-# OVERLAPS #-} Show (Lambda String) where
-  show = undefined
+  show (Var a) = a
+  show (Abs a b) = "\\" ++ a ++ "." ++ (show b)
+
+  show (App a b) = stringA ++ " " ++ stringB where
+    stringA = case a of
+      (Abs x m) -> "(" ++ show a ++ ")"
+      otherwise -> show a
+    stringB = case b of
+      (Var x) -> show b
+      otherwise -> "(" ++ show b ++ ")"
+ 
+
+toLambdaString :: Show a => Lambda a -> Lambda String
+toLambdaString (Var x) = Var $ (show x)
+toLambdaString (App x y) = App (toLambdaString x) (toLambdaString y)
+toLambdaString (Abs x y) = Abs (show x) (toLambdaString y)
 
 instance {-# OVERLAPPABLE #-} Show a => Show (Lambda a) where
-  show = undefined
+  
+  show a = show $ toLambdaString a
+
+    
+    
 
 -- Выберите подходящий тип для подстановок.
 data Subst a
@@ -88,7 +107,14 @@ data DeBruijn = VarDB Int
 -- Красивая печать без лишних скобок.
 instance Show DeBruijn where
   show = undefined
+    -- show (VarDB a) = show a
+    -- show (AbsDB a) = "\\." ++ (show a)
 
+    -- show (AppDB x y) = x ++ " " ++ stringB where
+    --   stringB = case y of
+    --     (AbsDB a) -> "(" ++ show y ++ ")"
+    --     (AppDB a b) -> "(" ++ show y ++ ")"
+    --     otherwise -> show y
 -- λx. λy. x ≡ λ λ 2
 -- λx. λy. λz. x z (y z) ≡ λ λ λ 3 1 (2 1)
 -- λz. (λy. y (λx. x)) (λx. z x) ≡ λ (λ 1 (λ 1)) (λ 2 1)
--- test/Test/Lambda.hs
+++ test/Test/Lambda.hs
@@ -1,1 +1,47 @@
+{-# LANGUAGE ImplicitParams #-}
+
 module Test.Lambda where
+
+
+import Test.Tasty.HUnit (Assertion, (@?=))
+import Test.HUnit.Approx ((@?~))
+
+import Lambda
+
+l0 = Var "x"
+l1 = Abs "x" (Var "x")
+l2 = App (Var "x") (Var "y")
+l3 = Abs "x" (Abs "y" (Abs "z" (App (App (Var "x") (Var "y")) (Var "z"))))
+l4 = App (App (Var "a") (Var "b")) (App (Var "c") (Var "d"))
+l5 = App (Var "a") (App (Var "b") (App (Var "c") (Var "d")))
+l6 = App (App (App (Var "a") (Var "b")) (Var "c")) (Var "d")
+
+y = Abs "f" (App (Abs "x" (App (Var "f") (App (Var "x") (Var "x"))))(Abs "x" (App (Var "f") (App (Var "x") (Var "x")))))
+
+
+unit_showString = do
+	show l0 @?= "x"
+	show l1 @?= "\\x.x"
+	show l2 @?= "x y"
+	show l3 @?= "\\x.\\y.\\z.x y z"
+	show l4 @?= "a b (c d)"
+	show l5 @?= "a (b (c d))"
+	show l6 @?= "a b c d"
+
+	show y @?= "\\f.(\\x.f (x x)) (\\x.f (x x))"
+
+data MyTestType = A | B | C
+	deriving (Show)
+k0 = A
+k3 = Abs A (Abs B (Abs C (App (App (Var A) (Var B)) (Var C))))
+unit_showNotString = do
+	show k0 @?= "A"
+	show k3 @?= "\\A.\\B.\\C.A B C"
+
+-- db0 = AbsDB (AbsDB (VarDB 2))
+-- db1 = AbsDB ( AbsDB ( AbsDB ( AppDB  (AppDB (VarDB 3) (VarDB 1)) (AppDB (VarDB 2) (VarDB 1))  ) ))
+-- db2 = AbsDB (AppDB (AbsDB (AppDB (VarDB 1) (AbsDB (VarDB 1)))) (AbsDB (AppDB (VarDB 2) (VarDB 1))))
+-- unit_showDeBruijn = do
+-- 	show db0 @?= "\\.\\.2"
+-- 	show db1 @?= "\\.\\.\\.3 1 (2 1)"
+-- 	show db2 @?= "\\.(\\.1 (\\.1)) (\\.2 1)"
