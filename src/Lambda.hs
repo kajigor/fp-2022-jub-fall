da7b74f42ba7982f2a67ba9d2d10d365037e2af8
rinas-ink <kotenok04in@gmail.com>
2022-11-19 09:26:14 +0100

    toDB, from DB in process

--- src/Lambda.hs
+++ src/Lambda.hs
@@ -1,12 +1,100 @@
 {-# LANGUAGE FlexibleInstances #-}
 module Lambda where
 
+import Control.Monad.Error (Error(strMsg))
+
+import Data.List
+import Data.Maybe
+import Data.Char
+import Control.Concurrent (yield)
+
+
 -- Тип для лямбда-термов.
 -- Параметризуем типом переменной.
 data Lambda a = Var a
               | App (Lambda a) (Lambda a)
               | Abs a (Lambda a)
 
+isVar::Lambda a -> Bool
+isVar (Var _) = True
+isVar _ = False
+
+isApp::Lambda a -> Bool
+isApp App{} = True
+isApp _ = False
+
+isAbs::Lambda a -> Bool
+isAbs Abs{} = True
+isAbs _ = False
+
+wrapIf:: Bool -> String -> String
+wrapIf True s = "(" ++ s ++ ")"
+wrapIf False s = s
+
+-- Красивая печать без лишних скобок.
+instance {-# OVERLAPS #-} Show (Lambda String) where
+  show (Var str) = str
+  show (App x y) = wrapIf (isAbs x) (show x) ++ " " ++ wrapIf (isAbs y || isApp y) (show y)
+  show (Abs x y) = "\\" ++ x ++ "." ++ show y
+
+instance {-# OVERLAPPABLE #-} Show a => Show (Lambda a) where
+  show = undefined
+
+-- Выберите подходящий тип для подстановок.
+data Subst a
+
+-- Проверка термов на альфа-эквивалентность.
+alphaEq :: Eq a => Lambda a -> Lambda a -> Bool
+alphaEq = undefined
+
+-- Capture-avoiding substitution.
+cas :: Lambda a -> Subst a -> Lambda a
+cas = undefined
+
+-- Возможные стратегии редукции (о них расскажут 7 ноября).
+data Strategy = CallByValue | CallByName | NormalOrder | ApplicativeOrder
+
+-- Интерпретатор лямбда термов, учитывающий стратегию.
+eval :: Strategy -> Lambda a -> Lambda a
+eval = undefined
+
+-- ДеБрауновское представление лямбда-термов
+data DeBruijn = VarDB Int
+              | AbsDB DeBruijn
+              | AppDB DeBruijn DeBruijn
+
+isVarDB::DeBruijn -> Bool
+isVarDB VarDB{} = True
+isVarDB _ = False
+
+isAppDB::DeBruijn -> Bool
+isAppDB AppDB{} = True
+isAppDB _ = False
+
+isAbsDB::DeBruijn -> Bool
+isAbsDB AbsDB{} = True
+isAbsDB _ = False
+
+-- Красивая печать без лишних скобок.
+instance Show DeBruijn where
+  show (VarDB num) = show num
+  show (AppDB x y) = wrapIf (isAbsDB x) (show x) ++ " " ++ wrapIf (isAbsDB y || isAppDB y) (show y)
+  show (AbsDB x) = "\\ " ++ show x
+
+-- λx. λy. x ≡ λ λ 2
+-- λx. λy. λz. x z (y z) ≡ λ λ λ 3 1 (2 1)
+-- λz. (λy. y (λx. x)) (λx. z x) ≡ λ (λ 1 (λ 1)) (λ 2 1)
+
+-- Преобразовать обычные лямбда-термы в деБрауновские
+toDeBruijn :: Eq a => Lambda a -> DeBruijn
+toDeBruijn = convert []
+  where
+    convert acc (Var str) = VarDB (fromJust (elemIndex str acc))
+    convert acc (App x y) = AppDB (convert acc x) (convert acc y)
+    convert acc (Abs x y) = AbsDB (convert (x:acc) y)
+
+
+-- Lambdas
 -- true ≡ λx.λy.x
 true = Abs "x" (Abs "y" (Var "x"))
 
@@ -50,53 +138,8 @@ successor = Abs "n" (Abs "f" (Abs "x" (App (Var "f") (App (App (Var "n") (Var "f
 add' = Abs "m" (Abs "n" (App (App (Var "m") successor) (Var "n")))
 
 -- mult ≡ λm.λn.λf.m (n f)
-mult = Abs "m" (Abs "n" (App (Var "m") (App (Var "n") (Var "f"))))
+mult = Abs "m" (Abs "n" (Abs "f" (App (Var "m") (App (Var "n") (Var "f")))))
 
 -- mult' ≡ λm.λn.m (add n) 0
 mult' = Abs "m" (Abs "n" (App (App (Var "m") (App add (Var "n"))) zero))
 
--- Красивая печать без лишних скобок.
-instance {-# OVERLAPS #-} Show (Lambda String) where
-  show = undefined
-
-instance {-# OVERLAPPABLE #-} Show a => Show (Lambda a) where
-  show = undefined
-
--- Выберите подходящий тип для подстановок.
-data Subst a
-
--- Проверка термов на альфа-эквивалентность.
-alphaEq :: Eq a => Lambda a -> Lambda a -> Bool
-alphaEq = undefined
-
--- Capture-avoiding substitution.
-cas :: Lambda a -> Subst a -> Lambda a
-cas = undefined
-
--- Возможные стратегии редукции (о них расскажут 7 ноября).
-data Strategy = CallByValue | CallByName | NormalOrder | ApplicativeOrder
-
--- Интерпретатор лямбда термов, учитывающий стратегию.
-eval :: Strategy -> Lambda a -> Lambda a
-eval = undefined
-
--- ДеБрауновское представление лямбда-термов
-data DeBruijn = VarDB Int
-              | AbsDB DeBruijn
-              | AppDB DeBruijn DeBruijn
-
--- Красивая печать без лишних скобок.
-instance Show DeBruijn where
-  show = undefined
-
--- λx. λy. x ≡ λ λ 2
--- λx. λy. λz. x z (y z) ≡ λ λ λ 3 1 (2 1)
--- λz. (λy. y (λx. x)) (λx. z x) ≡ λ (λ 1 (λ 1)) (λ 2 1)
-
--- Преобразовать обычные лямбда-термы в деБрауновские
-toDeBruijn :: Lambda a -> DeBruijn
-toDeBruijn = undefined
-
--- Преобразовать деБрауновские лямбда-термы в обычные.
-fromDeBruijn :: DeBruijn -> Lambda a
-fromDeBruijn = undefined
--- test/Test/Lambda.hs
+++ test/Test/Lambda.hs
@@ -1,1 +1,38 @@
 module Test.Lambda where
+
+import Lambda
+import Test.Tasty.HUnit
+
+import Text.Printf
+
+unit_show_lambda :: IO ()
+unit_show_lambda = do
+  show Lambda.true @?= "\\x.\\y.x"
+  show Lambda.false @?= "\\x.\\y.y"
+  show Lambda.and @?= "\\p.\\q.p q p"
+  show Lambda.or @?= "\\p.\\q.p p q"
+  show Lambda.not @?= "\\p.p ("++ show Lambda.false ++ ") ("++ show Lambda.true ++ ")"
+  show Lambda.ifThenElse @?= "\\p.\\a.\\b.p a b"
+  show Lambda.zero @?= "\\f.\\x.x"
+  show Lambda.one @?= "\\f.\\x.f x"
+  show Lambda.three @?= "\\f.\\x.f (f (f x))"
+  show Lambda.add @?= "\\m.\\n.\\f.\\x.m f (n f x)"
+  show Lambda.successor @?= "\\n.\\f.\\x.f (n f x)"
+  show Lambda.mult @?= "\\m.\\n.\\f.m (n f)"
+  show Lambda.t @?="v0"
+
+unit_show_debruijn :: IO ()
+unit_show_debruijn = do
+  show (toDeBruijn Lambda.true) @?= "\\ \\ 1"
+  show (toDeBruijn Lambda.false) @?= "\\ \\ 0"
+  show (toDeBruijn Lambda.and) @?= "\\ \\ 1 0 1"
+  show (toDeBruijn Lambda.or) @?= "\\ \\ 1 1 0"
+  show (toDeBruijn Lambda.not) @?=  "\\ 0 (\\ \\ 0) (\\ \\ 1)"
+  show (toDeBruijn Lambda.ifThenElse) @?= "\\ \\ \\ 2 1 0"
+  show (toDeBruijn Lambda.zero) @?= "\\ \\ 0"
+  show (toDeBruijn Lambda.one) @?= "\\ \\ 1 0"
+  show (toDeBruijn Lambda.three) @?= "\\ \\ 1 (1 (1 0))"
+  show (toDeBruijn Lambda.add) @?= "\\ \\ \\ \\ 3 1 (2 1 0)"
+  show (toDeBruijn Lambda.successor) @?= "\\ \\ \\ 1 (2 1 0)"
+  show (toDeBruijn Lambda.mult) @?= "\\ \\ \\ 2 (1 0)"
+  
