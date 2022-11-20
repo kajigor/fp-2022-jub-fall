567a8a715c9de21e2e4bb5cff62612e3ab35e455
kabachok-vpanike <90154942+kabachok-vpanike@users.noreply.github.com>
2022-11-20 14:38:17 +0100

    Update Lambda.hs

--- test/Test/Lambda.hs
+++ test/Test/Lambda.hs
@@ -15,6 +15,8 @@ t5 = App (Abs "x" (App (Var "x") (Var "x"))) (Abs "x" (App (Var "x") (Var "x")))
 
 t6 = App (Abs "x" (Abs "x" (Var "x"))) (Abs "y" (Var "y"))
 
+t7 = App (Abs (Just 1) (Abs (Just 1) (Var (Just 1)))) (Abs (Just 2) (Var (Just 2))) --chaotic evil
+
 e1 = App (App (Var "x") (Var "y")) (Var "z")
 
 e2 = App (App (Var "a") (Var "b")) (Var "c")
@@ -37,7 +39,7 @@ r5 = AppDB (AbsDB (AppDB (VarDB 0) (VarDB 0))) (AbsDB (AppDB (VarDB 0) (VarDB 0)
 
 r6 = AppDB (AbsDB (AbsDB (VarDB 0))) (AbsDB (VarDB 0))
 
-sub = Sub (Var "x") (Var "y")
+sub = Sub (SubVar "x") (Var "y")
 
 e = Abs "x" (Abs "y" (App (Var "x") (Var "y")))
 
@@ -45,17 +47,18 @@ unit_lambdaShow = do
   show t1 @?= "λx.λy.λs.λz.x s(y s z)"
   show t2 @?= "λp.λa.λb.p a b"
   show t3 @?= "λm.λn.λf.λx.m f(n f x)"
-  show t4 @?= "λx.λx.x λy.y"
-  show t5 @?= "λx.x x λx.x x"
-  show t6 @?= "λx.λx.x λy.y"
+  show t4 @?= "(λx.λx.x) λy.y"
+  show t5 @?= "(λx.x x) λx.x x"
+  show t6 @?= "(λx.λx.x) λy.y"
+  show t7 @?= "(λJust 1.λJust 1.Just 1) λJust 2.Just 2"
 
 unit_deBruijn = do
-  toDeBruijn t1 [] [] @?= r1
-  toDeBruijn t2 [] [] @?= r2
-  toDeBruijn t3 [] [] @?= r3
-  toDeBruijn t4 [] [] @?= r4
-  toDeBruijn t5 [] [] @?= r5
-  toDeBruijn t6 [] [] @?= r6
+  toDeBruijn t1 @?= r1
+  toDeBruijn t2 @?= r2
+  toDeBruijn t3 @?= r3
+  toDeBruijn t4 @?= r4
+  toDeBruijn t5 @?= r5
+  toDeBruijn t6 @?= r6
 
 unit_alphaEq = do
   alphaEq e1 e2 @?= True
