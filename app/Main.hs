1c98c5e7337a9d548ecd46266f1e793e8d2f511f
alexflen <77319251+alexflen@users.noreply.github.com>
2022-12-06 16:43:05 +0200

    added buttons

--- app/Main.hs
+++ app/Main.hs
@@ -4,16 +4,49 @@ import Minesweeper
 import System.Random
 import Graphics.UI.Gtk
 
+data FieldButtons = [[BU]]
+
 main :: IO ()
 main = do
-  g <- newStdGen
+    g <- newStdGen
 
-  let fieldPair = generateField 10 10 10 (randomRs (0, 2147483647) g)
-  let field = fst fieldPair
-  let rndLst = snd fieldPair
+    initGUI
+    window <- windowNew
 
-  initGUI
-  window <- windowNew
-  widgetShowAll window
-  mainGUI
+    let rows = 10
+    let cols = 10
+    let bombs = 10
 
+    let fieldPair = generateField rows cols bombs (randomRs (0, 2147483647) g)
+    let field = fst fieldPair
+    let rndLst = snd fieldPair
+
+    set window [windowTitle := "Table", containerBorderWidth := 20,
+                windowDefaultWidth := 150, windowDefaultHeight := 100]
+    table   <- tableNew rows cols True
+    containerAdd window table
+    --button1 <- buttonNewWithLabel "On"
+    --onClicked button1 (buttonSwitch button1)
+    --tableAttachDefaults table button1 0 1 0 1
+    --button2 <- buttonNewWithLabel "Off"
+   -- onClicked button2 (buttonSwitch button2)
+    --tableAttachDefaults table button2 1 2 0 1
+    --button3 <- buttonNewWithLabel "Quit"
+    --onClicked button3 mainQuit
+    --tableAttachDefaults table button3 0 2 1 2
+    buttons <- createButtonFieldTable field table
+    onDestroy window mainQuit
+
+    widgetShowAll window
+    mainGUI
+
+createButtonFieldTable :: FieldChars -> IO Table -> FieldButtons
+createButtonFieldTable field table = createButtonFieldTableHelper field table 0 0 []
+    where 
+        createButtonFieldTableHelper field table x y currRow =
+            if (x == rows) then []
+            else if (y == cols) then currRow : (createButtonFieldTableHelper rows cols (x + 1) 0 [])
+            else do
+                button <- buttonNewWithLabel ""
+                tableAttachDefaults table button2 cols (cols + 1) rows (rows + 1)
+                (createButtonFieldTablesHelper rows cols x (y + 1) (currRow ++ [button]))
