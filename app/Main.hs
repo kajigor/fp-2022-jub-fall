b3e1c9aee48e104d0b858afd96be7ce526456c99
kabachok-vpanike <90154942+kabachok-vpanike@users.noreply.github.com>
2023-01-01 14:33:09 +0100

    Update Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -1,30 +1,43 @@
+{-# LANGUAGE DataKinds #-}
+
 module Main (main) where
 
 import Graphics.Image as I
 import Lib (fromRGB2D, pixelizeWithR, toGrayScale2D, toTrueGrayScale2D)
 import System.Environment (getArgs)
+import Text.Read
+
+data IMG = ImgRGBA (I.Image VS I.RGBA Word8) | ImgRGB (I.Image VS I.RGB Word8)
 
 main :: IO ()
 main = do
   args <- getArgs
   let argsNum = length args
-
-  let (sourcePath, resPath, radius) =
+  let (sourcePath, resPath, r) =
         case argsNum of
-          3 -> (args !! 0, args !! 1, read $ args !! 2)
+          3 -> (args !! 0, args !! 1, readMaybe (args !! 2) :: Maybe Int)
           _ -> error "=== Too few arguments. Usage: path to the source file, destination folder and pixelization radius ==="
 
-  l <- readImageExact PNG sourcePath :: IO (Either String (I.Image VS I.RGBA Word8))
-  r <- readImageExact PNG sourcePath :: IO (Either String (I.Image VS I.RGB Word8))
+  let radius = case r of
+        Nothing -> error "Radius must be an integer"
+        Just r -> r
+
+  if radius <= 0
+    then error "Radius must be a positive integer"
+    else return ()
+
+  l <- readImageExact PNG sourcePath :: IO (Either String (I.Image VS I.RGB Word8))
+  r <- readImageExact PNG sourcePath :: IO (Either String (I.Image VS I.RGBA Word8))
   let image = case l of
-        Right img -> Left img
+        Right img -> ImgRGB img
         Left _ -> case r of
-          Right img -> Right img
+          Right img -> ImgRGBA img
           Left _ -> error "Unsupportable format. You can use PNG images only"
 
   let vec = case image of
-        Right im -> fromRGB2D $ toLists im
-        Left im -> toLists im
+        ImgRGB im -> fromRGB2D $ toLists im
+        ImgRGBA im -> toLists im
+
   let grayVec = toGrayScale2D vec
   let pixelized1 = pixelizeWithR vec radius
   let pixelized2 = pixelizeWithR grayVec radius
@@ -33,7 +46,3 @@ main = do
   writeImageExact PNG [] (resPath ++ "/resColored.png") backToImage
   writeImageExact PNG [] (resPath ++ "/resB&W.png") backToImageGray
   putStrLn ("=== Your pixelized images are" ++ " resColored.png and resB&W.png (in black-and-white) " ++ "at " ++ resPath ++ " ===")
-
---putStrLn "=============================================="
---setDisplayProgram ("gpicview", True)
---displayImage backToImage
